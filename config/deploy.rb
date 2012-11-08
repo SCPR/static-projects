68e2eaccff89707dc8dd064132d8df98d89a19a8
Bryan Ricker <bricker88@gmail.com>
2012-11-08 13:48:05 -0800

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :application, "static"
+set :scm, :git
+set :repository,  "git@github.com:SCPR/static-projects.git"
+set :scm_verbose, true
+set :deploy_via, :remote_cache
+set :deploy_to, "/web/projects/static"
+
+set :user, "archive"
+set :use_sudo, false
+set :group_writable, false
+
+# --------------
+# Variables
+set :branch, "master"
+
+# --------------
+# Roles
+role :app, "media.scpr.org"
+role :web, "media.scpr.org"
+
+# Setup staging
+task :staging do
+  roles.clear
+  role :app, "scprdev.org"
+  role :web, "scprdev.org"
+end
+
+# Noop restart things
+namespace :deploy do
+  task :start {}
+  task :stop {}
+  task :restart {}
+end
+
+after "deploy:restart", "deploy:cleanup"
