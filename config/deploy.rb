ce09af10180c5e04f24acbe466cf6c9cd76d84bf
Bryan Ricker <bricker88@gmail.com>
2012-11-08 14:57:23 -0800

    Update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,11 +31,19 @@ namespace :deploy do
   task :stop do end
   task :restart do end
   task :migrate do end
-  
-  # Don't really want these files accessible at the web
+
+  # You can uncomment these tasks to hide these files
+  # on deploy. It shouldn't be necessary but this is 
+  # how to do it.
+  #
+  # Note that having ruby files publicly accessible is 
+  # totally fine, it will just download them like any
+  # other file.
   task :remove_repo_files do
-    run "cd #{latest_release} && rm -rf Capfile config"
-    run "cd #{latest_release} && find . -name 'README.*' -print0 | xargs -0 rm"
+#    # Remove the Capfile and the entire config/ directory
+#    run "cd #{latest_release} && rm -rf Capfile config"
+#    # Remove all README files, recursively
+#    run "cd #{latest_release} && find . -name 'README.*' -print0 | xargs -0 rm"
   end
 end
 
