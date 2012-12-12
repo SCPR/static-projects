4f09c3924a26c66ebc32f93e4a9a622e73fa3d58
Chris Keller <christopherlawrencekeller@gmail.com>
2012-12-12 12:49:15 -0800

    Select district and display data working

--- test/keller/capital-bonds/static-files/scripts/data-script.js
+++ test/keller/capital-bonds/static-files/scripts/data-script.js
@@ -4,7 +4,8 @@
     // begin main function
     jqueryNoConflict(document).ready(function(){
         retriveData();
-        drawChart();
+        //drawChart();
+
     });
     // end
 
@@ -27,7 +28,9 @@
     // function to grab data
     function retriveData() {
         jqueryNoConflict.getJSON('static-files/data/cab_data.json', processData);
+
     };
+    // end
 
     // function to build html display
     function buildTemplateWith(data){
@@ -35,198 +38,58 @@
             jqueryNoConflict('#school-debt-details').html(template(data));
         });
     };
+    // end
 
 
     // create objects from json data
     function processData(data){
 
-        buildTemplateWith(data);
+        // structure handlebars data like this
+        /*
+        var testData = {"testObjects": [{"cab_principal": "17441620", "debt_to_principal": "1.09", "maturity_date": "3/1/2009", "county": "Orange", "sale_date": "5/31/2007", "cab_interest": "1558380", "sale_year": "2007", "cab_debt": "19000000", "issuer": "Irvine Unified School District CFD No 06-1", "maturity_length": "1.8"}, {"cab_principal": "61577670", "debt_to_principal": "1.25", "maturity_date": "9/1/2011", "county": "Orange", "sale_date": "6/13/2007", "cab_interest": "15422330", "sale_year": "2007", "cab_debt": "77000000", "issuer": "Tustin Unified School District CFD No 07-1", "maturity_length": "4.2"}]};
+        buildTemplateWith(testData);
+        */
 
-        // loop through data for markers
-        for(var i=0; i<data.objects.length; i++){
+        var schoolDistrictValue;
+        var bonds = data.objects;
+        var testObjects = [];
 
-            var cab_principal = [parseFloat(data.objects[i].cab_principal)];
-            var cab_interest = [parseFloat(data.objects[i].cab_interest)];
+        $('#school-district').change(function(){
+            schoolDistrictValue = $('#school-district :selected').val();
 
-/*
-            var myLoopObject = {
-                cab_debt: data.objects[i].cab_debt,
-                cab_interest: data.objects[i].cab_interest,
-                cab_principal: data.objects[i].cab_principal,
-                county: data.objects[i].county,
-                debt_to_principal: data.objects[i].debt_to_principal,
-                issuer: data.objects[i].issuer,
-                maturity_date: data.objects[i].maturity_date,
-                maturity_length: data.objects[i].maturity_length,
-                sale_date: data.objects[i].sale_date,
-                sale_year: data.objects[i].sale_year
+                testObjects = [];
 
-            };
-*/
+                for(var i=0; i<bonds.length; i++){
 
-            var testDataPrincipal = {
-                name: 'Principal',
-                color: '#FDCC8A',
-                data: cab_principal
-            };
+                    if (bonds[i].issuer === schoolDistrictValue) {
 
-            var testDataInterest = {
-                name: 'Interest',
-                color: '#D7301F',
-                data: cab_interest
-            };
+                        var myQueriedDataObject = {
+                            cab_debt: bonds[i].cab_debt,
+                            cab_interest: bonds[i].cab_interest,
+                            cab_principal: bonds[i].cab_principal,
+                            county: bonds[i].county,
+                            debt_to_principal: bonds[i].debt_to_principal,
+                            issuer: bonds[i].issuer,
+                            maturity_date: bonds[i].maturity_date,
+                            maturity_length: bonds[i].maturity_length,
+                            sale_date: bonds[i].sale_date,
+                            sale_year: bonds[i].sale_year
+                        };
 
-        };
+                        console.log(myQueriedDataObject);
+                        testObjects.push(myQueriedDataObject);
 
-        chart.addSeries(testDataInterest);
-        chart.addSeries(testDataPrincipal);
-
-    };
-
-    //begin function
-    function drawChart(){
-
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'school-debt-chart',
-                backgroundColor: 'none',
-                //zoomType: 'xy',
-                type: 'column'
-            },
-
-            title: {
-                text: 'Alhambra Unified',
-                style: {
-                    fontFamily: '"proxima-nova", "Helvetica Neue", Helvetica, Arial, sans-serif;',
-                    fontSize: '20px',
-                    color: '#2B2B2B'
-                }
-            },
-
-            xAxis: {
-                categories: ['Total CAB Amount']
-            },
-
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Bond Payoff Amount'
-                },
-
-                stackLabels: {
-                    enabled: true,
-                    style: {
-                        fontWeight: 'bold',
-                        color: '#000000'
-                    },
-
-                    formatter: function() {
-                        return '<strong>Total:</strong> $' +
-                        Highcharts.numberFormat(this.total, 2, '.');
-                    },
-                }
-            },
-
-            legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'bottom',
-                x: 30,
-                y: 10,
-                floating: false,
-                borderColor: '#CCC',
-                borderWidth: 0,
-                shadow: true
-            },
-
-            credits: {
-                text: 'Example.com',
-                href: 'http://www.example.com'
-            },
-
-
-            tooltip: {
-                enabled: false,
-                formatter: function() {
-                    return '<strong>'+ this.series.name +
-                    '</strong>: $' + Highcharts.numberFormat(this.y, 2, '.');
-                }
-            },
-
-            plotOptions: {
-                column: {
-                    stacking: 'normal',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        fontWeight: 'bold',
-                        formatter: function () {
-                            return '<strong>' + this.series.name +
-                            '</strong>: $' + Highcharts.numberFormat(this.y, 2, '.');
-                        },
                     }
-                }
 
-            },
+                };
 
-            series: []
+                var testData = {
+                    objects: testObjects
+                };
+
+                buildTemplateWith(testData);
 
         });
 
     };
     // end
-
-    // function to add commas to string
-    function addCommas(nStr){
-    	nStr += '';
-    	x = nStr.split('.');
-    	x1 = x[0];
-    	x2 = x.length > 1 ? '.' + x[1] : '';
-    	var rgx = /(\d+)(\d{3})/;
-    	while (rgx.test(x1)) {
-    		x1 = x1.replace(rgx, '$1' + ',' + '$2');
-    	}
-    	return x1 + x2;
-    };
-    // end
-
-    // function to generate iframe embed code
-    function embedBox() {
-        var embed_url = '#';
-        jAlert('<strong>To embed this on your blog or site, just copy this code:<br></strong>&lt;iframe src=\"'+
-        embed_url +
-        '\" width=\"420px\" height=\"450px\" scrolling=\"no\" frameborder=\"0\"&gt;&lt;/iframe>', 'Share or Embed');
-    };
-    // end
-
-
-
-
-
-
-/*
-    // render template
-    function renderYourTemplate(){
-
-        getTemplateAjax('static-files/templates/debt-table.handlebars', function(template) {
-
-            var testData = {
-                cab_debt: "19,000,000",
-                cab_interest: "1,558,380",
-                cab_principal: "17,441,620",
-                county: "Orange",
-                debt_to_principal: "1.09",
-                issuer: "Irvine Unified School District CFD No 06-1",
-                maturity_date: "3/1/2009",
-                maturity_length: "1.8",
-                sale_date: "5/31/2007",
-                sale_year: "2007"
-            }
-
-            jqueryNoConflict('#debt-table-content').html(template(testData));
-
-        })
-
-    };
-    // end
-*/
