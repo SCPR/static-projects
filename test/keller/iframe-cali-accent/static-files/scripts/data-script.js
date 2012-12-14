63cf7d79564ac81194a82a57150c428797dc8cf4
Chris Keller <christopherlawrencekeller@gmail.com>
2012-12-14 15:52:03 -0800

    moves cali accent project to iframe setup

--- test/keller/iframe-cali-accent/static-files/scripts/data-script.js
+++ test/keller/iframe-cali-accent/static-files/scripts/data-script.js
@@ -3,61 +3,9 @@ var map;
 
 // begin main function
 jqueryNoConflict(document).ready(function() {
-    renderStaticTemplates();
     createMap();
 });
 // end
-
-// render handlebars templates via ajax
-function getTemplateAjax(path, callback){
-    var source, template;
-    jqueryNoConflict.ajax({
-        url: path,
-        success: function (data) {
-            source = data;
-            template = Handlebars.compile(source);
-            if (callback) callback(template);
-        }
-    });
-}
-//end
-
-// begin
-function renderStaticTemplates(){
-    renderKpccHeaderTemplate();
-    renderDataDetailsTemplate();
-    renderDataFooterTemplate();
-    renderKpccFooterTemplate();
-};
-// end
-
-// create data footer template
-function renderKpccHeaderTemplate(){
-    getTemplateAjax('static-files/templates/kpcc-header.handlebars', function(template) {
-        jqueryNoConflict('#kpcc-header').html(template());
-    })
-};
-
-// create data details template
-function renderDataDetailsTemplate(){
-    getTemplateAjax('static-files/templates/data-details.handlebars', function(template) {
-        jqueryNoConflict('#data-details').html(template());
-    })
-};
-
-// create data footer template
-function renderDataFooterTemplate(){
-    getTemplateAjax('static-files/templates/data-footer.handlebars', function(template) {
-        jqueryNoConflict('#data-footer').html(template());
-    })
-};
-
-// create data footer template
-function renderKpccFooterTemplate(){
-    getTemplateAjax('static-files/templates/kpcc-footer.handlebars', function(template) {
-        jqueryNoConflict('#kpcc-footer').html(template());
-    })
-};
 
 // begin function
 function createMap(){
@@ -182,7 +130,7 @@ function createMap(){
 
         jqueryNoConflict('#data-description').html(
             '<p><strong>Name: </strong>' + e.row['name'].value +
-            '<p><strong>City: </strong>' + e.row['location'].value);
+            '<br /><strong>City: </strong>' + e.row['location'].value + '</p>');
 
         jqueryNoConflict('#data-audio-player').html(
             '<a class=\"soundcloud-player\" href=\" ' + trackURL + '\">Play</a>');
