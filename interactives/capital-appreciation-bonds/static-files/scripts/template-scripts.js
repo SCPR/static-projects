13d1eb511f82f8fa991e0927f793dd639bd95705
Chris Keller <christopherlawrencekeller@gmail.com>
2012-12-14 11:02:25 -0800

    adds glossary

--- interactives/capital-appreciation-bonds/static-files/scripts/template-scripts.js
+++ interactives/capital-appreciation-bonds/static-files/scripts/template-scripts.js
@@ -24,7 +24,7 @@ function getTemplateAjax(path, callback) {
 function renderStaticTemplates(){
     renderKpccHeaderTemplate();
     renderDataDetailsTemplate();
-    //renderDebtTableTermsTemplate();
+    renderDebtTableTermsTemplate();
     renderDataFooterTemplate();
     renderKpccFooterTemplate();
 };
