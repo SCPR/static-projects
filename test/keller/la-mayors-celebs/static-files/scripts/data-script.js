79abf498dd1de89775e5152bd897e391e12241ac
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-09 16:36:56 -0800

    updates

--- test/keller/la-mayors-celebs/static-files/scripts/data-script.js
+++ test/keller/la-mayors-celebs/static-files/scripts/data-script.js
@@ -1,60 +1,163 @@
-// begin main function
-jqueryNoConflict(document).ready(function() {
-    getFusionTableData();
+var jqueryNoConflict = jQuery;
+
+//begin main function
+jqueryNoConflict(document).ready(function(){
+    renderTestTemplate();
 });
-// end
+//end main function
 
-// retrieve json from Fusion Table
-function getFusionTableData() {
-    var tableID = '1JoBPVazF_LHrzNAKzPWW6CLLEe8S5V7hbvScbfY';
-    var query = 'SELECT * FROM ' + tableID;
-    var encodedQuery = encodeURIComponent(query);
-    var urlPrefix = 'https://www.googleapis.com/fusiontables/v1/query?key=';
-    var apiKey = 'AIzaSyBRs68D8JTAbrLy6R_zsYABLMgOV6zxC-4';
-    var urlSuffix = '&sql=' + encodedQuery + '&callback=?';
-    var url = urlPrefix + apiKey + urlSuffix
-    jqueryNoConflict.getJSON(url, createArrayFrom);
-};
-// end
-
-// organize json data
-function createArrayFrom(data){
-
-    // begin loop
-    for (var i = 0; i < data.rows.length; i++) {
-
-        // set each item of loop to variable
-        var row = data.rows[i];
-
-        // set each item of object to variable
-        var Endorser = row[0];
-        var Position = row[1];
-        var Endorsee = row[2];
-        var Endorsee_Image = row[3];
-        var Story_Url = row[4];
-
-        if(Endorsee == "Eric Garcetti") {
-            jqueryNoConflict("#data-container-garcetti").append(
-                '<a href="' + Story_Url + '">' + Endorser + '</a>, ' + Position + '<br />');
-
-        } else if(Endorsee == "Wendy Greuel") {
-            jqueryNoConflict("#data-container-greuel").append(
-                '<a href="' + Story_Url + '">' + Endorser + '</a>, ' + Position + '<br />');
-
-        } else if(Endorsee == "Jan Perry") {
-            jqueryNoConflict("#data-container-perry").append(
-                '<a href="' + Story_Url + '">' + Endorser + '</a>, ' + Position + '<br />');
-
-        } else if(Endorsee == "Kevin James") {
-            jqueryNoConflict("#data-container-james").append(
-                '<a href="' + Story_Url + '">' + Endorser + '</a>, ' + Position + '<br />');
-
-        } else {
-
+// render handlebars templates via ajax
+function getTemplateAjax(path, callback) {
+    var source, template;
+    jqueryNoConflict.ajax({
+        url: path,
+        success: function (data) {
+            source = data;
+            template = Handlebars.compile(source);
+            if (callback) callback(template);
         }
+    });
+}
+//end
 
-   };
-    // end loop
-
+var celebrityData = {
+    "objects": [{
+        candidate: "Austin Beutner",
+            donors: [
+                {donorName: "Annabeth Gish", donorContribution: "250"},
+                {donorName: "Christa B. Lawrence", donorContribution: "1000"},
+                {donorName: "Debra Messing", donorContribution: "1000"},
+                {donorName: "Heidi Tappis", donorContribution: "500"},
+                {donorName: "Josh Brolin", donorContribution: "1000"},
+                {donorName: "Kristen Vogelsong", donorContribution: "1000"},
+                {donorName: "Robert Maschio", donorContribution: "100"}
+            ]
+        }, {
+        candidate: "Eric Garcetti",
+            donors: [
+                {donorName: "Alicia Mitchell", donorContribution: "1300"},
+                {donorName: "Aliza Lesser", donorContribution: "180"},
+                {donorName: "Amy Aquino", donorContribution: "1000"},
+                {donorName: "Amy Poehler", donorContribution: "1300"},
+                {donorName: "Amy Smart", donorContribution: "750"},
+                {donorName: "Andres Faucher", donorContribution: "200"},
+                {donorName: "Ben Weber", donorContribution: "500"},
+                {donorName: "Blanca Soto", donorContribution: "100"},
+                {donorName: "Buckley Norris", donorContribution: "1000"},
+                {donorName: "Danay Garcia", donorContribution: "500"},
+                {donorName: "Donna Mills", donorContribution: "100"},
+                {donorName: "Ed Begley Jr.", donorContribution: "250"},
+                {donorName: "Elizabeth Kennedy", donorContribution: "250"},
+                {donorName: "Emily Foy", donorContribution: "1300"},
+                {donorName: "Evan Arnold", donorContribution: "1300"},
+                {donorName: "Faith Salie", donorContribution: "500"},
+                {donorName: "Ginna Carter", donorContribution: "250"},
+                {donorName: "Gregg Henry", donorContribution: "200"},
+                {donorName: "Harry J. Lennix", donorContribution: "1300"},
+                {donorName: "Heather Thomas", donorContribution: "1050"},
+                {donorName: "Hill Harper", donorContribution: "1000"},
+                {donorName: "Hollie Stenson", donorContribution: "100"},
+                {donorName: "Holly Maples", donorContribution: "100"},
+                {donorName: "Ian Roberts", donorContribution: "1300"},
+                {donorName: "J.K. Simmons", donorContribution: "1000"},
+                {donorName: "Jake Gyllenhaal", donorContribution: "1000"},
+                {donorName: "James Denton", donorContribution: "1000"},
+                {donorName: "Jamie Lee Curtis Guest", donorContribution: "500"},
+                {donorName: "Jon Kean", donorContribution: "1000"},
+                {donorName: "Jonathan Del Arco", donorContribution: "250"},
+                {donorName: "June Sattler", donorContribution: "100"},
+                {donorName: "Kate Danson", donorContribution: "500"},
+                {donorName: "Kimberly Selby", donorContribution: "1300"},
+                {donorName: "Kristina Stonebreaker", donorContribution: "250"},
+                {donorName: "Laura Giacomo", donorContribution: "250"},
+                {donorName: "Lawrence Clarke", donorContribution: "500"},
+                {donorName: "Lena Georgas", donorContribution: "300"},
+                {donorName: "Lenore Douglas", donorContribution: "500"},
+                {donorName: "Leo Marks", donorContribution: "200"},
+                {donorName: "Leonora Gershman", donorContribution: "100"},
+                {donorName: "Lisa Brenner", donorContribution: "500"},
+                {donorName: "Lisa Edelstein", donorContribution: "1300"},
+                {donorName: "Lloyd Gordon", donorContribution: "100"},
+                {donorName: "Marg Helgenberger", donorContribution: "500"},
+                {donorName: "Margaret Priest", donorContribution: "100"},
+                {donorName: "Marie Georgas", donorContribution: "250"},
+                {donorName: "Martha Hackett", donorContribution: "500"},
+                {donorName: "Mary McDonnell", donorContribution: "250"},
+                {donorName: "Matthew Carmony", donorContribution: "250"},
+                {donorName: "Matthew Walsh", donorContribution: "1300"},
+                {donorName: "Meera Narasimhan", donorContribution: "100"},
+                {donorName: "Michael O'Malley", donorContribution: "1000"},
+                {donorName: "Monica H. Rosenthal", donorContribution: "1300"},
+                {donorName: "Phillip Keene", donorContribution: "1000"},
+                {donorName: "Rachel Nichols", donorContribution: "100"},
+                {donorName: "Robert Gossett", donorContribution: "1000"},
+                {donorName: "Russell Steinberg", donorContribution: "500"},
+                {donorName: "Sal Iacono", donorContribution: "400"},
+                {donorName: "Salma Hayek", donorContribution: "1000"},
+                {donorName: "Sarah Chalke", donorContribution: "500"},
+                {donorName: "Steven Klein", donorContribution: "150"},
+                {donorName: "Talulah Riley", donorContribution: "500"},
+                {donorName: "Tanya Mcqueen Forman", donorContribution: "1300"},
+                {donorName: "Tiffany Sweet", donorContribution: "150"},
+                {donorName: "Timothy Olyphant", donorContribution: "1300"},
+                {donorName: "Tony Denison", donorContribution: "1300"},
+                {donorName: "Vincent Kartheiser", donorContribution: "250"},
+                {donorName: "William Brochtrup", donorContribution: "100"},
+                {donorName: "Yasmine Johnson", donorContribution: "1000"}
+            ]
+        }, {
+        candidate: "Wendy Greuel",
+            donors: [
+                {donorName: "Alan Mandell", donorContribution: "1300"},
+                {donorName: "Alexandra McLeod", donorContribution: "1300"},
+                {donorName: "Ann Hall", donorContribution: "150"},
+                {donorName: "Ann Lopez", donorContribution: "1000"},
+                {donorName: "Barry Karas", donorContribution: "100"},
+                {donorName: "Brandon Routh", donorContribution: "500"},
+                {donorName: "Ed Begley Jr.", donorContribution: "500"},
+                {donorName: "Eva Longoria", donorContribution: "1300"},
+                {donorName: "Gabriel A. Gonzales", donorContribution: "1300"},
+                {donorName: "Jill Kenny", donorContribution: "250"},
+                {donorName: "Jillian Bartels", donorContribution: "200"},
+                {donorName: "Jo Ciampafarrell", donorContribution: "500"},
+                {donorName: "Julie Kagan", donorContribution: "1000"},
+                {donorName: "Kate Capshaw", donorContribution: "1300"},
+                {donorName: "Kevin Kendrick", donorContribution: "100"},
+                {donorName: "Leonard Nimoy", donorContribution: "500"},
+                {donorName: "Marg Helgenberger", donorContribution: "100"},
+                {donorName: "Mariska Hargitay", donorContribution: "1000"},
+                {donorName: "Michael Sutton", donorContribution: "500"},
+                {donorName: "Monica Rosenthal", donorContribution: "1300"},
+                {donorName: "Nancy Stephens", donorContribution: "500"},
+                {donorName: "Nancy Travis", donorContribution: "500"},
+                {donorName: "Penny Peyser", donorContribution: "360"},
+                {donorName: "Rita Wilson", donorContribution: "1300"},
+                {donorName: "Susan Nimoy", donorContribution: "500"},
+                {donorName: "Teddi Cole", donorContribution: "350"},
+                {donorName: "Theresa Davis", donorContribution: "500"},
+                {donorName: "Tobey Maguire", donorContribution: "1300"},
+                {donorName: "Tom Hanks", donorContribution: "1300"},
+                {donorName: "Tom Kenny", donorContribution: "250"}
+            ]
+        }, {
+        candidate: "Kevin James",
+            donors: [
+                {donorName: "Blake Edward Boyd", donorContribution: "1300"},
+                {donorName: "Clint Howard", donorContribution: "400"},
+                {donorName: "Donna Scott", donorContribution: "1300"},
+                {donorName: "Erica Brettler", donorContribution: "250"},
+                {donorName: "Gary Sinise", donorContribution: "250"},
+                {donorName: "Jennifer Massey", donorContribution: "300"},
+                {donorName: "Michelle Stafford", donorContribution: "1000"},
+                {donorName: "Ruth Ann Geoghan", donorContribution: "100"}
+            ]
+        }
+    ]
 };
-// end function
+
+// render flat file data
+function renderTestTemplate(){
+    getTemplateAjax('static-files/templates/data-visuals.handlebars', function(template) {
+        jqueryNoConflict('#data-visuals').html(template(celebrityData));
+    })
+};
--- test/keller/la-mayors-celebs/static-files/scripts/template-scripts.js
+++ test/keller/la-mayors-celebs/static-files/scripts/template-scripts.js
@@ -24,10 +24,6 @@ function getTemplateAjax(path, callback) {
 function renderStaticTemplates(){
     renderKpccHeaderTemplate();
     renderDataDetailsTemplate();
-
-    renderTestTemplate();
-
-    renderDataVisualsTemplate();
     renderDataFooterTemplate();
     renderKpccFooterTemplate();
 };
@@ -40,161 +36,10 @@ function renderKpccHeaderTemplate(){
     })
 };
 
-
-
-var testData = {
-    "objects": [{
-        candidate: "Austin Beutner",
-            donors: [
-                {donorName: "Annabeth Gish", donorContribution: "250"},
-                {donorName: "Christa B. Lawrence", donorContribution: "1000"},
-                {donorName: "Debra Messing", donorContribution: "1000"},
-                {donorName: "Heidi Tappis", donorContribution: "500"},
-                {donorName: "Josh Brolin", donorContribution: "1000"},
-                {donorName: "Kristen Vogelsong", donorContribution: "1000"},
-                {donorName: "Robert Maschio", donorContribution: "100"}
-            ]
-        }, {
-        candidate: "Eric Garcetti",
-            donors: [
-                {donorName: "Alicia Mitchell", donorContribution: "1300"},
-                {donorName: "Aliza Lesser", donorContribution: "180"},
-                {donorName: "Amy Aquino", donorContribution: "1000"},
-                {donorName: "Amy Poehler", donorContribution: "1300"},
-                {donorName: "Amy Smart", donorContribution: "750"},
-                {donorName: "Andres Faucher", donorContribution: "200"},
-                {donorName: "Ben Weber", donorContribution: "500"},
-                {donorName: "Blanca Soto", donorContribution: "100"},
-                {donorName: "Buckley Norris", donorContribution: "1000"},
-                {donorName: "Danay Garcia", donorContribution: "500"},
-                {donorName: "Donna Mills", donorContribution: "100"},
-                {donorName: "Ed Begley Jr.", donorContribution: "250"},
-                {donorName: "Elizabeth Kennedy", donorContribution: "250"},
-                {donorName: "Emily Foy", donorContribution: "1300"},
-                {donorName: "Evan Arnold", donorContribution: "1300"},
-                {donorName: "Faith Salie", donorContribution: "500"},
-                {donorName: "Ginna Carter", donorContribution: "250"},
-                {donorName: "Gregg Henry", donorContribution: "200"},
-                {donorName: "Harry J. Lennix", donorContribution: "1300"},
-                {donorName: "Heather Thomas", donorContribution: "1050"},
-                {donorName: "Hill Harper", donorContribution: "1000"},
-                {donorName: "Hollie Stenson", donorContribution: "100"},
-                {donorName: "Holly Maples", donorContribution: "100"},
-                {donorName: "Ian Roberts", donorContribution: "1300"},
-                {donorName: "J.K. Simmons", donorContribution: "1000"},
-                {donorName: "Jake Gyllenhaal", donorContribution: "1000"},
-                {donorName: "James Denton", donorContribution: "1000"},
-                {donorName: "Jamie Lee Curtis Guest", donorContribution: "500"},
-                {donorName: "Jon Kean", donorContribution: "1000"},
-                {donorName: "Jonathan Del Arco", donorContribution: "250"},
-                {donorName: "June Sattler", donorContribution: "100"},
-                {donorName: "Kate Danson", donorContribution: "500"},
-                {donorName: "Kimberly Selby", donorContribution: "1300"},
-                {donorName: "Kristina Stonebreaker", donorContribution: "250"},
-                {donorName: "Laura Giacomo", donorContribution: "250"},
-                {donorName: "Lawrence Clarke", donorContribution: "500"},
-                {donorName: "Lena Georgas", donorContribution: "300"},
-                {donorName: "Lenore Douglas", donorContribution: "500"},
-                {donorName: "Leo Marks", donorContribution: "200"},
-                {donorName: "Leonora Gershman", donorContribution: "100"},
-                {donorName: "Lisa Brenner", donorContribution: "500"},
-                {donorName: "Lisa Edelstein", donorContribution: "1300"},
-                {donorName: "Lloyd Gordon", donorContribution: "100"},
-                {donorName: "Marg Helgenberger", donorContribution: "500"},
-                {donorName: "Margaret Priest", donorContribution: "100"},
-                {donorName: "Marie Georgas", donorContribution: "250"},
-                {donorName: "Martha Hackett", donorContribution: "500"},
-                {donorName: "Mary Mcdonnell", donorContribution: "250"},
-                {donorName: "Matthew Carmony", donorContribution: "250"},
-                {donorName: "Matthew Walsh", donorContribution: "1300"},
-                {donorName: "Meera Narasimhan", donorContribution: "100"},
-                {donorName: "Michael O'malley", donorContribution: "1000"},
-                {donorName: "Monica H. Rosenthal", donorContribution: "1300"},
-                {donorName: "Phillip Keene", donorContribution: "1000"},
-                {donorName: "Rachel Nichols", donorContribution: "100"},
-                {donorName: "Robert Gossett", donorContribution: "1000"},
-                {donorName: "Russell Steinberg", donorContribution: "500"},
-                {donorName: "Sal Iacono", donorContribution: "400"},
-                {donorName: "Salma Hayek", donorContribution: "1000"},
-                {donorName: "Sarah Chalke", donorContribution: "500"},
-                {donorName: "Steven Klein", donorContribution: "150"},
-                {donorName: "Talulah Riley", donorContribution: "500"},
-                {donorName: "Tanya Mcqueen Forman", donorContribution: "1300"},
-                {donorName: "Tiffany Sweet", donorContribution: "150"},
-                {donorName: "Timothy Olyphant", donorContribution: "1300"},
-                {donorName: "Tony Denison", donorContribution: "1300"},
-                {donorName: "Vincent Kartheiser", donorContribution: "250"},
-                {donorName: "William Brochtrup", donorContribution: "100"},
-                {donorName: "Yasmine Johnson", donorContribution: "1000"}
-            ]
-        }, {
-        candidate: "Wendy Greuel",
-            donors: [
-                {donorName: "Alan Mandell", donorContribution: "1300"},
-                {donorName: "Alexandra Mcleod", donorContribution: "1300"},
-                {donorName: "Ann Hall", donorContribution: "150"},
-                {donorName: "Ann Lopez", donorContribution: "1000"},
-                {donorName: "Barry Karas", donorContribution: "100"},
-                {donorName: "Brandon Routh", donorContribution: "500"},
-                {donorName: "Edward J. Begley", donorContribution: "500"},
-                {donorName: "Eva Longoria", donorContribution: "1300"},
-                {donorName: "Gabriel A. Gonzales", donorContribution: "1300"},
-                {donorName: "Jill Kenny", donorContribution: "250"},
-                {donorName: "Jillian Bartels", donorContribution: "200"},
-                {donorName: "Jo Ciampafarrell", donorContribution: "500"},
-                {donorName: "Julie Kagan", donorContribution: "1000"},
-                {donorName: "Kate Capshaw", donorContribution: "1300"},
-                {donorName: "Kevin Kendrick", donorContribution: "100"},
-                {donorName: "Leonard Nimoy", donorContribution: "500"},
-                {donorName: "Marg Helgenberger", donorContribution: "100"},
-                {donorName: "Mariska Hargitay", donorContribution: "1000"},
-                {donorName: "Michael Sutton", donorContribution: "500"},
-                {donorName: "Monica Rosenthal", donorContribution: "1300"},
-                {donorName: "Nancy Stephens", donorContribution: "500"},
-                {donorName: "Nancy Travis", donorContribution: "500"},
-                {donorName: "Penny Peyser", donorContribution: "360"},
-                {donorName: "Rita Wilson", donorContribution: "1300"},
-                {donorName: "Susan Nimoy", donorContribution: "500"},
-                {donorName: "Teddi Cole", donorContribution: "350"},
-                {donorName: "Theresa Davis", donorContribution: "500"},
-                {donorName: "Tobey Maguire", donorContribution: "1300"},
-                {donorName: "Tom Hanks", donorContribution: "1300"},
-                {donorName: "Tom Kenny", donorContribution: "250"}
-            ]
-        }, {
-        candidate: "Kevin James",
-            donors: [
-                {donorName: "Blake Edward Boyd", donorContribution: "1300"},
-                {donorName: "Clint Howard", donorContribution: "400"},
-                {donorName: "Donna Scott", donorContribution: "1300"},
-                {donorName: "Erica Brettler", donorContribution: "250"},
-                {donorName: "Gary Sinise", donorContribution: "250"},
-                {donorName: "Jennifer Massey", donorContribution: "300"},
-                {donorName: "Michelle Stafford", donorContribution: "1000"},
-                {donorName: "Ruth Ann Geoghan", donorContribution: "100"}
-            ]
-        }
-    ]
-};
-
-// create data details template
-function renderTestTemplate(){
-    getTemplateAjax('static-files/templates/test.handlebars', function(template) {
-        jqueryNoConflict('#handlebars-test').html(template(testData));
-    })
-};
-
 // create data details template
 function renderDataDetailsTemplate(){
     getTemplateAjax('static-files/templates/data-details.handlebars', function(template) {
         jqueryNoConflict('#data-details').html(template());
-    })
-};
-
-// create data details template
-function renderDataVisualsTemplate(){
-    getTemplateAjax('static-files/templates/data-visuals.handlebars', function(template) {
-        jqueryNoConflict('#data-visuals').html(template());
     })
 };
 
