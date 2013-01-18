6d581bd38249d4975730a1e743c92d50400b2b16
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-18 08:51:19 -0800

    adds readme file to directories

---
+++ test/keller/executive-actions-on-firearms/static-files/scripts/data-script.js
@@ -0,0 +1,53 @@
+var jqueryNoConflict = jQuery;
+
+//begin main function
+jqueryNoConflict(document).ready(function(){
+    retriveData();
+});
+//end main function
+
+// grab data
+function retriveData(data) {
+    var dataSource = 'static-files/data/23_executive_actions_gun_violence.json';
+    jqueryNoConflict.getJSON(dataSource, renderDataVisualsTemplate);
+};
+
+// create projects content template
+function renderDataVisualsTemplate(data){
+    getTemplateAjax('static-files/templates/data-visuals.handlebars', function(template) {
+        handlebarsDebugHelper();
+        jqueryNoConflict('#data-visuals').html(template(data));
+    })
+};
+
+// render handlebars templates via ajax
+function getTemplateAjax(path, callback) {
+    var source, template;
+    jqueryNoConflict.ajax({
+        url: path,
+        success: function (data) {
+            source = data;
+            template = Handlebars.compile(source);
+            if (callback) callback(template);
+        }
+    });
+}
+//end
+
+// add handlebars debugger
+function handlebarsDebugHelper(){
+    Handlebars.registerHelper("debug", function(optionalValue) {
+        console.log("Current Context");
+        console.log("====================");
+        console.log(this);
+    });
+};
+// end
+
+// embed function
+function embedBox() {
+    var embed_url = 'http://projects.scpr.org/static/maps/flu-clinics/iframe.html';
+
+    jAlert('<strong>To embed this visualization your blog or site, just copy this code:<br></strong>&lt;iframe src=\"'+ embed_url +'\" width=\"540px\" height=\"600px\" style=\"margin: 0 auto;\" scrolling=\"no\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
+}
+// end
