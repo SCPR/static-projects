4cecf7ee8265d167f3f3f4e4455a007e9aa9d0f9
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-21 13:07:12 -0800

    adds sharing template

--- test/keller/executive-actions-on-firearms/static-files/scripts/template-scripts.js
+++ test/keller/executive-actions-on-firearms/static-files/scripts/template-scripts.js
@@ -30,6 +30,7 @@ function renderHandlebarsTemplate(withTemplate,inElement,withData){
 function renderStaticTemplates(){
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '#kpcc-header');
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '#kpcc-footer');
+    renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
     renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
     renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
 };
