0e2c056d3a05c4dc4bb576856c10ee9819a0b507
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-21 15:17:40 -0800

    updates California schools offering bilingual immersion programs with static header/footer

--- maps/bilingual-immersion-programs/static-files/scripts/template-scripts.js
+++ maps/bilingual-immersion-programs/static-files/scripts/template-scripts.js
@@ -1,4 +1,5 @@
 var jqueryNoConflict = jQuery;
+var proxyPrefix = 'http://projects.scpr.org/static/static-files/templates/';
 
 // begin main function
 jqueryNoConflict(document).ready(function() {
@@ -18,7 +19,7 @@ function getTemplateAjax(path, callback) {
     });
 };
 
-// render handlebars template function
+// function to compile handlebars template
 function renderHandlebarsTemplate(withTemplate,inElement,withData){
     getTemplateAjax(withTemplate, function(template) {
         jqueryNoConflict(inElement).html(template(withData));
@@ -27,12 +28,11 @@ function renderHandlebarsTemplate(withTemplate,inElement,withData){
 
 // render all the templates
 function renderStaticTemplates(){
-    renderHandlebarsTemplate('static-files/templates/kpcc-header.handlebars', '#kpcc-header');
-    renderHandlebarsTemplate('static-files/templates/kpcc-pencil-ad.handlebars', '#kpcc-pencil-ad');
+    renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '#kpcc-header');
+    renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '#kpcc-footer');
+    renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
     renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
     renderHandlebarsTemplate('static-files/templates/content-explainer.handlebars', '#content-explainer');
     renderHandlebarsTemplate('static-files/templates/content-action-bar.handlebars', '#content-action-bar');
-    //renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals');
     renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
-    renderHandlebarsTemplate('static-files/templates/kpcc-footer.handlebars', '#kpcc-footer');
 };
