a0aab3f676372fe6dc1dcce4b06944d19654715b
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-24 09:21:36 -0800

    updates word frequency json

--- test/keller/state-of-state/static-files/scripts/template-scripts.js
+++ test/keller/state-of-state/static-files/scripts/template-scripts.js
@@ -32,12 +32,12 @@ function renderStaticTemplates(){
     //renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '#kpcc-header');
     //renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '#kpcc-footer');
 
-    //renderHandlebarsTemplate('static-files/templates/kpcc-header.handlebars', '#kpcc-header');
-    //renderHandlebarsTemplate('static-files/templates/kpcc-footer.handlebars', '#kpcc-footer');
-    //renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
-    //renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
+    renderHandlebarsTemplate('static-files/templates/kpcc-header.handlebars', '#kpcc-header');
+    renderHandlebarsTemplate('static-files/templates/kpcc-footer.handlebars', '#kpcc-footer');
+    renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
+    renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
     renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals');
-    //renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
+    renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
     renderHandlebarsTemplate('static-files/templates/content-explainer.handlebars', '#content-explainer');
 };
 // end
