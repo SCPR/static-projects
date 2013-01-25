9eb6351697dd8896a82747a95c9814e6f038e7be
Chris Keller <christopherlawrencekeller@gmail.com>
2013-01-24 23:35:31 -0800

    adds revised/overhauled bilingual baby map

---
+++ test/keller/revised-bilingual/static-files/scripts/data-script.js
@@ -0,0 +1,74 @@
+var jqueryNoConflict = jQuery;
+
+jqueryNoConflict(document).ready(function() {
+    google.maps.event.addDomListener(window, 'load', buildMapDisplay);
+});
+
+function buildMapDisplay(){
+
+    /*
+        map library and search filtering from Derek Eder's Searchable Map Template
+        find out more here: https://github.com/derekeder/FusionTable-Map-Template
+    */
+
+    MapsLib.initialize();
+    jqueryNoConflict("#search_address").geocomplete();
+
+    jqueryNoConflict(':checkbox').click(function(){
+        MapsLib.doSearch();
+    });
+
+    jqueryNoConflict(':radio').click(function(){
+        MapsLib.doSearch();
+    });
+
+    jqueryNoConflict('#search_radius').change(function(){
+        MapsLib.doSearch();
+    });
+
+    jqueryNoConflict('#search').click(function(){
+        MapsLib.doSearch();
+    });
+
+    jqueryNoConflict('#find_me').click(function(){
+        MapsLib.findMe();
+        return false;
+    });
+
+    jqueryNoConflict('#reset').click(function(){
+        jqueryNoConflict.address.parameter('address','');
+        MapsLib.initialize();
+        showDisplayDetails();
+        return false;
+    });
+
+    jqueryNoConflict(":text").keydown(function(e){
+        var key =  e.keyCode ? e.keyCode : e.which;
+        if(key == 13) {
+            jqueryNoConflict('#search').click();
+            return false;
+        }
+    });
+};
+
+// begin remove article text
+function focusOnDisplayDetails(){
+    jqueryNoConflict('#content-article-text').fadeOut('fast');
+    jqueryNoConflict('#about-this-project').removeClass('hidden');
+};
+
+// begin show article text
+function showDisplayDetails(){
+    jqueryNoConflict('#content-article-text').fadeIn('fast');
+    jqueryNoConflict('#content-display').empty();
+    jqueryNoConflict('#about-this-project').addClass('hidden');
+};
+
+// create the bootrap modal
+function clickModal(){
+    jqueryNoConflict('#popupContent').modal('show');
+};
+
+function closeDialog () {
+	jqueryNoConflict('#popupContent').modal('hide');
+};
---
+++ test/keller/revised-bilingual/static-files/scripts/template-scripts.js
@@ -0,0 +1,41 @@
+var jqueryNoConflict = jQuery;
+var proxyPrefix = 'http://projects.scpr.org/static/static-files/templates/';
+
+// begin main function
+jqueryNoConflict(document).ready(function() {
+    renderStaticTemplates();
+});
+
+// render handlebars templates via ajax
+function getTemplateAjax(path, callback) {
+    var source, template;
+    jqueryNoConflict.ajax({
+        url: path,
+
+        success: function (data) {
+            source = data;
+            template = Handlebars.compile(source);
+            if (callback) callback(template);
+        }
+    });
+};
+
+// function to compile handlebars template
+function renderHandlebarsTemplate(withTemplate,inElement,withData){
+    getTemplateAjax(withTemplate, function(template) {
+        jqueryNoConflict(inElement).html(template(withData));
+    })
+};
+
+// render all the templates
+function renderStaticTemplates(){
+    renderHandlebarsTemplate('static-files/templates/kpcc-header.handlebars', '#kpcc-header');
+    renderHandlebarsTemplate('static-files/templates/kpcc-footer.handlebars', '#kpcc-footer');
+    renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
+    renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
+    renderHandlebarsTemplate('static-files/templates/content-explainer.handlebars', '#content-explainer');
+    renderHandlebarsTemplate('static-files/templates/content-action-bar.handlebars', '#content-action-bar');
+    renderHandlebarsTemplate('static-files/templates/data-modal.handlebars', '#data-modal');
+    renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
+};
+// end
