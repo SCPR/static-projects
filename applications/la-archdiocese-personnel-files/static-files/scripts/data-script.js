d78f59d437972346df53d0447ca76bf93dd7e39a
chrislkeller <christopherlawrencekeller@gmail.com>
2013-02-06 08:03:36 -0800

    updates embed script

--- applications/la-archdiocese-personnel-files/static-files/scripts/data-script.js
+++ applications/la-archdiocese-personnel-files/static-files/scripts/data-script.js
@@ -50,7 +50,7 @@ function alterDisplayDetails(){
 
 // embed function
 function embedBox() {
-    var embed_url = 'http://localhost:8880/2kpcc/static-projects/applications/la-archdiocese-personnel-files/iframe.html';
+    var embed_url = 'http://projects.scpr.org/static/applications/la-archdiocese-personnel-files/iframe.html';
 
     jAlert('<h4>Embed this on your site or blog</h4>' +
     '<span>Copy the code below and paste to source of your page: <br /> &lt;iframe src=\"'+ embed_url +'\" width=\"540px\" height=\"750px\" style=\"margin: 0 auto;\" scrolling=\"no\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
