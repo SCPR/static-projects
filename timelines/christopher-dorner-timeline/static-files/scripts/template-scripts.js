186c12f3a28024eeed30e3cc372705b26e6b7837
Chris Keller <christopherlawrencekeller@gmail.com>
2013-02-07 19:39:29 -0800

    adjsust iframe embed

--- timelines/christopher-dorner-timeline/static-files/scripts/template-scripts.js
+++ timelines/christopher-dorner-timeline/static-files/scripts/template-scripts.js
@@ -11,7 +11,7 @@ jqueryNoConflict(document).ready(function() {
 function embedBox() {
     var embed_url = 'http://projects.scpr.org/static/timelines/christopher-dorner-timeline/iframe.html';
     jAlert('<h4>Embed this on your site or blog</h4>' +
-    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"600px\" height=\"850px\" style=\"margin: 0 auto;\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
+    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"700px\" height=\"850px\" style=\"margin: 0 auto;\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
 };
 
 // render handlebars templates via ajax
