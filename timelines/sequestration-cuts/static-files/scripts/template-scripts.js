fdf8101257988ee7bbfe4ec4b31749465d20ffbd
chrislkeller <christopherlawrencekeller@gmail.com>
2013-02-28 21:40:26 -0500

    activates embed button, adds intro text

--- timelines/sequestration-cuts/static-files/scripts/template-scripts.js
+++ timelines/sequestration-cuts/static-files/scripts/template-scripts.js
@@ -9,9 +9,9 @@ jqueryNoConflict(document).ready(function() {
 
 // embed function
 function embedBox() {
-    var embed_url = 'http://projects.scpr.org/static/timelines/christopher-dorner-timeline/iframe.html';
+    var embed_url = 'http://projects.scpr.org/static/timelines/sequestration-cuts/iframe.html';
     jAlert('<h4>Embed this on your site or blog</h4>' +
-    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"700px\" height=\"850px\" style=\"margin: 0 auto;\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
+    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"620px\" height=\"820px\" style=\"margin: 0 auto;\" scrolling=\"yes\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
 };
 
 // render handlebars templates via ajax
