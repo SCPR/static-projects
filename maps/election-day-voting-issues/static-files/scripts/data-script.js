ae1eef11d3d00c383a3c72eb1583f7db96bda0ff
Chris Keller <christopherlawrencekeller@gmail.com>
2013-03-04 18:08:05 -0800

    updates size of iframe embed code

--- maps/election-day-voting-issues/static-files/scripts/data-script.js
+++ maps/election-day-voting-issues/static-files/scripts/data-script.js
@@ -93,6 +93,6 @@ function mapIntro(){
 function embedBox() {
     var embed_url = 'http://projects.scpr.org/static/maps/election-day-voting-issues/iframe.html';
     jAlert('<h4>Embed this on your site or blog</h4>' +
-    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"620px\" height=\"820px\" style=\"margin: 0 auto;\" scrolling=\"yes\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
+    '<span>Copy the code below and paste to source of your page: <br /><br /> &lt;iframe src=\"'+ embed_url +'\" width=\"620px\" height=\"820px\" style=\"margin: 0 auto;\" scrolling=\"no\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
 };
 // end
