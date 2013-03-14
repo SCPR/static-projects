8856ba408df9e50609df3471a425a0134450fd5c
Chris Keller <christopherlawrencekeller@gmail.com>
2013-03-14 11:08:45 -0700

    refactors timeline template for flat data plugin

--- timelines/timeline-template/static-files/scripts/timeline-config.js
+++ timelines/timeline-template/static-files/scripts/timeline-config.js
@@ -1,21 +1,15 @@
-var jqueryNoConflict = jQuery;
+$(document).ready(function() {
 
-jqueryNoConflict(document).ready(function() {
-
-    jqueryNoConflict('#timeline-title').html(
-        '<h4 class="kicker">Curated Timeline</h4>' +
-        '<h1>The search for Christopher Dorner</h1>');
-
-    jqueryNoConflict('#data-visuals').verticalTimeline({
+    $('#data-visuals').verticalTimeline({
 
         // spreadsheet key
-        key: 'https://docs.google.com/spreadsheet/pub?key=0An8W63YKWOsxdExVR0JPcENEaUJ4cGtWNXJNYkJ3MWc&output=html',
+        key: 'https://docs.google.com/spreadsheet/pub?key=0ApI9xmBd0kPzdG9CcDR0eUxvV2p0Z1dFbjVpZnQ2U2c&output=html',
 
         // name of sheet on spreadsheet
-        sheetName: 'breaking_display_sheet',
+        sheetName: 'final',
 
         // newest or oldest
-        defaultDirection: 'newest',
+        defaultDirection: 'oldest',
 
         // collapsed or expanded
         defaultExpansion: 'expanded',
@@ -24,11 +18,11 @@ jqueryNoConflict(document).ready(function() {
         groupFunction: 'groupSegmentByYear',
 
         // adjust timeline width
-        width: '65%'
+        width: '75%'
     });
 
 /*
-    jqueryNoConflict.getJSON('dorner_display_sheet_timeline.json', function(data) {
+    $.getJSON('static-files/data/dorner_display_sheet_timeline.json', function(data) {
         $('#data-visuals').verticalTimeline({
 
             data: data,
