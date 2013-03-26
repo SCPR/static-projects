8a2d97035f2054ea8dceadb81f5f26bd4a3bbc80
Chris Keller <christopherlawrencekeller@gmail.com>
2013-03-26 11:03:44 -0700

    updates doc cloud viewer script

--- static-files/scripts/document-cloud-viewer.js
+++ static-files/scripts/document-cloud-viewer.js
@@ -11,7 +11,7 @@ jqueryNoConflict(document).ready(function() {
         // get the document id from the url, using jquery.url.min.js
         // so from http://s3.amazonaws.com/datadesk/dc.html?doc=29933-rahm-ruling
         // we get '29933-rahm-ruling'
-        var documentcloud_id = $.url.param("doc");
+        var documentcloud_id = jqueryNoConflict.url.param("doc");
 
         // build the documentcloud url
         var documentcloud_url = "http://www.documentcloud.org/documents/" + documentcloud_id + ".js";
@@ -29,15 +29,15 @@ jqueryNoConflict(document).ready(function() {
 
                 // ... get its title, and put it on the page and in the title bar
                 var document_title = viewer.api.getTitle();
-                $('#titletext').text(document_title);
-                $('#maintitle').text(document_title);
+                jqueryNoConflict('#maintitle').text(document_title);
+                jqueryNoConflict('#titletext').text(document_title);
 
                 // ... get its source, and put it in if it exists
                 var document_source = viewer.api.getSource();
                 var document_source_html = "Source: " + document_source
 
                 if(document_source != null) {
-                    $('#document-source').html(document_source_html);
+                    jqueryNoConflict('#document-source').html(document_source_html);
                 }
 
                 // ... show the link to the related article, if it exists
@@ -46,7 +46,7 @@ jqueryNoConflict(document).ready(function() {
                     related_article_url + "'>&laquo; Go to the related story.</a>";
 
                 if(related_article_url != null) {
-                    $('#article-link').html(related_html);
+                    jqueryNoConflict('#article-link').html(related_html);
                 }
             }
         });
