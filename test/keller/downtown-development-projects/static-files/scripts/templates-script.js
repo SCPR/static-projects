695efc43fc1049cd63363dd6ee7bce64911565e7
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-02 12:53:40 -0700

    adjusts template script'

--- test/keller/downtown-development-projects/static-files/scripts/templates-script.js
+++ test/keller/downtown-development-projects/static-files/scripts/templates-script.js
@@ -3,11 +3,7 @@ var proxyPrefix = 'http://projects.scpr.org/static/static-files/templates/';
 
 // begin main function
 jqueryNoConflict(document).ready(function() {
-    renderStaticTemplates();
-});
 
-// render all the templates
-function renderStaticTemplates(){
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '#kpcc-header');
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '#kpcc-footer');
     renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
@@ -20,5 +16,5 @@ function renderStaticTemplates(){
 
     renderHandlebarsTemplate('static-files/templates/content-explainer.handlebars', '#content-explainer');
     renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
-};
-// end
+
+});
