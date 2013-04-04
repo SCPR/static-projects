8fd00e22fc1b482e47fe0dcf22feb1a87d141a15
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-03 17:39:16 -0700

    adds project inventory project to test directory

---
+++ test/keller/project-inventory/static-files/scripts/tabletop-script.js
@@ -0,0 +1,30 @@
+var jqueryNoConflict = jQuery;
+
+// make sure the spreadsheet is published to the web
+var dataSpreadsheet = '0An8W63YKWOsxdC1DbjM0MVFUelFXcUFCY2xZWGtSZGc';
+
+// the sheet being queried
+var dataSheet = 'Presentation_inventory';
+
+// begin main function
+jqueryNoConflict(document).ready(function() {
+
+    Tabletop.init({
+        key: dataSpreadsheet,
+        callback: retriveData,
+        parseNumbers: true,
+        simpleSheet: false,
+        debug: false
+    });
+
+});
+// end
+
+// display data on template
+function retriveData(data, tabletop) {
+    var handlebarsData = {
+        objects: data.Presentation_inventory.elements
+    };
+    renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals', handlebarsData);
+};
+// end
