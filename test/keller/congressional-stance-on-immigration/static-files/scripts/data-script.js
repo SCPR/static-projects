ab2371d5169a70c36f3d1177df979426c7470cfe
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-18 10:34:40 -0700

    adds Immigration: Where does your US congressman stand?

---
+++ test/keller/congressional-stance-on-immigration/static-files/scripts/data-script.js
@@ -0,0 +1,24 @@
+var jqueryNoConflict = jQuery;
+
+// begin main function
+jqueryNoConflict(document).ready(function() {
+    retriveData();
+});
+
+// grab data
+function retriveData() {
+    var dataSource = 'static-files/data/ca_congressional_delegation_immigration-handlebars.json';
+    jqueryNoConflict.getJSON(dataSource, processDataForTemplate);
+};
+
+// render data visuals template
+function processDataForTemplate(data){
+    renderHandlebarsTemplate('static-files/templates/content-action-bar.handlebars', '#content-action-bar', data);
+    renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals', data);
+};
+
+// scroll into view function
+function scrollToRep(){
+    var congressionalMember = jqueryNoConflict('#search-congressional-delegation').val();
+    jqueryNoConflict.scrollTo(congressionalMember)
+};
