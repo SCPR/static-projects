7a5d9b93ecd92cbdc4b9deeba2ce6b5445309b07
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-24 11:56:07 -0700

    updates citywide exit polling project

--- interactives/citywide-exit-polls/static-files/scripts/data-script.js
+++ interactives/citywide-exit-polls/static-files/scripts/data-script.js
@@ -1,15 +1,3 @@
-/*
-    docs on highcharts click events
-    http://api.highcharts.com/highcharts#plotOptions.line.events.legendItemClick
-
-TO DO
-* The narrative could use some work, as could clear instructions to the user... For instance, click on the chart legend to filter the chart and the table, mouse over, etc.
-* I'd like to do more to integrate the chart and the table... rather than highlight the entire row, the specific number perhaps.
-* Start over with a new 2013 chart? Seems like it should track the past year or so.
-* Clearer instructions, particularly for using the legend are needed.
-* Slide toggle through the dates would be cool.
-*/
-
     var jqueryNoConflict = jQuery;
 
     // make sure the spreadsheet is published to the web
@@ -18,139 +6,147 @@ TO DO
     // the sheet being queried
     var dataSheet = 'mayoral_vote_by_race';
 
-    // container arrays
-    var allCityData = [];
-    var arraysOfCityData = [];
-
-    // chart options
-    var chart;
-    var chartCategories = ['Percentofvote', 'Latino', 'White', 'Black', 'Asian', 'Other'];
-
     // pull data from spreadsheet onload
     jqueryNoConflict(document).ready(function(){
         Tabletop.init({
             key: dataSpreadsheet,
-            callback: showInfo,
+            callback: processData,
             simpleSheet: false,
             debug: false
         });
 
-        drawHighchart();
-
     });
 
     // display data from tabletop
-    function showInfo(data, tabletop){
+    function processData(data, tabletop){
+        var arrayOfCandidates = [];
+        var candidateObject = {};
 
         // pulls data from the spreadsheet
         jqueryNoConflict.each(tabletop.sheets(dataSheet).all(), function(i, record) {
+            candidateObject = {
+                //'candidate_name': record.candidate,
+                'Latinos': parseFloat(record.latino),
+                'Whites': parseFloat(record.white),
+                'Blacks': parseFloat(record.black),
+                'Asians': parseFloat(record.asian),
+                'Other': parseFloat(record.other)
+            };
 
-            var candidate = record.candidate;
-            var asian = parseInt(record.asian);
-            var black = parseInt(record.black)
-            var latino = parseInt(record.latino)
-            var other = parseInt(record.other)
-            var percentofvote = parseInt(record.percentofvote)
-            var rowNumber = record.rowNumber
-            var white = parseInt(record.white)
-
-            // logs data type of variable
-           // console.log(typeof(asian));
-           
-            // build array from each row of spreadsheet
-            allCityData = [candidate, asian, black, latino, other, white, percentofvote];
-
-            // logs array of data each time through the loop
-            console.log(allCityData);
+            var targetData = convertObjectKeyValuesTo(candidateObject);
 
             // push each array to an array
-            arraysOfCityData.push(allCityData);
+            arrayOfCandidates.push(targetData);
 
         });
 
-        // shows array of arrays after loop
-        console.log(arraysOfCityData);
-
-
-        var finalMonthlyJobs = {
-            name: 'Final',
+        var garcettiNumbers = {
+            name: 'Eric Garcetti',
             color: '#005873',
             type: 'pie',
-            data: arraysOfCityData[2]
+            data: arrayOfCandidates[0]
         };
 
-        // add respective objects to highcharts series
-        //chart.addSeries(initialMonthlyJobs);
+        var greuelNumbers = {
+            name: 'Wendy Greuel',
+            color: '#005873',
+            type: 'pie',
+            data: arrayOfCandidates[1]
+        };
+
+        var jamesNumbers = {
+            name: 'Kevin James',
+            color: '#005873',
+            type: 'pie',
+            data: arrayOfCandidates[2]
+        };
+
+        var perryNumbers = {
+            name: 'Jan Perry',
+            color: '#005873',
+            type: 'pie',
+            data: arrayOfCandidates[3]
+        };
+
+        var charts = [];
+
+        charts.push(new Highcharts.Chart(
+            getChartConfig('data-chart-one', 'Eric Garcetti', 'March 5 primary breakdown by race', garcettiNumbers)
+        ))
+
+        charts.push(new Highcharts.Chart(
+            getChartConfig('data-chart-two', 'Wendy Greuel', 'March 5  primary breakdown by race', greuelNumbers)
+        ))
+
+        charts.push(new Highcharts.Chart(
+            getChartConfig('data-chart-three', 'Jan Perry', 'March 5 primary breakdown by race', perryNumbers)
+        ))
+
+        charts.push(new Highcharts.Chart(
+            getChartConfig('data-chart-four', 'Kevin James', 'March 5 primary breakdown by race', jamesNumbers)
+        ))
 
     };
 
-    // draw the chart
-    function drawHighchart(){
+    // convert object to an array of arrays
+    function convertObjectKeyValuesTo(assocArray){
+        var arrayOfIndividuals = [];
+        for (var key in assocArray) {
+            if (assocArray.hasOwnProperty(key)) {
+              arrayOfIndividuals.push([key, assocArray[key]]);
+            }
+        }
+        return arrayOfIndividuals;
+    }
 
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'data-chart',
-                zoomType: 'xy'
-            },
+    function getChartConfig(renderId, titleText, subtitleText, data) {
 
-            title: {
-                text: 'Mayoral Elections by Race'
-            },
+        var config = {};
 
-            subtitle: {
-                text: 'Data Source: Loyala Marymount University'
-            },
+        config.chart = {
+            renderTo: renderId,
+            zoomType: 'xy',
+            backgroundColor: '#eaeaea',
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        };
 
-            xAxis: [{
-                categories: chartCategories
-            }],
+        config.title = {
+            text: titleText
+        };
 
-            yAxis: [{
+        config.subtitle = {
+            text: subtitleText
+        };
 
-                // primary axis
-                labels: {
-                   formatter: function() {
-                        return this.value +'%';
-                    },
-                    style: {
-                        color: '#2B2B2B'
+        config.tooltip = {
+            formatter: function(){
+                return ''+ this.series.name +': '+ Highcharts.numberFormat(this.y, 2, '.');
+            }
+        };
+
+        config.plotOptions = {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    formatter: function() {
+                        return '<strong>' + this.point.name + '</strong>: ' +
+                        Highcharts.numberFormat(this.y, 2, '.');
                     }
-                },
-                title: {
-                    text: 'Candidate',
-                    style: {
-                        color: '#2B2B2B'
-                    }
-                },
-
-                tickPixelInterval: 5
-
-            }],
-
-            tooltip: {
-                formatter: function(){
-                    return ''+ this.series.name +': '+ Highcharts.numberFormat(this.y, 0, ',');
                 }
-            },
+            }
+        };
 
-            legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'bottom',
-                //x: 0,
-                y: 0,
-                floating: false,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            },
+        config.credits = {
+            enabled: true,
+            text: 'KPCC'
+        };
 
-            credits: {
-                enabled: false
-            },
-
-            series: []
-        });
-
+        config.series = [data];
+        return config;
     };
-    //end function
