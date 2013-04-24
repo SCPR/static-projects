b477c224d821cb68a13884f07bd44644e42392c7
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-24 13:02:49 -0700

    moves dear mayor to three across layout

--- interactives/la-mayors-race/dear-mayor/static-files/scripts/data-script.js
+++ interactives/la-mayors-race/dear-mayor/static-files/scripts/data-script.js
@@ -3,6 +3,7 @@ var jqueryNoConflict = jQuery;
 // begin main function
 jqueryNoConflict(document).ready(function() {
     retriveData();
+    handlebarsLooperHelper();
 });
 
 // grab data
@@ -72,10 +73,6 @@ function evaluateTopics(data){
     jqueryNoConflict('#transportation-results').html(' (' + numOfTransportation + ') ');
 };
 
-
-
-
-
 // begin checkbox toggle
 function checkBoxToggle(){
     jqueryNoConflict('#data-legend-items :checkbox').click(function() {
@@ -85,3 +82,25 @@ function checkBoxToggle(){
         });
     });
 }
+
+
+function handlebarsLooperHelper(){
+// handlebars help function to set every nth element
+    Handlebars.registerHelper('everyNth', function(context, every, options) {
+        var fn = options.fn, inverse = options.inverse;
+        var ret = "";
+        if(context && context.length > 0) {
+            for(var i=0, j=context.length; i<j; i++) {
+                var modZero = i % every === 0;
+                ret = ret + fn(_.extend({}, context[i], {
+                    isModZero: modZero,
+                    isModZeroNotFirst: modZero && i > 0,
+                    isLast: i === context.length - 1
+                }));
+            }
+        } else {
+            ret = inverse(this);
+        }
+        return ret;
+    });
+};
