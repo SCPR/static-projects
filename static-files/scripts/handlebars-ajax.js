e9529fbe4ead3353c2c40c2c535272a53a9c2b7c
Chris Keller <christopherlawrencekeller@gmail.com>
2013-05-01 11:18:01 -0700

    adds helpers to handlebars-ajax plugin

--- static-files/scripts/handlebars-ajax.js
+++ static-files/scripts/handlebars-ajax.js
@@ -21,6 +21,14 @@ function renderHandlebarsTemplate(withTemplate,inElement,withData){
     })
 };
 
+// handlebars help function to set every nth element
+function runRegisteredHandlebarsHelpers(){
+    handlebarsDebugHelper();
+    handlebarsreplaceSpacesHelper();
+    handlebarsLooperHelper();
+    handlebarsFormatDateHelper();
+}
+
 // add handlebars debugger
 function handlebarsDebugHelper(){
     Handlebars.registerHelper("debug", function(optionalValue) {
@@ -28,9 +36,51 @@ function handlebarsDebugHelper(){
         console.log("====================");
         console.log(this);
     });
-};
+}
 
-// handlebars help function to set every nth element
+// build helper function
+function handlebarsreplaceSpacesHelper() {
+    Handlebars.registerHelper('replaceSpaces', function(context) {
+        var replacedContext = context.split(' ').join('-');
+        return replacedContext;
+    });
+}
+
 function handlebarsLooperHelper(){
+    // handlebars help function to set every nth element
+    Handlebars.registerHelper('everyNth', function(context, every, options) {
+        var fn = options.fn, inverse = options.inverse;
+        var ret = "";
+        if(context && context.length > 0) {
+            for(var i=0, j=context.length; i<j; i++) {
+                var modZero = i % every === 0;
+                ret = ret + fn(_.extend({}, context[i], {
+                    isModZero: modZero,
+                    isModZeroNotFirst: modZero && i > 0,
+                    isLast: i === context.length - 1
+                }));
+            }
+        } else {
+            ret = inverse(this);
+        }
+        return ret;
+    });
+}
 
-};
+// format date/time
+function takeTime(dateInput) {
+    var dateFormat = 'MMM. D, h:mm a';
+    var dateOutput = moment(dateInput).format(dateFormat);
+    return dateOutput;
+}
+
+// begin dateFormatFunction
+function handlebarsFormatDateHelper(){
+    Handlebars.registerHelper('dateFormat', function(context, block) {
+        if (window.moment) {
+            return takeTime(context);
+        }else{
+            return context;
+        };
+    });
+}
