db40c689599815a95584bf07bf194c8251425f72
Eric Zassenhaus <zassenhaus@gmail.com>
2013-05-01 22:23:54 -0700

    minor fixes to text, mouseover

--- test/Ericstestfolder/test_pension_contribution/static-files/scripts/data-script.js
+++ test/Ericstestfolder/test_pension_contribution/static-files/scripts/data-script.js
@@ -1,15 +1,3 @@
-/*
-    docs on highcharts click events
-    http://api.highcharts.com/highcharts#plotOptions.line.events.legendItemClick
-
-TO DO
-* The narrative could use some work, as could clear instructions to the user... For instance, click on the chart legend to filter the chart and the table, mouse over, etc.
-* I'd like to do more to integrate the chart and the table... rather than highlight the entire row, the specific number perhaps.
-* Start over with a new 2013 chart? Seems like it should track the past year or so.
-* Clearer instructions, particularly for using the legend are needed.
-* Slide toggle through the dates would be cool.
-*/
-
     var jqueryNoConflict = jQuery;
 
     // make sure the spreadsheet is published to the web
@@ -20,7 +8,7 @@ TO DO
 
     // container arrays
     var allContribsData = [];
-    var arraysOfJobsData = [];
+    var arraysOfContribsData = [];
 
     // chart options
     var chart;
@@ -42,199 +30,186 @@ TO DO
     // display data from tabletop
     function showInfo(data, tabletop){
 
+        var allContribsData = [];
+        var arraysOfContribsData = [];
+
+        console.log(data.LACERS_contribs.elements);
+
         // pulls data from the spreadsheet
         jqueryNoConflict.each(tabletop.sheets(dataSheet).all(), function(i, record) {
 
-            // set variables and convert to integers if needed
-            var 2002 = parseInt(record.2002);
-            var 2003 = parseInt(record.2003);
-            var 2004 = parseInt(record.2004);
-            var 2005 = parseInt(record.2005);
-            var 2006 = parseInt(record.2006);
-            var 2007 = parseInt(record.2007);
-            var 2008 = parseInt(record.2008);
-            var 2009 = parseInt(record.2009);
-            var 2010 = parseInt(record.2010);
-            var 2011 = parseInt(record.2011);
-            var 2012 = parseInt(record.2012);
+                   // set variables and convert to integers if needed
+                    var yr2002 = parseInt(record.yr2002);
+                    var yr2003 = parseInt(record.yr2003);
+                    var yr2004 = parseInt(record.yr2004);
+                    var yr2005 = parseInt(record.yr2005);
+                    var yr2006 = parseInt(record.yr2006);
+                    var yr2007 = parseInt(record.yr2007);
+                    var yr2008 = parseInt(record.yr2008);
+                    var yr2009 = parseInt(record.yr2009);
+                    var yr2010 = parseInt(record.yr2010);
+                    var yr2011 = parseInt(record.yr2011);
+                    var yr2012 = parseInt(record.yr2012);
+                    
+                    allContribsData = [yr2002, yr2003, yr2004, yr2005, yr2006, yr2007, yr2008, yr2009, yr2010, yr2011, yr2012];
 
-            // build array from each row of spreadsheet
-            allContribsData = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
-
-            // push each array to an array
-            arraysOfJobsData.push(allContribsData);
+                    // push each array to an array
+                    arraysOfContribsData.push(allContribsData);
 
         });
 
-            // objects for highcharts data series
-           // var initialMonthlyJobs = {
-                var PensionType = {
-                name: 'Initial',
-                color: '#002734',
-                type: 'column',
-                data: arraysOfContribsData[0]
-            };
+                   // objects for highcharts data series
+                   // var initialMonthlyJobs = {
+                        var MemberContribLacers = {
+                        name: 'Member contributions',
+                        color: '#002734',
+                        type: 'spline',
+                        data: arraysOfContribsData[0]
+                    };
 
-/*
-            var revisedMonthlyJobs = {
-                name: 'Revised',
-                color: '#377EB8',
-                type: 'areaspline',
-                data: arraysOfJobsData[1]
-            };
-*/
+    
+                    var CityContribLacers = {
+                        name: 'City contributions',
+                        color: '#377EB8',
+                        type: 'spline',
+                        data: arraysOfContribsData[1]
+                    };
+            
+                           // add respective objects to highcharts series
+                            //chart.addSeries(revisedMonthlyJobs);
+                            chart.addSeries(MemberContribLacers);
+                            chart.addSeries(CityContribLacers);            
+    }
+    
+     // draw the chart
+        function drawHighchart(){
 
-           /* var finalMonthlyJobs = {
-                name: 'Final',
-                color: '#005873',
-                type: 'areaspline',
-                data: arraysOfJobsData[2]
-            };
-
-            var differenceMonthlyJobs = {
-                name: 'Monthly Difference',
-                data: arraysOfJobsData[3]
-            };
-*/
-
-            renderHandlebarsTemplate('static-files/templates/data-table.handlebars', '#data-table', differenceMonthlyJobs);
-
-            // add respective objects to highcharts series
-            //chart.addSeries(revisedMonthlyJobs);
-            chart.addSeries(PensionType);
-            //chart.addSeries(finalMonthlyJobs);
-
-    };
-
-    // draw the chart
-    function drawHighchart(){
-
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'data-chart',
-                zoomType: 'xy'
-            },
-
-            title: {
-                text: 'Pension contributions '
-            },
-
-            subtitle: {
-                text: 'Data Sources: xxx'
-            },
-
-            xAxis: [{
-                categories: chartCategories
-            }],
-
-            yAxis: [{
-
-                // primary axis
-                labels: {
-                   formatter: function() {
-                        return this.value / 1000000 +'M';
-                    },
-                    style: {
-                        color: '#2B2B2B'
-                    }
+            chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'data-chart',
+                    zoomType: 'xy'
                 },
+
                 title: {
-                    text: 'Years',
-                    style: {
-                        color: '#2B2B2B'
+                    text: 'Pension contributions '
+                },
+
+                subtitle: {
+                    text: 'Data Sources: xxx'
+                },
+
+                xAxis: [{
+                    categories: chartCategories
+                }],
+
+                yAxis: [{
+
+                    // primary axis
+                    labels: {
+                       formatter: function() {
+                            return this.value / 1000000 +'M';
+                        },
+                        style: {
+                            color: '#2B2B2B'
+                        }
+                    },
+                    title: {
+                        text: 'Annual pension contributions (in millions)',
+                        style: {
+                            color: '#2B2B2B'
+                        }
+                    },
+
+                    tickPixelInterval: 25
+
+                }],
+
+                tooltip: {
+                    formatter: function(){
+                        return ''+ this.series.name +': '+ Highcharts.numberFormat(this.y, 0, ',');
                     }
                 },
 
-                tickPixelInterval: 25
+                // testing various click event options
+                plotOptions: {
+                    series: {
+                        point: {
+                            events: {
+                                mouseOver: function() {
+                                    var highlightRowColor = String(this.series.color);
+                                    var highlightRowId = String(this.series.name);
+                                    var highlightCellId = String(this.x);
+                                    var selectedRow = 'tr#' + highlightRowId;
+                                    var selectedCell = 'td#' + highlightCellId;
 
-            }],
+                                    jqueryNoConflict(selectedRow).css(
+                                        {'text-decoration': 'underline', 'color': highlightRowColor});
 
-            tooltip: {
-                formatter: function(){
-                    return ''+ this.series.name +': '+ Highcharts.numberFormat(this.y, 0, ',');
-                }
-            },
+                                },
 
-            // testing various click event options
-            plotOptions: {
-                series: {
-                    point: {
+                                mouseOut: function() {
+                                    var highlightRowColor = String(this.series.color);
+                                    var highlightRowId = String(this.series.name);
+                                    var highlightCellId = String(this.x);
+                                    var selectedRow = 'tr#' + highlightRowId;
+                                    var selectedCell = 'td#' + highlightCellId;
+
+                                    jqueryNoConflict(selectedRow).css(
+                                        {'text-decoration': 'none', 'color': 'black'});
+                                }
+                            }
+                        },
+
                         events: {
+
+    /*
                             mouseOver: function() {
-                                var highlightRowColor = String(this.series.color);
-                                var highlightRowId = String(this.series.name);
-                                var highlightCellId = String(this.x);
+                                var highlightRowId = String(this.name);
+                                var highlightRowColor = String(this.color);
                                 var selectedRow = 'tr#' + highlightRowId;
-                                var selectedCell = 'td#' + highlightCellId;
-
                                 jqueryNoConflict(selectedRow).css(
-                                    {'text-decoration': 'underline', 'color': highlightRowColor});
-
+                                        'color', highlightRowColor);
                             },
 
                             mouseOut: function() {
-                                var highlightRowColor = String(this.series.color);
-                                var highlightRowId = String(this.series.name);
-                                var highlightCellId = String(this.x);
+                                var highlightRowId = String(this.name);
+                                var highlightRowColor = String(this.color);
                                 var selectedRow = 'tr#' + highlightRowId;
-                                var selectedCell = 'td#' + highlightCellId;
-
                                 jqueryNoConflict(selectedRow).css(
-                                    {'text-decoration': 'none', 'color': 'black'});
+                                        'color', 'black');
+                            },
+    */
+
+                            legendItemClick: function () {
+                                var highlightRowId = String(this.name);
+                                var selectedRow = 'tr#' + highlightRowId;
+                                jqueryNoConflict(selectedRow).toggle();
+
+                                 //returning false cancels default action
+                                // return false;
                             }
                         }
-                    },
-
-                    events: {
-
-/*
-                        mouseOver: function() {
-                            var highlightRowId = String(this.name);
-                            var highlightRowColor = String(this.color);
-                            var selectedRow = 'tr#' + highlightRowId;
-                            jqueryNoConflict(selectedRow).css(
-                                    'color', highlightRowColor);
-                        },
-
-                        mouseOut: function() {
-                            var highlightRowId = String(this.name);
-                            var highlightRowColor = String(this.color);
-                            var selectedRow = 'tr#' + highlightRowId;
-                            jqueryNoConflict(selectedRow).css(
-                                    'color', 'black');
-                        },
-*/
-
-                        legendItemClick: function () {
-                            var highlightRowId = String(this.name);
-                            var selectedRow = 'tr#' + highlightRowId;
-                            jqueryNoConflict(selectedRow).toggle();
-
-                             //returning false cancels default action
-                            // return false;
-                        }
                     }
-                }
-            },
+                },
 
-            legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'bottom',
-                //x: 0,
-                y: 0,
-                floating: false,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            },
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    //x: 0,
+                    y: 0,
+                    floating: false,
+                    borderWidth: 1,
+                    backgroundColor: '#FFFFFF',
+                    shadow: true
+                },
 
-            credits: {
-                enabled: false
-            },
+                credits: {
+                    enabled: false
+                },
 
-            series: []
-        });
+                series: []
+            });
 
-    };
-    //end function
+        };
+        //end function
