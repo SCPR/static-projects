f8a37996f251ef2df3f9bb04f2ee85010bad6c87
chrislkeller <christopherlawrencekeller@gmail.com>
2013-05-03 00:25:07 -0700

    weekend-traffic-advisory generator showing working preview

--- internal/tools/weekend-traffic-advisories/static-files/scripts/tabletop-script.js
+++ internal/tools/weekend-traffic-advisories/static-files/scripts/tabletop-script.js
@@ -21,54 +21,77 @@
             });
         },
 
-        arrayOfData: [],
-
-
-        incidentsTrafficArray: [{
-            location: "405 S",
-            details: "Starting May 1, construction starts to aid wall 1921 construction at the southbound Skirball on-ramp. There will be intermittent full-ramp closures from X am to Z pm.", "url": "http://sv08data.dot.ca.gov/memos/files/comalert/042913_0.pdf?utm_source=dlvr.it&utm_medium=twitter"},{
-            location: "405 S",
-            details: "Starting May 1, construction starts to aid wall 1921 construction at the southbound Skirball on-ramp. There will be intermittent full-ramp closures from X am to Z pm.", "url": "http://sv08data.dot.ca.gov/memos/files/comalert/042913_0.pdf?utm_source=dlvr.it&utm_medium=twitter"}],
-
+        // first pass at pulling out data and displaying it
         processDataSource: function(data, tabletop){
 
-            console.log(data);
-            var categoryElement;
+            // get unique categories and place them as key to frequency
+            var trafficCategoriesObject = dataConfig.separateKeysFromValues(data);
 
-            jqueryNoConflict.each(data, function(i, record) {
+            // separate the keys from the values and place into array
+            var keys = Object.keys(trafficCategoriesObject);
 
-                categoryElement = record.category;
-                console.log(categoryElement);
+            var incidentArrayHoldingContainer = [];
 
-                var locationElement = '<li><strong>' + record.location + '</strong>:';
-                var detailsElement = record.details + '</li>';
-                dataConfig.arrayOfData.push(locationElement, detailsElement);
+            // begin outer loop
+            for (var x=0; x<keys.length; x++){
 
-            });
+                // begin inner loop
+                for (var i=0; i<data.length; i++){
+                    if (data[i].category === keys[x]){
+                        incidentArrayHoldingContainer.push({location: data[i].location, details: data[i].details});
+                    }
+                }
+                // end inner loop
 
-            var newTest = dataConfig.arrayOfData.join(' ');
-            jqueryNoConflict('#formatted-output').html('<h6>Category Here</h6><ul>' + newTest + '</ul>');
-            jqueryNoConflict('#html-output').text('<h6>Category Here</h6><ul>' + newTest + '</ul>');
-
-
-            dataConfig.buildTrafficObject(categoryElement, dataConfig.incidentsTrafficArray);
-
+                    dataConfig.buildTrafficObject(keys[x], incidentArrayHoldingContainer);
+            }
+            // end outer loop
 
         },
 
+        // count instances of values and set to keys
+        separateKeysFromValues: function(dataSource){
+            var categories = {};
+            var categoryCount;
+            for (var i=0; i<dataSource.length; i++){
+                categoryCount = dataSource[i];
+                categories[categoryCount.category] = (categories[categoryCount.category] || 0) + 1;
+            }
+            return categories;
+        },
 
-
-
+        // builds & processes an object for display
         buildTrafficObject: function(categoryTraffic, incidentsTrafficArray){
-
             var trafficCategoryObject = {
                 category: categoryTraffic,
                 incidents: incidentsTrafficArray
             };
 
-            console.log(trafficCategoryObject);
+            var holdingArrayForDisplayStrings = [];
+
+            jqueryNoConflict.each(trafficCategoryObject.incidents, function(i, record) {
+                var locationElement = '<li><strong>' + record.location + '</strong>:';
+                var detailsElement = record.details + '</li>';
+                holdingArrayForDisplayStrings.push(locationElement, detailsElement);
+            });
+
+            var targetOutput = holdingArrayForDisplayStrings.join(' ');
+            dataConfig.displayTrafficObject(trafficCategoryObject.category, targetOutput);
         },
 
+        // renders the display
+        displayTrafficObject: function(categoryParam, outputParam){
+            var combinedOutputString = '<h6>' + categoryParam + '</h6><ul>' + outputParam + '</ul>';
+            jqueryNoConflict('#formatted-output').append(combinedOutputString);
+            jqueryNoConflict('#html-output').text(combinedOutputString);
+        },
+
+        /// what an incident array looks like for reference
+        incidentsTrafficArray: [{
+            location: "405 S",
+            details: "Starting May 1, construction starts to aid wall 1921 construction at the southbound Skirball on-ramp. There will be intermittent full-ramp closures from X am to Z pm.", "url": "http://sv08data.dot.ca.gov/memos/files/comalert/042913_0.pdf?utm_source=dlvr.it&utm_medium=twitter"},{
+            location: "405 S",
+            details: "Starting May 1, construction starts to aid wall 1921 construction at the southbound Skirball on-ramp. There will be intermittent full-ramp closures from X am to Z pm.", "url": "http://sv08data.dot.ca.gov/memos/files/comalert/042913_0.pdf?utm_source=dlvr.it&utm_medium=twitter"}],
 
     }
     // end data configuration object
