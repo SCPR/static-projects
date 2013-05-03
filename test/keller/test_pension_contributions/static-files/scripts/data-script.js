f8623c523eb63ac8336796884c24b585d7e8441e
Chris Keller <christopherlawrencekeller@gmail.com>
2013-05-03 16:51:51 -0700

    swaps filter buttons for slide filter

--- test/keller/test_pension_contributions/static-files/scripts/data-script.js
+++ test/keller/test_pension_contributions/static-files/scripts/data-script.js
@@ -4,15 +4,20 @@
     // pull data from spreadsheet onload
     jqueryNoConflict(document).ready(function(){
         configData.initializeDataSource();
+        configData.createSliderFilter();
 
+        /* old code
         jqueryNoConflict('a.chart-trigger').click(function (){
             configData.updateChart(this.id);
         });
+        */
 
     });
 
-    // data configuration object
+    // master data configuration object
     var configData = {
+
+        // call data from tabletop
         initializeDataSource: function(){
             Tabletop.init({
                 key: '0AjsyCVrBXivzdFBOeDBOMnRJMmpzc3pHLXhhQnVRQVE',
@@ -23,9 +28,10 @@
             });
         },
 
+        // remove some strings from our data array
         deleteKeyFromObject: function(arrayToClean){
             var cleanedContribsData = [];
-            for (var i=0;i<3;i++){
+            for (var i=0;i<2;i++){
                 delete arrayToClean[i].pensioncontributions;
                 delete arrayToClean[i].rowNumber;
                 cleanedContribsData.push(arrayToClean[i]);
@@ -33,41 +39,45 @@
             return cleanedContribsData;
         },
 
+        // holding object we can access later
         objectChartData: {},
 
+        // take tabletop data and render it to a holding object we can access later
         showInfo: function(data, tabletop){
-            var dwpClean = configData.deleteKeyFromObject(data.dwp_contribs.elements);
+            var wperpClean = configData.deleteKeyFromObject(data.wperp_contribs.elements);
             var lacersClean = configData.deleteKeyFromObject(data.lacers_contribs.elements);
             var laffpClean = configData.deleteKeyFromObject(data.laffp_contribs.elements);
-            configData.objectChartData['dwp_chart'] = dwpClean;
+            configData.objectChartData['wperp_chart'] = wperpClean;
             configData.objectChartData['lacers_chart'] = lacersClean;
             configData.objectChartData['laffp_chart'] = laffpClean;
             configData.writeDefaultChart();
         },
 
+        // render the default chart when the page loads
         writeDefaultChart: function(){
             configData.blueprintChartSeries(
                 'data-chart',
-                'Contributions to DWP',
+                'Contributions to wperp',
                 'Source:',
-                configData.convertObjectToArray(configData.objectChartData.dwp_chart[0]),
-                configData.convertObjectToArray(configData.objectChartData.dwp_chart[1]),
-                configData.convertObjectToArray(configData.objectChartData.dwp_chart[2])
+                configData.convertObjectToArray(configData.objectChartData.wperp_chart[0]),
+                configData.convertObjectToArray(configData.objectChartData.wperp_chart[1]),
+                configData.convertObjectToArray(configData.objectChartData.wperp_chart[2])
             );
 
         },
 
+        /* old code */
+        // update the chart when a button is clicked
+        /*
         updateChart: function(new_chart){
-            console.log(new_chart);
-
-            if (new_chart === 'dwp_chart'){
+            if (new_chart === 'wperp_chart'){
                 configData.blueprintChartSeries(
                     'data-chart',
-                    'Contributions to DWP',
+                    'Contributions to wperp',
                     'Source:',
-                    configData.convertObjectToArray(configData.objectChartData.dwp_chart[0]),
-                    configData.convertObjectToArray(configData.objectChartData.dwp_chart[1]),
-                    configData.convertObjectToArray(configData.objectChartData.dwp_chart[2])
+                    configData.convertObjectToArray(configData.objectChartData.wperp_chart[0]),
+                    configData.convertObjectToArray(configData.objectChartData.wperp_chart[1]),
+                    configData.convertObjectToArray(configData.objectChartData.wperp_chart[2])
                 );
 
             } else if (new_chart === 'lacers_chart'){
@@ -91,7 +101,9 @@
                 );
             }
         },
+        */
 
+        // convert an object to an array
         convertObjectToArray: function(objectLiteral){
             var arr = [];
             for (prop in objectLiteral) {
@@ -100,18 +112,19 @@
             return arr;
         },
 
+        // the blueprint for a chart data
         blueprintChartSeries: function(containerToRenderTo, titleText, subtitleText, chartDataOne, chartDataTwo, chartDataThree){
             var chartMemberContribsSeries = {
                 name: 'Member Contributions',
                 color: '#002734',
-                type: 'column',
+                type: 'spline',
                 data: chartDataOne
             };
 
             var chartCityContribsSeries = {
                 name: 'City Contributions',
                 color: '#377EB8',
-                type: 'column',
+                type: 'spline',
                 data: chartDataTwo
             };
 
@@ -129,6 +142,72 @@
             )];
 
         },
+
+        // begin function
+        createSliderFilter: function(){
+
+            var increment;
+
+            //creates slider
+            jqueryNoConflict("#slider").slider();
+            var sliderBar = jqueryNoConflict('#slider');
+
+            // begin slider options
+            sliderBar.slider({
+                'value':0,
+                'min': 1000,
+                'max': 3000,
+                'step': 1000,
+
+                // displays when user slides the controller
+                slide: function(event, ui) {
+                    var increment = ui.value;
+                    var handle = ui.handle;
+                },
+
+                // begin when slider change is made
+                change: function(event, ui) {
+                    var increment = ui.value;
+                    var handle = ui.handle;
+                    var valueToEvaulaute = 'chart_' + increment;
+
+                    if (valueToEvaulaute === 'chart_1000'){
+                        configData.blueprintChartSeries(
+                            'data-chart',
+                            'Contributions to wperp',
+                            'Source:',
+                            configData.convertObjectToArray(configData.objectChartData.wperp_chart[0]),
+                            configData.convertObjectToArray(configData.objectChartData.wperp_chart[1]),
+                            configData.convertObjectToArray(configData.objectChartData.wperp_chart[2])
+                        );
+
+                    } else if (valueToEvaulaute === 'chart_2000'){
+                        configData.blueprintChartSeries(
+                            'data-chart',
+                            'Contributions to LACERS',
+                            'Source:',
+                            configData.convertObjectToArray(configData.objectChartData.lacers_chart[0]),
+                            configData.convertObjectToArray(configData.objectChartData.lacers_chart[1]),
+                            configData.convertObjectToArray(configData.objectChartData.lacers_chart[2])
+                        );
+
+                    } else {
+                        configData.blueprintChartSeries(
+                            'data-chart',
+                            'Contributions to LAFFP',
+                            'Source:',
+                            configData.convertObjectToArray(configData.objectChartData.laffp_chart[0]),
+                            configData.convertObjectToArray(configData.objectChartData.laffp_chart[1]),
+                            configData.convertObjectToArray(configData.objectChartData.laffp_chart[2])
+                        );
+                    }
+                }
+                // end slider change is made
+
+          });
+            // end slider options
+
+        }
 
     }
     // end main config function
