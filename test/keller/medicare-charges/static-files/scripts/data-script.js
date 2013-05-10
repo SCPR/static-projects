e05a03514e4999bc5d6536b6e80643b5c7ddbab6
chrislkeller <christopherlawrencekeller@gmail.com>
2013-05-09 22:35:39 -0700

    updates medicare data source, adds calcualtions to determine highest cost, lowest and average cost

--- test/keller/medicare-charges/static-files/scripts/data-script.js
+++ test/keller/medicare-charges/static-files/scripts/data-script.js
@@ -11,6 +11,7 @@ var dataConfig = dataConfig || {};
 // begin main function
 jqueryNoConflict(document).ready(function() {
     dataConfig.retrieveDataFromFile();
+
 });
 
 // begin data configuration object
@@ -81,12 +82,65 @@ var dataConfig = {
         });
     },
 
+
+    addCommas: function(nStr){
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+
+        var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1)) {
+                x1 = x1.replace(rgx, '$1' + ',' + '$2');
+            }
+        return x1 + x2;
+    },
+
+    calculateAverageCost: function(arrayOfTotals){
+        var total = 0;
+        for (var z=0; z<arrayOfTotals.length; z++){
+            total = total + arrayOfTotals[z];
+        }
+
+        total = total/arrayOfTotals.length;
+        return '$' + dataConfig.addCommas(total.toFixed(0));
+    },
+
+    calcaulateHighestAverageCost: function(array){
+        var total = Math.max.apply(Math, array);
+        return dataConfig.addCommas(total.toFixed(0));
+    },
+
+    calcaulateLowestAverageCost: function(array){
+        var total = Math.min.apply(Math, array);
+        return dataConfig.addCommas(total.toFixed(0));
+    },
+
+
+
+
+
+    // take string of dollar amount and convert to int
+    convertCurrencyToInt: function(currency){
+        var value = currency.replace('$', '').replace(',', '');
+        value = parseInt(value);
+        return value;
+    },
+
+
+
+
+
+    arrayProcedureCosts: [],
+
+
+
     compareSelectWithData: function(data){
 
         console.log(data);
 
         // array to hold our target hospitals
-        var testHoldingArray = [];
+        var arrayFilteredHospitalObjects = [];
 
         // retrieve dataConfig.comparisonDataObject which has the procedure
         var procedureToQuery = dataConfig.comparisonDataObject.procedure
@@ -97,30 +151,43 @@ var dataConfig = {
             if (data.objects[i].drgdefinition === procedureToQuery){
 
                 // for each of the hospital that has the procedure create a new object
-                var testTargetHospitalObject = {
+                var filteredHospitalObject = {
                     providername: data.objects[i].providername,
                     providercity: data.objects[i].providercity,
+                    providerstate: data.objects[i].providerstate,
                     drgdefinition: data.objects[i].drgdefinition,
                     totaldischarges: data.objects[i].totaldischarges,
                     averagecoveredcharges: data.objects[i].averagecoveredcharges,
                     averagetotalpayments: data.objects[i].averagetotalpayments
                 };
 
-                // push to our holding array
-                testHoldingArray.push(testTargetHospitalObject);
+                // push filteredHospitalObject to arrayFilteredHospitalObjects
+                arrayFilteredHospitalObjects.push(filteredHospitalObject);
+
+                // push filteredHospitalObject to arrayFilteredHospitalObjects
+                var testCosts = dataConfig.convertCurrencyToInt(data.objects[i].averagecoveredcharges);
+                dataConfig.arrayProcedureCosts.push(testCosts);
+
+                // calculate average costs for display
+                var costAverageToDisplay = dataConfig.calculateAverageCost(dataConfig.arrayProcedureCosts);
+                var lowestAverageCostToDisplay = dataConfig.calcaulateLowestAverageCost(dataConfig.arrayProcedureCosts);
+                var highestAverageCostToDisplay = dataConfig.calcaulateHighestAverageCost(dataConfig.arrayProcedureCosts);
+
+                jqueryNoConflict('#procedure').html(
+                    '<p>The average cost for this procedure at a California hospital is <strong>' + costAverageToDisplay + '</strong>.</p>' +
+                    '<p>The lowest average cost for this procedure at a California hospital is <strong>$' + lowestAverageCostToDisplay + '</strong>.</p>' +
+                    '<p>The highest average cost for this procedure at a California hospital is <strong>$' + highestAverageCostToDisplay + '</strong>.</p>');
 
             } else {
 
                 console.log('no match');
             }
-
         }
         // end loop
 
-        console.log(testHoldingArray);
-
+        // set our filtered array to an object
         var hospitalsObjectToBuildSelect = {
-            objects: testHoldingArray
+            objects: arrayFilteredHospitalObjects
         };
 
         jqueryNoConflict('#hospital-div-left').removeClass('hidden');
@@ -139,39 +206,36 @@ var dataConfig = {
 
         // check each hospital in the array of objects to grab
         //those that match the left and right hospital values
-        for (var x=0; x<testHoldingArray.length; x++){
+        for (var x=0; x<arrayFilteredHospitalObjects.length; x++){
             if (dataConfig.comparisonDataObject.hospitalLeft === undefined || dataConfig.comparisonDataObject.hospitalLeft === null){
-                jqueryNoConflict('#hospital-left').html('Choose a hospital to compare');
+                jqueryNoConflict('#hospital-left').html('Choose a hospital for comparison');
             };
 
             if (dataConfig.comparisonDataObject.hospitalRight === undefined || dataConfig.comparisonDataObject.hospitalRight === null){
-                jqueryNoConflict('#hospital-right').html('Choose a hospital to compare');
+                jqueryNoConflict('#hospital-right').html('Choose a hospital for comparison');
             };
 
             // display the data
-            if (dataConfig.comparisonDataObject.hospitalLeft === testHoldingArray[x].providername){
+            if (dataConfig.comparisonDataObject.hospitalLeft === arrayFilteredHospitalObjects[x].providername){
                 jqueryNoConflict('#hospital-left').html(
-                '<h4>' + testHoldingArray[x].providername + '</h4>' +
-                '<p>' + testHoldingArray[x].providercity + '</p>' +
-                '<p>Procedure: ' + testHoldingArray[x].drgdefinition + '</p>' +
-                '<p>Discharges: ' + testHoldingArray[x].totaldischarges + '</p>' +
-                '<p>Covered Charges: ' + testHoldingArray[x].averagecoveredcharges + '</p>' +
-                '<p>Average Total Payments:' + testHoldingArray[x].averagetotalpayments + '</p>');
+                '<h4>' + arrayFilteredHospitalObjects[x].providername + '</h4>' +
+                '<p>' + arrayFilteredHospitalObjects[x].providercity + ', ' + arrayFilteredHospitalObjects[x].providerstate + '</p>' +
+                '<p>Procedure: ' + arrayFilteredHospitalObjects[x].drgdefinition + '</p>' +
+                '<p>Discharges: ' + arrayFilteredHospitalObjects[x].totaldischarges + '</p>' +
+                '<p>Covered Charges: ' + arrayFilteredHospitalObjects[x].averagecoveredcharges + '</p>' +
+                '<p>Average Total Payments:' + arrayFilteredHospitalObjects[x].averagetotalpayments + '</p>');
             }
 
-            if (dataConfig.comparisonDataObject.hospitalRight === testHoldingArray[x].providername){
+            if (dataConfig.comparisonDataObject.hospitalRight === arrayFilteredHospitalObjects[x].providername){
                 jqueryNoConflict('#hospital-right').html(
-                '<h4>' + testHoldingArray[x].providername + '</h4>' +
-                '<p>' + testHoldingArray[x].providercity + '</p>' +
-                '<p>Procedure: ' + testHoldingArray[x].drgdefinition + '</p>' +
-                '<p>Discharges: ' + testHoldingArray[x].totaldischarges + '</p>' +
-                '<p>Covered Charges: ' + testHoldingArray[x].averagecoveredcharges + '</p>' +
-                '<p>Average Total Payments:' + testHoldingArray[x].averagetotalpayments + '</p>');
+                '<h4>' + arrayFilteredHospitalObjects[x].providername + '</h4>' +
+                '<p>' + arrayFilteredHospitalObjects[x].providercity + ', ' + arrayFilteredHospitalObjects[x].providerstate + '</p>' +
+                '<p>Procedure: ' + arrayFilteredHospitalObjects[x].drgdefinition + '</p>' +
+                '<p>Discharges: ' + arrayFilteredHospitalObjects[x].totaldischarges + '</p>' +
+                '<p>Covered Charges: ' + arrayFilteredHospitalObjects[x].averagecoveredcharges + '</p>' +
+                '<p>Average Total Payments:' + arrayFilteredHospitalObjects[x].averagetotalpayments + '</p>');
             }
-
-
         }
-
     }
 };
 // end data configuration object
