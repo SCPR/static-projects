70dbd015d7a2e2527cf3e988e08a452f7a1df234
Chris Keller <christopherlawrencekeller@gmail.com>
2013-05-22 18:48:15 -0700

    pushes exit polling charts live

--- interactives/la-mayors-race/mayoral-general-election-exit-polls/static-files/scripts/data-script.js
+++ interactives/la-mayors-race/mayoral-general-election-exit-polls/static-files/scripts/data-script.js
@@ -5,11 +5,38 @@
         startProcessingTheData()
     });
 
+    // scroll into view function
+    function scrollToTopic(){
+        var exitPollTopic = jqueryNoConflict('#search-exit-polling-topics').val();
+        jqueryNoConflict.scrollTo(exitPollTopic)
+    }
+
     function startProcessingTheData(){
         processGenderData();
         processGeographyData();
         processHousingData();
         processMilitaryData();
+
+        //Calculate the height of <header>
+        //Use outerHeight() instead of height() if have padding
+        var aboveHeight = $('header').outerHeight();
+
+        // when scroll
+        $(window).scroll(function(){
+
+        	//if scrolled down more than the header's height
+            if ($(window).scrollTop() > aboveHeight){
+
+        		// if yes, add "fixed" class to the <nav>
+        		// add padding top to the #content (value is same as the height of the nav)
+                $('nav').addClass('fixed').css('top','100').next().css('padding-top','60px');
+            } else {
+
+        		// when scroll up or less than aboveHeight, remove the "fixed" class, and the padding-top
+                $('nav').removeClass('fixed').next().css('padding-top','0');
+            }
+        });
+
     }
 
     // display data from tabletop
