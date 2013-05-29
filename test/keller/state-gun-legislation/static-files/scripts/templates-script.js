ac624fa2e133752472017b3b13da934b2eeec707
Chris Keller <christopherlawrencekeller@gmail.com>
2013-05-28 17:43:14 -0700

    removes content-explainer template from being rendered

--- test/keller/state-gun-legislation/static-files/scripts/templates-script.js
+++ test/keller/state-gun-legislation/static-files/scripts/templates-script.js
@@ -13,8 +13,6 @@ function renderStaticTemplates(){
     renderHandlebarsTemplate('static-files/templates/kpcc-embed-header.handlebars', '#kpcc-embed-header');
     renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
     renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
-    //renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals');
     renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
-    renderHandlebarsTemplate('static-files/templates/content-explainer.handlebars', '#content-explainer');
 };
 // end
