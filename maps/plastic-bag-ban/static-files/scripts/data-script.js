7c2ca18be93ff140a078ac532727b48d793d4a7d
Chris Keller <christopherlawrencekeller@gmail.com>
2013-06-17 10:10:23 -0700

    updates plastic bag ban to add analytics and such

---
+++ maps/plastic-bag-ban/static-files/scripts/data-script.js
@@ -0,0 +1,22 @@
+var jqueryNoConflict = jQuery;
+var fn = fn || {};
+
+// begin main function
+jqueryNoConflict(document).ready(function() {
+    fn.writeMapToCanvas();
+});
+
+// begin data configuration object
+var fn = {
+
+    // pull the data from the flat file
+    writeMapToCanvas: function(){
+        var map = L.mapbox.map('map', 'kpccdatadesk.map-79qkuslo',{
+            center: [34.201,-118.305],
+            zoom: 10,
+            minZoom: 9,
+            maxZoom: 12,
+        });
+    }
+};
+// end data configuration object
