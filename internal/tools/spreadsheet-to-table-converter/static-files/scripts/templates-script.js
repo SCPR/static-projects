600bf8584eaebd5319a9a6720f5c7423834ed171
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-26 10:17:23 -0700

    removes social sharing

--- internal/tools/spreadsheet-to-table-converter/static-files/scripts/templates-script.js
+++ internal/tools/spreadsheet-to-table-converter/static-files/scripts/templates-script.js
@@ -10,7 +10,7 @@ jqueryNoConflict(document).ready(function() {
 function renderStaticTemplates(){
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '#kpcc-header');
     renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '#kpcc-footer');
-    renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
+    //renderHandlebarsTemplate('static-files/templates/data-share.handlebars', '#data-share');
     renderHandlebarsTemplate('static-files/templates/data-details.handlebars', '#data-details');
     //renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals');
     renderHandlebarsTemplate('static-files/templates/data-footer.handlebars', '#data-footer');
