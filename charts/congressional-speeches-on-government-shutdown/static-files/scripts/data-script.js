4445fd5458cf5a3f0595689c9c9b866602d0d91f
Chris Keller <christopherlawrencekeller@gmail.com>
2013-10-04 17:56:08 -0700

    fixes date sort in What did state Congressional representatives say about the shutdown?, attempts to catalog instances, add ana;ytics

--- charts/congressional-speeches-on-government-shutdown/static-files/scripts/data-script.js
+++ charts/congressional-speeches-on-government-shutdown/static-files/scripts/data-script.js
@@ -39,30 +39,48 @@ var fn = {
     retrievePhraseToQuery: function(){
         jqueryNoConflict('#data-legend a').click(function(){
             var phrase = jqueryNoConflict(this).attr('id').replace('_', '+');
+            jqueryNoConflict('#data-visuals').empty();
             fn.constructCapitolWordsQuery(phrase);
         });
     },
 
     // construct the url to query for data
     constructCapitolWordsQuery: function(phrase){
-        var urlPrefix = 'http://capitolwords.org/api/1/text.json?apikey=b717252e9bc44d4ea57321c49e7dd5e8';
+        var urlPhrasesPrefix = 'http://capitolwords.org/api/1/text.json?apikey=b717252e9bc44d4ea57321c49e7dd5e8';
+        var urlInstancesPrefix = 'http://capitolwords.org/api/1/phrases/party.json?apikey=b717252e9bc44d4ea57321c49e7dd5e8';
         var urlStartDate = '&start_date=2013-08-01';
         var urlEndDate = '&end_date=2013-10-30';
         var urlState = '&state=CA';
         var urlPhrase = '&phrase=' + phrase;
         var urlCallback = '&callback=?';
-        var targetUrl = urlPrefix + urlStartDate + urlEndDate + urlState + urlPhrase + urlCallback;
-        fn.retriveCapitolWordsData(targetUrl);
+        var targetPhrasesUrl = urlPhrasesPrefix + urlStartDate + urlEndDate + urlState + urlPhrase + urlCallback;
+        var targetInstancesUrl = urlInstancesPrefix + urlStartDate + urlEndDate + urlState + urlPhrase + urlCallback;
+        fn.retriveCapitolWordsData(targetPhrasesUrl, targetInstancesUrl);
         fn.displayPhraseHeadline(phrase);
     },
 
-    retriveCapitolWordsData: function(targetUrl){
-        jqueryNoConflict.getJSON(targetUrl, fn.processCapitolWordsData);
+    retriveCapitolWordsData: function(targetPhrasesUrl, targetInstancesUrl){
+        jqueryNoConflict.getJSON(targetPhrasesUrl, fn.processCapitolPhrasesData);
+        //jqueryNoConflict.getJSON(targetInstancesUrl, fn.processCapitolInstancesData);
     },
 
-    processCapitolWordsData: function(data){
+    processCapitolInstancesData: function(data){
 
-        console.log(data);
+        console.log(data.results[0]);
+
+        for(var y=0; y<data.results.length; y++){
+            if (data.results[y].party === 'D'){
+                var demsTotal = data.results[y].count;
+                jqueryNoConflict('#total-democrat-instances').html(data.results[y].count);
+            } else if (data.results[y].party === 'R'){
+                var gopTotal = data.results[y].count;
+                jqueryNoConflict('#total-republican-instances').html(data.results[y].count);
+            }
+        }
+
+    },
+
+    processCapitolPhrasesData: function(data){
 
         // clear the container
         fn.objectOfLegislators.objects = [];
@@ -88,14 +106,19 @@ var fn = {
             fn.objectOfLegislators.objects.push(instanceOfLegislatorSpeaking);
         }
 
+        var sortedObjectOfLegislators = _.chain(fn.objectOfLegislators.objects)
+            .map(fn.to_date)
+            .sortBy(fn.desc_start_time)
+            .value();
+
         var handlebarsData = {
-            objects: fn.objectOfLegislators.objects
+            objects: sortedObjectOfLegislators
         };
 
         Handlebars.registerHelper('dateFormat', function(context, block) {
             if (window.moment) {
                 return fn.takeTime(context);
-            }else{
+            } else {
                 return context;
             };
         });
@@ -116,6 +139,16 @@ var fn = {
 
     toTitleCase: function(str){
         return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    },
+
+    to_date: function(o) {
+        var parts = o.date.split('-');
+        o.date = new Date(parts[0], parts[1] - 1, parts[2]);
+        return o;
+    },
+
+    desc_start_time: function(o) {
+        return -o.date.getTime();
     }
 
 };
