77dcc79055ecca7bc03547c0d87fef95dd2d34e6
Chris Keller <christopherlawrencekeller@gmail.com>
2013-12-03 17:20:30 -0800

    adds if/else magic for legislator model if no kpcc articles are found

--- test/keller/find-my-rep/scripts/views/detailView.js
+++ test/keller/find-my-rep/scripts/views/detailView.js
@@ -9,29 +9,22 @@ App.Views.DetailView = Backbone.View.extend({
     setModel: function(model){
         this.model = model;
         var $this = this;
+
+        var age = this.calculateAge(moment($this.model[0].attributes.birthday).format('MM/DD/YYYY'));
+        $this.model[0].set('age', age);
+
         var kpccApiQuery = this.model[0].attributes.first_name + '+' + this.model[0].attributes.last_name;
         var kpccApiQueryUrl = "http://www.scpr.org/api/v3/articles?types=news,blogs&limit=5&query=" + kpccApiQuery;
         $this.model[0].set('kpccApiQueryUrl', kpccApiQueryUrl);
 
         var twitterApiQuery = this.model[0].attributes.twitter_id;
         var twitterApiQueryUrl;
-
         if (twitterApiQuery === null){
             twitterApiQueryUrl = null
         } else {
             twitterApiQueryUrl = "https://socal-political-tweets.herokuapp.com/1.1/statuses/user_timeline.json?count=5&screen_name=" + twitterApiQuery + "&callback=?";
-        }
-
+        };
         $this.model[0].set('twitterApiQueryUrl', twitterApiQueryUrl);
-
-        var birthday = moment($this.model[0].attributes.birthday).format('MM/DD/YYYY');
-        var birth = new Date(birthday);
-        var today = moment().format('MM/DD/YYYY');
-        var check = new Date(today);
-        var milliDay = 1000 * 60 * 60 * 24;
-        var ageInDays = (check-birth)/milliDay;
-        var ageInYears =  Math.floor(ageInDays / 365 );
-        $this.model[0].set('age', ageInYears);
 
         if (!this.model[0].get('loaded')) {
             $this.model[0].set('loaded', true);
@@ -59,45 +52,33 @@ App.Views.DetailView = Backbone.View.extend({
                     getArticles(kpccApiQueryUrl),
                     getTweets(twitterApiQueryUrl)
                 ).done(function(articles, tweets){
-                    $this.model[0].set('kpccApiArticles', articles[0].articles);
+                    if (articles[0].articles.length === 0){
+                        console.log('no articles');
+                        $this.model[0].set('kpccApiArticles', null);
+                    } else {
+                        console.log('articles');
+                        $this.model[0].set('kpccApiArticles', articles[0].articles);
+                    };
                     $this.model[0].set('twitterApiTweets', tweets[0]);
                     $this.render();
                 });
-
             }
 
-            // trying to create null value for articles if not present
-            if ($this.model[0].attributes.kpccApiArticles === null){
-                console.log('0');
-                $this.model[0].set('kpccApiArticles', null);
-            }
             console.log($this.model[0].attributes);
-
-
-            /*
-            $.ajax({
-                url: kpccApiQueryUrl,
-                type: 'GET',
-                dataType: 'json',
-                async: false,
-                success: function(data){
-                    $this.model[0].set('kpccApiArticles', data.articles);
-                }
-            });
-
-            $.ajax({
-                url: twitterApiQueryUrl,
-                dataType: 'jsonp',
-                async: false,
-                success: function(data){
-                    $this.model[0].set('twitterApiTweets', data);
-                }
-            });
-            */
 
         } else {
             $this.render();
-        }
+        };
+    },
+
+    calculateAge: function(birthday){
+        var birth = new Date(birthday);
+        var today = moment().format('MM/DD/YYYY');
+        var check = new Date(today);
+        var milliDay = 1000 * 60 * 60 * 24;
+        var ageInDays = (check-birth)/milliDay;
+        var ageInYears =  Math.floor(ageInDays / 365 );
+        return ageInYears;
     },
 
     render: function(){
