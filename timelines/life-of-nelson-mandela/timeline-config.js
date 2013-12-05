91063165ca87d690e824f34d9dfdc78ee4a5cc93
Chris Keller <christopherlawrencekeller@gmail.com>
2013-12-05 14:44:03 -0800

    adds timeline for life of nelson mandela

---
+++ timelines/life-of-nelson-mandela/timeline-config.js
@@ -0,0 +1,34 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: 'spreadsheet',
+
+    // example spreadsheet key
+    key: '0Aq8qwSArzKP9dEo4Wl9VMTJXNllVaG5nWU5GMkhJclE',
+
+    // example path to file
+    //sourceFile: 'data/moments-in-giants-godgers-rivalry_timeline.json',
+
+    // name of sheet containing timeline entries
+    sheetName: 'Posts',
+
+    // name of sheet containing timeline meta
+    sheetMeta: 'MetaData',
+
+    // url of the timeline
+    projectDirectory: 'http://projects.scpr.org/static/timelines/life-of-nelson-mandela',
+
+    // newest or oldest
+    defaultDirection: 'oldest',
+
+    // collapsed or expanded
+    defaultExpansion: 'expanded',
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: 'groupSegmentByYear',
+
+    // adjust timeline width
+    width: '90%'
+};
