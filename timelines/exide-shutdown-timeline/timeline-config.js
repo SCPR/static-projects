455b8d449be70fb42e87f8c2bd8593c286c9eff9
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-23 15:41:36 -0800

    updates to the project directory for the exide timeline

--- timelines/exide-shutdown-timeline/timeline-config.js
+++ timelines/exide-shutdown-timeline/timeline-config.js
@@ -21,7 +21,7 @@ var kpccTimelineConfig = {
     embedThis: true,
 
     // url of the timeline
-    projectDirectory: 'http://projects.scpr.org/static/timelines/timeline-template/',
+    projectDirectory: 'http://projects.scpr.org/static/timelines/exide-shutdown-timeline/',
 
     // newest or oldest
     defaultDirection: 'newest',
