616a9016122c2da12282cb253a9238631cb3b731
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-23 15:35:30 -0800

    updates exide timeline to the new format

---
+++ timelines/exide-shutdown-timeline/timeline-config.js
@@ -0,0 +1,40 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: 'spreadsheet',
+
+    // example spreadsheet key
+    key: '0Aq8qwSArzKP9dEcyNW9TS3V3NVdCM3hOMFpYZHVhcmc',
+
+    // example path to data file
+    sourceFile: 'timeline-data.json',
+
+    // example path to meta file
+    metaFile: 'timeline-meta.json',
+
+    // name of sheet containing timeline entries
+    sheetName: 'Posts',
+
+    // name of sheet containing timeline meta
+    sheetMeta: 'MetaData',
+
+    // make the timeline embeddable
+    embedThis: true,
+
+    // url of the timeline
+    projectDirectory: 'http://projects.scpr.org/static/timelines/timeline-template/',
+
+    // newest or oldest
+    defaultDirection: 'newest',
+
+    // collapsed or expanded
+    defaultExpansion: 'expanded',
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: 'groupSegmentByYear',
+
+    // adjust timeline width
+    width: '90%'
+};
