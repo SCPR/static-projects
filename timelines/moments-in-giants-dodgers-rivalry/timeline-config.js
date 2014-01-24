6635d60c384265cd638418fe55ededc89b418ad1
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-23 16:22:13 -0800

    converts giants-dodgers timeline to new timeline template using flatfile data

---
+++ timelines/moments-in-giants-dodgers-rivalry/timeline-config.js
@@ -0,0 +1,40 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: 'flat-file',
+
+    // example spreadsheet key
+    key: '0AukLBD7DEyX8dHJFRWxDbF9wZ19GajNOV3Nnamp4a2c',
+
+    // example path to data file
+    sourceFile: 'timeline-data.json',
+
+    // example path to meta file
+    metaFile: 'timeline-meta.json',
+
+    // name of sheet containing timeline entries
+    sheetName: 'Posts',
+
+    // name of sheet containing timeline meta
+    sheetMeta: 'MetaData',
+
+    // make the timeline embeddable
+    embedThis: true,
+
+    // url of the timeline
+    projectDirectory: 'http://projects.scpr.org/static/timelines/moments-in-giants-dodgers-rivalry/',
+
+    // newest or oldest
+    defaultDirection: 'oldest',
+
+    // collapsed or expanded
+    defaultExpansion: 'expanded',
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: 'groupSegmentByDecade',
+
+    // adjust timeline width
+    width: '90%'
+};
