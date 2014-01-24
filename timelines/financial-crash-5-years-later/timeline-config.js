88a13b33c538b3e3c4e2210da28f70ddbcccd7a2
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-23 16:44:04 -0800

    moves financial crash timeline to static data and new version

---
+++ timelines/financial-crash-5-years-later/timeline-config.js
@@ -0,0 +1,40 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: 'spreadsheet',
+
+    // example spreadsheet key
+    key: '0Aq8qwSArzKP9dEpfTG5GTnNfcFhuR0RVbkJha3BtdlE',
+
+    // example path to data file
+    sourceFile: 'timeline-data.json',
+
+    // example path to meta file
+    metaFile: 'timeline-meta.json',
+
+    // name of sheet containing timeline entries
+    sheetName: 'Posts',
+
+    // name of sheet containing timeline meta
+    sheetMeta: 'MetaData',
+
+    // make the timeline embeddable
+    embedThis: true,
+
+    // url of the timeline
+    projectDirectory: 'http://projects.scpr.org/static/timelines/financial-crash-5-years-later/',
+
+    // newest or oldest
+    defaultDirection: 'oldest',
+
+    // collapsed or expanded
+    defaultExpansion: 'expanded',
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: 'groupSegmentByYear',
+
+    // adjust timeline width
+    width: '90%'
+};
