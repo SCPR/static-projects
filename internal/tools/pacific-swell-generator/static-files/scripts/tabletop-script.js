0f34e16ccb29de08630d7876f34508bb1a819816
Chris Keller <christopherlawrencekeller@gmail.com>
2014-03-21 15:24:08 -0700

    streamlines script spacing in molly's blog post generator

--- internal/tools/pacific-swell-generator/static-files/scripts/tabletop-script.js
+++ internal/tools/pacific-swell-generator/static-files/scripts/tabletop-script.js
@@ -40,39 +40,29 @@
             for (var x=0; x<array.length; x++){
                 fn.evaluateLengthOfSources(array[x])
             };
-
             fn.displayPreviewOnPage(fn.postContent);
-
         },
 
         evaluateLengthOfSources: function(object){
             if (object.source != ""){
-
                 if (object.source.indexOf(';') > 0){
                     var sourceArray = object.source.split(';')
                     object.source = sourceArray;
                 }
-
                 if (object.sourceurl.indexOf(';') > 0){
                     var sourceUrlArray = object.sourceurl.split(';')
                     object.sourceurl = sourceUrlArray;
                 }
             }
-
             fn.createPostHtml(object)
         },
 
         createPostHtml: function(object){
             if (object.element === "sentence"){
-
                 object.html = "<p>" + object.text + "</p>";
-
             } else if (object.element === "bullet_item"){
-
                 if (object.source != "" && typeof(object.source) === "object" && typeof(object.sourceurl) === "object"){
-
                     var sourceString = "";
-
                     for (var x=0; x<object.source.length; x++){
                         sourceString += "<a href='" +
                             object.sourceurl[x].replace(/^\s+|\s+$/g,'') +
@@ -80,25 +70,15 @@
                             object.source[x].replace(/^\s+|\s+$/g,'') +
                             "</a>, ";
                     };
-
                     object.html = "<li>" + object.text + " (" + sourceString + ")</li>";
-
                 } else if (object.source != "" && typeof(object.source) != "object" && typeof(object.sourceurl) != "object"){
-
                     object.html = "<li>" + object.text + " (<a href='" + object.sourceurl + "' target='_blank'>" + object.source + "</a>)</li>";
-
                 } else {
-
                     object.html = "<li>" + object.text + "</li>";
-
                 }
-
             } else if (object.element === "blockquote"){
-
                 if (object.source != "" && typeof(object.source) === "object" && typeof(object.sourceurl) === "object"){
-
                     var sourceString = "";
-
                     for (var x=0; x<object.source.length; x++){
                         sourceString += "<a href='" +
                             object.sourceurl[x].replace(/^\s+|\s+$/g,'') +
@@ -106,24 +86,15 @@
                             object.source[x].replace(/^\s+|\s+$/g,'') +
                             "</a>, ";
                     };
-
                     object.html = "<blockquote>" + object.text + " (" + sourceString + ")</blockquote>";
-
                 } else if (object.source != "" && typeof(object.source) != "object" && typeof(object.sourceurl) != "object"){
-
                     object.html = "<blockquote>" + object.text + " (<a href='" + object.sourceurl + "' target='_blank'>" + object.source + "</a>)</blockquote>";
-
                 } else {
-
                     object.html = "<blockquote>" + object.text + "</blockquote>";
-
                 }
-
             } else {
                 object.html = "<p></p>";
-
             }
-
             fn.postContent.push(object);
         },
 
