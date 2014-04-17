0ddf534b16fe75ec14da2d03d9da8312dfdca4f9
Chris Keller <christopherlawrencekeller@gmail.com>
2014-04-16 18:33:41 -0700

    adds if/else fall back if no aggregate income data for given map feature

--- longreads/la_renting/static/js/map-application.js
+++ longreads/la_renting/static/js/map-application.js
@@ -96,11 +96,12 @@ var fn = {
             onEachFeature: function(feature, layer) {
 
                 layer.on('click', function (e) {
-
                     var featcherSentence = _.template(
                         "<div id='zip_<%= name %>'>" +
                             "<h4>Renters occupy an estimated <span class='gt-30pct'><%= fn.addCommas(la_area_rent_rent_total) %> </span> households in <%= la_area_rent_county_proper %> County's <a href='http://censusreporter.org/profiles/86000US<%= name %>-<%= name %>/'><%= name %></a> Zip Code Tabulation Area (<a href='http://www.census.gov/geo/reference/zctas.html' target='_blank'>ZCTA</a>), according to American Community Survey data.</h4>" +
-                            "<h4>The average renter-occupied household here earns about <span class='gt-30pct'>$<%= fn.averageRenter(la_area_rent_rent_income_month_average, la_area_rent_rent_total) %></span> a month and spends an average of <span class='gt-30pct'>$<%= fn.averageRenter(la_area_rent_rent_aggregate_total, la_area_rent_rent_total) %></span> on rent each month, about <span class='gt-30pct'><%= fn.rentToIncome(la_area_rent_rent_aggregate_total, la_area_rent_rent_income_month_average, la_area_rent_rent_total) %>% </span>of their monthly income.</h4>" +
+                            "<% if (la_area_rent_rent_income_past_12_aggregate != null && la_area_rent_rent_aggregate_total != null) { %>" +
+                                "<h4>The average renter-occupied household here earns about <span class='gt-30pct'>$<%= fn.averageRenter(la_area_rent_rent_income_month_average, la_area_rent_rent_total) %></span> a month and spends an average of <span class='gt-30pct'>$<%= fn.averageRenter(la_area_rent_rent_aggregate_total, la_area_rent_rent_total) %></span> on rent each month, about <span class='gt-30pct'><%= fn.rentToIncome(la_area_rent_rent_aggregate_total, la_area_rent_rent_income_month_average, la_area_rent_rent_total) %>% </span>of their monthly income.</h4>" +
+                            "<% } %>" +
                             "<h4>Here's the breakdown of this area's household's rent burden as a percentage of its income:</h4>" +
                         "</div>", feature.properties);
 
