aaa0f5b11d8666283337c9c5252b5efb26f8efb5
Chris Keller <christopherlawrencekeller@gmail.com>
2014-05-14 21:26:12 -0700

    adds required files for hotel california project and a bunch of test files

---
+++ infographics/hotel-california/README.md
@@ -0,0 +1,4 @@
+base-tarbell-template
+=====================
+
+The base KPCC template for using the Tarbell static site generator.
---
+++ infographics/hotel-california/app.js
@@ -0,0 +1,58 @@
+var fn = fn || {};
+var initializeProject = initializeProject || {};
+
+$(document).ready(function() {
+    initializeProject.renderPage();
+});
+
+var fn = {
+
+
+};
+
+// begin intitial page rendering
+var initializeProject = {
+
+    renderPage: function(){
+        var checkExist = setInterval(function() {
+            if ($('.header-links').length) {
+                clearInterval(checkExist);
+                initializeProject.hideEmbedBox();
+            }
+
+            if ($('.buttons').length) {
+                clearInterval(checkExist);
+                initializeProject.toggleDisplayIcon();
+            }
+        }, 1000);
+    },
+
+    hideEmbedBox: function(){
+        if ("" === ""){
+            $('li.projects-embed').addClass('hidden');
+        };
+    },
+
+    renderEmbedBox: function(){
+        jAlert('<h4>Embed this on your site or blog</h4>' + '<span>Copy this code and paste to source of your page. You may need to adjust the height parameter. <br /><br /> &lt;iframe src=\"'+ "http://projects.scpr.org/static/infographics/hotel-california" +'\" width=\"100%\" height=\"850px\" style=\"margin: 0 auto;\" frameborder=\"no\"&gt;&lt;/iframe>', 'Share or Embed');
+    },
+
+    toggleDisplayIcon: function(){
+        $('.text').on('shown.bs.collapse', function(){
+            $('span.text').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        });
+        $('.text').on('hidden.bs.collapse', function(){
+            $('span.text').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+        });
+        $('.about').on('shown.bs.collapse', function(){
+            $('span.about').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        });
+        $('.about').on('hidden.bs.collapse', function(){
+            $('span.about').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+        });
+        if ("1" === "1"){
+            $('.text').collapse('show');
+        };
+    }
+};
+// end intitial page rendering
