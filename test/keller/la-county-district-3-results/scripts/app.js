cf18dbf8c1732471e1774a9d1635278e6a8b9cc5
Chris Keller <christopherlawrencekeller@gmail.com>
2014-06-24 15:39:27 -0700

    updates to the underscore header for backbone projects

--- test/keller/la-county-district-3-results/scripts/app.js
+++ test/keller/la-county-district-3-results/scripts/app.js
@@ -1,5 +1,16 @@
 (function(){
 
+    window.appConfig = {
+        testing: true,
+        project_root: "http://projects.scpr.org/static/applications/",
+        open_about_this: true,
+        comments: false,
+        embed_this: false,
+        embed_width: "100%",
+        embed_height: "850px",
+        twitter_share_text: "This is a test"
+    };
+
     window.App = {
         Models: {},
         Collections: {},
@@ -57,301 +68,112 @@
         return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     };
 
-    App.Models.Response = Backbone.Model.extend({});
-
-    App.Models.Map = Backbone.Model.extend({});
-
-    App.Models.Marker = Backbone.Model.extend({});
-
-    App.Collections.Responses = Backbone.Collection.extend({
-        model: App.Models.Response,
-        parse: function(response){
-            return response.results;
-        }
-    });
-
-    App.Collections.Markers = Backbone.Collection.extend({
-        model: App.Models.Marker
-    });
-
     App.Router = Backbone.Router.extend({
         initialize: function(){
-            $(".kpcc-header").html(_.template(template("http://projects.scpr.org/static/static-files/v3-dependencies/templates/kpcc-header.html")));
-            $(".data-details").html(_.template(template("templates/data-details.html")));
-            $(".kpcc-footer").html(_.template(template("http://projects.scpr.org/static/static-files/v3-dependencies/templates/kpcc-footer.html")));
+            console.log("router initialized");
         },
 
         routes: {
-            "": "createMap",
+            "": "renderApplicationWrapper",
         },
 
-        createMap: function(){
-            if (this.mapView){
-                this.mapView.remove();
-            };
-
-            this.mapView = new App.Views.MapApplication({
-                collection: null,
-                container: ".data-visuals",
-                initialZoom: 6
-            });
-
-            return this.mapView;
-        }
+        renderApplicationWrapper: function(){
+            this.applicationWrapper = new App.Views.ApplicationWrapper();
+            return this.applicationWrapper;
+        },
     });
 
-    App.Views.MapApplication = Backbone.View.extend({
-        template: template("templates/data-visuals.html"),
-        el: ".data-visuals",
-        initialize: function(mapDataObject){
-            this.mapDataObject = mapDataObject;
-            this.stamenToner = new L.tileLayer(
-                "http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png", {
-                    attribution: "Map tiles by <a href='http://stamen.com' target='_blank'>Stamen Design</a>, <a href='http://creativecommons.org/licenses/by/3.0' target='_blank'>CC BY 3.0</a> &mdash; Map data &copy; <a href='http://openstreetmap.org' target='_blank'>OpenStreetMap</a> contributors, <a href='http://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC-BY-SA</a>",
-                    minZoom: 6,
-                    maxZoom: 14
-            });
+    App.Views.ApplicationWrapper = Backbone.View.extend({
 
-            // set zoom for mobile devices
-            if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
-                initialZoom = 7;
+        el: ".kpcc-header",
+
+        initialize: function(){
+
+            // checks for testing environment
+            if (window.appConfig.testing === true){
+                window.wrapperTemplatePath = "/2kpcc/static-projects/static-files/v3-dependencies/templates/"
             } else {
-                initialZoom = 11;
+                window.wrapperTemplatePath = "http://projects.scpr.org/static/static-files/v3-dependencies/templates/"
             }
 
-            this.center = new L.LatLng(34.15954545771161, -118.57177019119261);
-            this.geojsonOne = L.geoJson(electionResults, {
-                filter: this.filterFeatures,
-                style: this.styleFeatures,
-                onEachFeature: this.onEachFeature
+            // checks embed settings
+            if (window.appConfig.embed_this != true){
+                $('li.projects-embed').addClass('hidden');
+            }
+
+            // checks url to see if its embedded
+            var urlLink = window.location.href;
+            if (urlLink.indexOf("embed") > -1){
+                window.appConfig.openAboutThis = false;
+                window.appConfig.comments = false;
+                $(".data-comments").remove();
+            };
+
+            // checks comments setting
+            if (window.appConfig.comments === true){
+                var disqus_shortname = 'kpcc-projects';
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);
+            } else {
+                $(".data-comments").remove();
+            };
+
+            // checks open the about this project pane
+            if (window.appConfig.openAboutThis === true){
+                $('.text').collapse('show');
+            };
+
+            $('.text').on('shown.bs.collapse', function(){
+                $('span.text').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
             });
-            this.render(this.mapDataObject);
+
+            $('.text').on('hidden.bs.collapse', function(){
+                $('span.text').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+            });
+
+            $('.about').on('shown.bs.collapse', function(){
+                $('span.about').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            });
+
+            $('.about').on('hidden.bs.collapse', function(){
+                $('span.about').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+            });
+
+            this.render();
+
         },
 
         events: {
-            "click a.resetMap": "resetMap",
-            "click a.kuehlPrecincts": "kuehlMap",
-            "click a.shriverPrecincts": "shriverMap",
-            "click .precinct-data": "thisPrecinct",
+            "click li.projects-embed a": "renderEmbedBox",
         },
 
-        styleFeatures: function (feature) {
-            var layerColor;
-            var totalVotes = feature.properties.ballots;
-            feature.properties.results = {
-                registeredVoters: feature.properties.registrati,
-                totalVotes: totalVotes,
-                voterTurnout: window.toFixedPercent(totalVotes, feature.properties.registrati),
-                precinctResults: [
-                    {candidate: "Shriver", votes: feature.properties.shriver, percent: window.toFixedPercent(feature.properties.shriver, totalVotes)},
-                    {candidate: "Kuehl", votes: feature.properties.kuehl, percent: window.toFixedPercent(feature.properties.kuehl, totalVotes)},
-                    /*
-                    {candidate: "Duran", votes: feature.properties.duran, percent: window.toFixedPercent(feature.properties.duran, totalVotes)},
-                    {candidate: "Fay", votes: feature.properties.fay, percent: window.toFixedPercent(feature.properties.fay, totalVotes)},
-                    {candidate: "Kremer", votes: feature.properties.kremer, percent: window.toFixedPercent(feature.properties.kremer, totalVotes)},
-                    {candidate: "Melendez", votes: feature.properties.melendez, percent: window.toFixedPercent(feature.properties.melendez, totalVotes)},
-                    {candidate: "Preven", votes: feature.properties.preven, percent: window.toFixedPercent(feature.properties.preven, totalVotes)},
-                    {candidate: "Ulich", votes: feature.properties.ulich, percent: window.toFixedPercent(feature.properties.ulich, totalVotes)}
-                    */
-                ],
-            };
-
-            var maxValue = _.max(feature.properties.results.precinctResults, function(candidate){
-                return candidate.percent;
-            });
-
-            var winners = _.filter(feature.properties.results.precinctResults, function(candidate){
-                return candidate.percent == maxValue.percent;
-            });
-
-            feature.properties.winner = winners;
-
-            if (winners.length === 1){
-                if (winners[0].candidate === "Duran"){
-                    layerColor = "rgb(127,201,127)";
-                } else if (winners[0].candidate === "Fay"){
-                    layerColor = "rgb(190,174,212)";
-                } else if (winners[0].candidate === "Kremer"){
-                    layerColor = "rgb(253,192,134)";
-                } else if (winners[0].candidate === "Kuehl"){
-                    layerColor = "#fbd341";
-                } else if (winners[0].candidate === "Melendez"){
-                    layerColor = "rgb(56,108,176)";
-                } else if (winners[0].candidate === "Preven"){
-                    layerColor = "rgb(240,2,127)";
-                } else if (winners[0].candidate === "Shriver"){
-                    layerColor = "#55cbdd";
-                } else if (winners[0].candidate === "Ulich"){
-                    layerColor = "rgb(102,102,102)";
-                }
-            } else {
-                layerColor = "rgb(0,0,0)";
-            }
-
-            return {
-                color: '#000000',
-                weight: .8,
-                opacity: .8,
-                fillOpacity: .8,
-                fillColor: layerColor
-            }
+        popEmbedBox: function(){
+            var embed_url_root = window.appConfig.project_root + "?=embed/";
+            jAlert("<h4>Embed this on your site or blog</h4><span>Copy this code and paste to source of your page. You may need to adjust the height parameter.<br /><br /><textarea>&lt;iframe src='" + embed_url_root + "' width='" + appConfig.embed_width + "' height='" + appConfig.embed_height + "' style='margin: 0 3% 0 3%;' frameborder='no'&gt;&lt;/iframe></textarea>");
         },
 
-        onEachFeature: function(feature, layer) {
-
-            feature.selected = false;
-
-            layer.on("click", function (e) {
-
-                var thisLat = e.latlng.lat;
-                var thisLong = e.latlng.lng;
-                //getCensusApiData(thisLat, thisLong);
-
-                if (feature.selected === false){
-                    this.setStyle({
-                        weight: 2,
-                        opacity: 2,
-                        fillOpacity: 2,
-                    });
-                    $("#data-point-display").append(_.template(template("templates/featcher-precinct.html"), feature.properties));
-                    feature.selected = true;
-                } else {
-                    this.setStyle({
-                        weight: .8,
-                        opacity: .8,
-                        fillOpacity: .8,
-                    });
-                    $("#data-point-display #precinct_" + feature.properties.june_2014_).remove();
-                    feature.selected = false;
-                }
-            });
-        },
-
-        getCensusApiData: function(lat, lng){
-            var apiPrefix = "http://api.censusreporter.org/1.0/geo/search?";
-            var apiQuery = apiPrefix + "lat=" + lat + "&lon=" + lng;
-            console.log(apiQuery);
-        },
-
-        kuehlMap: function(e){
-            this.changeLayerStylesForWinner("Kuehl");
-            $(e.currentTarget).css(
-                "background", "rgba(0, 127, 166, .6)"
-            );
-            $("a.shriverPrecincts").css(
-                "background", "rgba(0, 127, 166, 1)"
-            );
-        },
-
-        shriverMap: function(e){
-            this.changeLayerStylesForWinner("Shriver");
-            $(e.currentTarget).css(
-                "background", "rgba(0, 127, 166, .6)"
-            );
-            $("a.kuehlPrecincts").css(
-                "background", "rgba(0, 127, 166, 1)"
-            );
-        },
-
-        thisPrecinct: function(e){
-            var precinctBoundingBox;
-            _.each(this.map._layers, function(group, key){
-                if ("feature" in group){
-                    if (e.currentTarget.id === "precinct_" + group.feature.properties.june_2014_){
-                        precinctBoundingBox = group.getBounds();
-                        group.setStyle({
-                            weight: 2,
-                            opacity: 2,
-                            fillOpacity: 2,
-                        });
-                        group.feature.selected = true;
-                    } else {
-                        group.setStyle({
-                            weight: .3,
-                            opacity: .3,
-                            fillOpacity: .3,
-                        });
-                    }
-                }
-            });
-            this.map.fitBounds(precinctBoundingBox);
-        },
-
-        changeLayerStylesForWinner: function(winnerName){
-            $("#data-point-display").empty();
-            _.each(this.map._layers, function(group, key){
-                if ("feature" in group){
-                    if (group.feature.properties.winner.length === 1){
-                        var precinctWinner = group.feature.properties.winner[0].candidate;
-                        if (precinctWinner === winnerName){
-                            group.setStyle({
-                                weight: 2,
-                                opacity: 2,
-                                fillOpacity: 2,
-                            });
-                            group.feature.selected = true;
-
-                            $("#data-point-display").append(_.template(template("templates/featcher-precinct.html"), group.feature.properties));
-
-                        } else {
-                            group.setStyle({
-                                weight: .3,
-                                opacity: .3,
-                                fillOpacity: .3,
-                            });
-                        }
-                    }
-                }
-            });
-        },
-
-        resetMap: function(){
-            $("#data-point-display").empty();
-            $("a.shriverPrecincts").css(
-                "background", "rgba(0, 127, 166, 1)"
-            );
-            $("a.kuehlPrecincts").css(
-                "background", "rgba(0, 127, 166, 1)"
-            );
-            _.each(this.map._layers, function(group, key){
-                if ("feature" in group){
-                    if (group.feature.selected === true){
-                        group.feature.selected = false;
-                    }
-                    group.setStyle({
-                        weight: .8,
-                        opacity: .8,
-                        fillOpacity: .8,
-                    });
-                }
-            });
-            this.map.setView(
-                this.center, 11
-            );
-        },
-
-        render: function(mapDataObject){
-            $(mapDataObject.container).html(_.template(this.template));
-            this.map = L.map("content-map-canvas", {
-                scrollWheelZoom: false,
-                zoomControl: false,
-                minZoom: 6,
-                maxZoom: 14
-            }).setView(
-                this.center, 11
-            ).addLayer(
-                this.stamenToner
-            ).addControl(L.control.zoom({
-                position: "topright"
+        render: function(){
+            $(".kpcc-header").html(_.template(template(window.wrapperTemplatePath + "kpcc-header.html"), {
+                "encoded_share_url": encodeURIComponent(window.appConfig.project_root),
+                "twitter_share_text": window.appConfig.twitter_share_text
             }));
-            mapDataObject.map = this.map;
-            this.geojsonOne.addTo(this.map);
-        },
+            $(".kpcc-footer").html(_.template(template(window.wrapperTemplatePath + "kpcc-footer.html")));
+        }
     });
 
-    window.app = new App.Router();
-    Backbone.history.start({
-        root: "http://localhost:8880/2kpcc/static-projects/test/keller/la-county-district-3-results/",
-        pushState: false,
+
+
+
+
+
+
+
+    $(function(){
+        window.app = new App.Router();
+        Backbone.history.start({
+            root: "http://localhost:8880/2kpcc/static-projects/test/keller/la-county-district-3-results/",
+            pushState: false,
+        });
     });
 })();
