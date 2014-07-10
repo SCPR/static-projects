1684ab64c63190425bef88a5a9724b330965e4b7
Chris Keller <christopherlawrencekeller@gmail.com>
2014-07-10 09:24:36 -0700

    adds loading image and scripts to static files directory

--- static-files/v3-dependencies/scripts/backbone.tabletopSync.js
+++ static-files/v3-dependencies/scripts/backbone.tabletopSync.js
@@ -1,4 +1,4 @@
-/* 
+/*
   A drop-in read-only Google-Spreadsheets-backed replacement for Backbone.sync
 
   Tabletop must be successfully initialized prior to using fetch()
@@ -15,7 +15,7 @@ Backbone.tabletopSync = function(method, model, options, error) {
       error: error
     };
   }
-  
+
   var resp;
 
   var tabletopOptions = model.tabletop || model.collection.tabletop;
@@ -30,16 +30,16 @@ Backbone.tabletopSync = function(method, model, options, error) {
   } else {
     instance.addWanted(tabletopOptions.sheet);
   }
-  
+
   if(typeof(tabletopOptions.sheet) == "undefined") {
     return;
   }
-  
+
   var sheet = instance.sheets( tabletopOptions.sheet );
 
   if(typeof(sheet) === "undefined") {
     // Hasn't been fetched yet, let's fetch!
-    
+
     // Let's make sure we aren't re-requesting a sheet that doesn't exist
     if(typeof(instance.foundSheetNames) !== 'undefined' && _.indexOf(instance.foundSheetNames, tabletopOptions.sheet) === -1) {
       throw("Can't seem to find sheet " + tabletopOptions.sheet);
@@ -50,7 +50,7 @@ Backbone.tabletopSync = function(method, model, options, error) {
     })
     return;
   }
-  
+
   switch (method) {
     case "read":
       if(model.id) {
