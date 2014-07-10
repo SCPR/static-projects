74fa65995a013f5e49cf18b165888207a8dc3c36
Chris Keller <christopherlawrencekeller@gmail.com>
2014-07-10 10:41:25 -0700

    moves application wrapper to main static files directory and adds config options file to whooping cough directory

---
+++ static-files/v3-dependencies/scripts/application-wrapper.js
@@ -0,0 +1,121 @@
+(function(){
+
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {},
+        Router: {}
+    };
+
+    // load external templates via ajax
+    window.template = function(url){
+        var data = "<h1> failed to load url : " + url + "</h1>";
+        $.ajax({
+            async: false,
+            dataType: "text",
+            url: url,
+            success: function(response) {
+                data = response;
+            }
+        });
+        return data;
+    };
+
+    App.Views.ApplicationWrapper = Backbone.View.extend({
+
+        el: "body",
+
+        initialize: function(){
+
+            // checks url to see if we're live
+            var urlLink = window.location.href;
+
+            // sets template path
+            if (urlLink.indexOf("http://projects.scpr.org/") > -1){
+                window.wrapperTemplatePath = "http://projects.scpr.org/static/static-files/v3-dependencies/templates/"
+            } else {
+                window.wrapperTemplatePath = "/2kpcc/static-projects/static-files/v3-dependencies/templates/"
+            };
+
+            // sets embed options
+            if (urlLink.indexOf("embed") > -1){
+                window.appConfig.open_about_this = false;
+                window.appConfig.comments = false;
+                $(".data-comments").remove();
+            };
+
+            // set params for mobile devices
+            if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+                window.appConfig.open_about_this = false;
+                window.appConfig.initial_zoom = 5;
+            };
+
+            // checks comments setting
+            if (window.appConfig.comments === true){
+                var disqus_shortname = 'kpcc-projects';
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);
+            } else {
+                $(".data-comments").remove();
+            };
+
+            this.render();
+
+        },
+
+        events: {
+            "click li.projects-embed a": "renderEmbedBox",
+        },
+
+        renderEmbedBox: function(){
+            jAlert("<h4>Embed this on your site or blog</h4><span>Copy this code and paste to source of your page. You may need to adjust the height parameter.<br /><br /><textarea>&lt;iframe src='" + window.appConfig.project_root + "?=embed/' width='" + appConfig.embed_width + "' height='" + appConfig.embed_height + "' style='margin: 0 0 0 0;' frameborder='no'&gt;&lt;/iframe></textarea>");
+        },
+
+        render: function(){
+
+            $(".kpcc-header").html(_.template(template(window.wrapperTemplatePath + "kpcc-header.html"), {
+                "encoded_share_url": encodeURIComponent(window.appConfig.project_root),
+                "twitter_share_text": window.appConfig.twitter_share_text
+            }));
+
+            $(".data-details").html(_.template(template("templates/data-details.html")));
+
+            $(".kpcc-footer").html(_.template(template(window.wrapperTemplatePath + "kpcc-footer.html")));
+
+            if (window.appConfig.open_about_this === true){
+                $('.text').collapse('show');
+            };
+
+            if (window.appConfig.embed_this === false){
+                $('li.projects-embed').addClass('hidden');
+            }
+
+            $('.text').on('shown.bs.collapse', function(){
+                $('span.text').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            });
+
+            $('.text').on('hidden.bs.collapse', function(){
+                $('span.text').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+            });
+
+            $('.about').on('shown.bs.collapse', function(){
+                $('span.about').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            });
+
+            $('.about').on('hidden.bs.collapse', function(){
+                $('span.about').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
+            });
+
+        }
+    });
+
+    $(function(){
+        window.app = new App.Router();
+        Backbone.history.start({
+            root: window.appConfig.project_root,
+            pushState: false,
+        });
+    });
+
+})();
