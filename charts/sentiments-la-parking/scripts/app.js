186175c879aa08b07ea8d9ae1c7cded8c5f66ee8
Chris Keller <christopherlawrencekeller@gmail.com>
2014-07-22 16:51:00 -0700

    updates parking sentiments chart with more configurable version

--- charts/sentiments-la-parking/scripts/app.js
+++ charts/sentiments-la-parking/scripts/app.js
@@ -1,84 +1,44 @@
-var jqueryNoConflict = jQuery;
-var initializeTemplates = initializeTemplates || {};
-var fn = fn || {};
-var appConfig = appConfig || {};
+App.Router = Backbone.Router.extend({
+    initialize: function(){
+        this.applicationWrapper = new App.Views.ApplicationWrapper();
+        return this.applicationWrapper;
+    }
 
-// begin main function
-jqueryNoConflict(document).ready(function() {
+    /*
+    routes: {
+        "": "renderApplicationVisuals",
+    },
+    */
 
-    var urlLink = window.location.href;
-    if (urlLink.indexOf("embed") > -1){
-        appConfig.open_about_this = false;
-    };
+    /*
+    renderApplicationVisuals: function(){
+        if (this.applicationVisuals){
+            this.applicationVisuals.remove();
+        };
 
-    // set params for mobile devices
-    if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
-        appConfig.open_about_this = false;
-    };
+        this.applicationVisuals = new App.Views.ApplicationVisuals({
+            container: ".data-visuals"
+        });
 
-    initializeTemplates.renderStaticTemplates();
+        return this.applicationVisuals;
+    },
+    */
+
 });
 
-// application configuration object
-var appConfig = {
-    open_about_this: true,
-    embed_this: false,
-    embed_url_root: "http://projects.scpr.org/static/charts/sentiments-la-parking/?=embed/",
-    embed_width: "100%",
-    embed_height: "850px"
-};
+/*
+App.Views.ApplicationVisuals = Backbone.View.extend({
 
-// begin template rendering object
-var initializeTemplates = {
-    renderStaticTemplates: function(){
-        var proxyPrefix = 'http://projects.scpr.org/static/static-files/v3-dependencies/templates/';
-        renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '.kpcc-header');
-        renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '.kpcc-footer');
-        renderHandlebarsTemplate('templates/data-share.handlebars', '.data-share');
-        renderHandlebarsTemplate('templates/data-details.handlebars', '.data-details');
+    template: template("templates/data-visuals.html"),
 
-        var checkExist = setInterval(function() {
+    el: ".data-visuals",
 
-            if (jqueryNoConflict('.header-links').length) {
-                clearInterval(checkExist);
-                initializeTemplates.hideEmbedBox();
-            }
-
-            if (jqueryNoConflict('.buttons').length) {
-                clearInterval(checkExist);
-                initializeTemplates.toggleDisplayIcon();
-            }
-        }, 1000);
+    initialize: function(viewObject){
+        //this.render(viewObject);
     },
 
-    hideEmbedBox: function(){
-        if (appConfig.embed_this === false){
-            jqueryNoConflict('li.projects-embed').addClass('hidden');
-        };
-    },
-
-    renderEmbedBox: function(){
-        jAlert("<h4>Embed this on your site or blog</h4><span>Copy this code and paste to source of your page. You may need to adjust the height parameter.<br /><br /><textarea>&lt;iframe src='" + appConfig.embed_url_root + "' width='" + appConfig.embed_width + "' height='" + appConfig.embed_height + "' style='margin: 0 3% 0 3%;' frameborder='no'&gt;&lt;/iframe></textarea>");
-    },
-
-    toggleDisplayIcon: function(){
-        jqueryNoConflict('.text').on('shown.bs.collapse', function(){
-            jqueryNoConflict('span.text').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.text').on('hidden.bs.collapse', function(){
-            jqueryNoConflict('span.text').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.about').on('shown.bs.collapse', function(){
-            jqueryNoConflict('span.about').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.about').on('hidden.bs.collapse', function(){
-            jqueryNoConflict('span.about').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
-        });
-
-        if (appConfig.open_about_this === true){
-            $('.text').collapse('show');
-        };
-
+    render: function(viewObject){
+        $(viewObject.container).html(_.template(this.template));
     }
-};
-// end template rendering object
+});
+*/
---
+++ charts/sentiments-la-parking/scripts/config.js
@@ -0,0 +1,14 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/static/charts/sentiments-la-parking/?=embed/",
+    embed_this: false,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1650px",
+    twitter_share_text: "Parking in LA: Where is it worst?"
+
+};
