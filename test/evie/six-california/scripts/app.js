5a838e6bf5bd64a258eb61a192864ec94306aa49
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-06 08:55:43 -0700

    updates to six california's quiz to add comments to help evie navigate the structure

--- test/evie/six-california/scripts/app.js
+++ test/evie/six-california/scripts/app.js
@@ -1,46 +1,22 @@
-    window.storage = Tabletop.init({
-        key: "https://docs.google.com/spreadsheets/d/1H6hFZQiolqW7fU5Zdruy0LWTPn8zqDN2gW4ZkQoFUaI/pubhtml",
-        wait: true
-    });
-
-    App.Models.WhoopingCoughCurrent = Backbone.Model.extend({
+    // create a model for a question here
+    App.Models.UserQuestion = Backbone.Model.extend({
         defaults: {
-            county: null,
-            countyproper: null,
-            updated: null,
-            cases: null,
-            rate: null,
-        },
-        tabletop: {
-            instance: window.storage,
-            sheet: "data_7_8_2014"
-        },
-        sync: Backbone.tabletopSync
-    });
-
-    App.Models.WhoopingCoughHistorical = Backbone.Model.extend({
-        defaults: {
-            county: null,
-            countyproper: null,
-            updated: null,
-            cases: null,
-            rate: null,
+            question_text: null,
+            answer_1: null,
+            answer_2: null,
+            answer_3: null,
+            answer_4: null,
+            answer_5: null,
+            answer_6: null
         },
     });
 
-    App.Collections.WhoopingCoughCurrents = Backbone.Collection.extend({
-        model: App.Models.WhoopingCoughCurrent,
-        tabletop: {
-            instance: window.storage,
-            sheet: "data_7_8_2014"
-        },
-        sync: Backbone.tabletopSync
+    // create a collection for models here
+    App.Collections.UserQuestions = Backbone.Collection.extend({
+        model: App.Models.UserQuestion,
     });
 
-    App.Collections.WhoopingCoughHistoricals = Backbone.Collection.extend({
-        model: App.Models.WhoopingCoughHistorical
-    });
-
+    // router helps to manage the flow of things through routes
     App.Router = Backbone.Router.extend({
         initialize: function(){
             this.applicationWrapper = new App.Views.ApplicationWrapper();
@@ -51,9 +27,11 @@
             "": "fetchData",
         },
 
+        // we'll use this when user hits the main page to
+        // populate our models with data
         fetchData: function(){
             var _this = this;
-            var applicationCollection = new App.Collections.WhoopingCoughCurrents();
+            this.applicationCollection = new App.Collections.UserQuestions();
             applicationCollection.fetch({
                 async: true
             });
@@ -65,52 +43,55 @@
             }, 500);
         },
 
+        // render our template where the user will interact
         renderApplicationVisuals: function(collection){
             if (this.applicationVisuals){
                 this.applicationVisuals.remove();
             };
-
             this.applicationVisuals = new App.Views.ApplicationVisuals({
                 collection: collection,
                 container: ".data-visuals"
             });
-
             return this.applicationVisuals;
-
         },
-
     });
 
+    // this is what will basically be our landing view
+    // where the user can initiate the quiz
+    // all our views will mimic this structure and
+    // change out templates and element values
     App.Views.ApplicationVisuals = Backbone.View.extend({
 
+        // specifies the template we'll use
         template: template("templates/data-visuals.html"),
 
+        // specifies which element receives the template
         el: ".data-visuals",
 
+        // initialize starts the ball rolling
+        // accepts an object holding information about our app
         initialize: function(viewObject){
-            console.log("hi");           
+            console.log("hi");
 
+            // passes the view object to a render
+            // function that writes the template
             this.render(viewObject);
 
         },
 
-
-
+        // controls actions on dom elements
+        // helps organize the state of the application
         events: {
           "click button": "beginQuiz"
         },
 
-                
         beginQuiz: function () {
             alert("Clicked!")
         },
 
+        // renders the template
+        // accepts an object holding information about our app
         render: function(viewObject){
-
             $(viewObject.container).html(_.template(this.template));
-           
-            
         },
-
-
     });
