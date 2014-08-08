8d9357acb011975c9add98f23c60d40fbbb939b9
Evie Liu <eviexing@gmail.com>
2014-08-08 11:08:54 -0700

    create RSO timeline

---
+++ timelines/rso-in-socal/timeline-config.js
@@ -0,0 +1,40 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: 'spreadsheet',
+
+    // example spreadsheet key
+    key: '0Aq8qwSArzKP9dDdnVUtaNzJPQTZYSmhSYzFMcEVqQXc',
+
+    // example path to data file
+    sourceFile: 'timeline-data.json',
+
+    // example path to meta file
+    metaFile: 'timeline-meta.json',
+
+    // name of sheet containing timeline entries
+    sheetName: 'Posts',
+
+    // name of sheet containing timeline meta
+    sheetMeta: 'MetaData',
+
+    // make the timeline embeddable
+    embedThis: true,
+
+    // url of the timeline
+    projectDirectory: 'http://projects.scpr.org/static/timelines/rso-in-socal/',
+
+    // newest or oldest
+    defaultDirection: 'newest',
+
+    // collapsed or expanded
+    defaultExpansion: 'expanded',
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: 'groupSegmentByYear',
+
+    // adjust timeline width
+    width: '90%'
+};
