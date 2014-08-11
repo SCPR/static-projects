08c397f378fde0edad959daca17c47506bac3198
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-11 10:06:27 -0700

    commits updates to the static map generator tool

--- internal/tools/static-map-generator/scripts/app.js
+++ internal/tools/static-map-generator/scripts/app.js
@@ -11,7 +11,7 @@
 
         routes: {
             "": "renderApplicationVisuals",
-            "map-creator/:mapType/": "renderPointMapInstance",
+            "map-creator/:mapType/": "renderMapApplication",
             "map-creator/:mapType?lat=:latitude&lng=:longitude&zoom=:zoomLevel&layer=:layer": "renderCustomMapInstance"
         },
 
@@ -22,9 +22,11 @@
             return this.applicationVisuals;
         },
 
-        renderPointMapInstance: function(mapType){
+        renderMapApplication: function(mapType){
+
             this.renderApplicationVisuals();
-            this.pointMapInstance = new App.Views.PointMapInstance({
+
+            var configObject = {
                 route: "initial",
                 container: ".content-map-data",
                 mapDiv: "content-map-canvas",
@@ -32,15 +34,30 @@
                 mapCenter: window.appConfig.map_center_los_angeles,
                 mapZoomLevel: window.appConfig.initial_map_zoom,
                 mapLayerId: "ROADMAP"
-            });
-            return this.pointMapInstance;
+            };
+
+            if (mapType === "point-map"){
+                this.pointMapInstance = new App.Views.PointMapInstance(configObject);
+                return this.pointMapInstance;
+            } else if (mapType === "line-map"){
+                this.lineMapInstance = new App.Views.LineMapInstance(configObject);
+                return this.lineMapInstance;
+            } else if (mapType === "polygon-map"){
+                console.log("gonna learn to draw polygons");
+                //this.polygonMapInstance = new App.Views.PolygonMapInstance(configObject);
+                //return this.polygonMapInstance;
+            }
+
+
         },
 
         renderCustomMapInstance: function(mapType, latitude, longitude, zoomLevel, layer){
             this.renderApplicationVisuals();
+
             var lat = parseFloat(latitude);
             var lng = parseFloat(longitude);
-            this.customMapInstance = new App.Views.PointMapInstance({
+
+            var configObject = {
                 route: "custom",
                 container: ".content-map-data",
                 mapDiv: "content-map-canvas",
@@ -48,8 +65,20 @@
                 mapCenter: new google.maps.LatLng(lat,lng),
                 mapZoomLevel: zoomLevel,
                 mapLayerId: layer
-            });
-            return this.customMapInstance;
+            };
+
+            if (mapType === "point-map"){
+                this.customMapInstance = new App.Views.PointMapInstance(configObject);
+                return this.customMapInstance;
+            } else if (mapType === "line-map"){
+                this.customMapInstance = new App.Views.LineMapInstance(configObject);
+                return this.customMapInstance;
+            } else if (mapType === "polygon-map"){
+                console.log("gonna learn to draw polygons");
+                //this.customMapInstance = new App.Views.PolygonMapInstance(configObject);
+                //return this.customMapInstance;
+            }
+
         }
     });
 
@@ -76,7 +105,7 @@
         }
     });
 
-    // renders the initial general map
+    // renders point map
     App.Views.PointMapInstance = Backbone.View.extend({
 
         template: template("templates/point-map.html"),
@@ -129,31 +158,19 @@
         },
 
         changeRadio: function(e){
-            var latitude = $("input[id='latitudeSearch']").val();
-            var longitude = $("input[id='longitudeSearch']").val();
+            var latitude = this.viewObject.mapCenter.k;
+            var longitude = this.viewObject.mapCenter.B;
             var layer = e.target.value;
             this.updateMap(latitude, longitude, layer);
         },
 
         updateMap: function(latitude, longitude, layer){
-
-            var latitude = $("input[id='latitudeSearch']").val();
-            var longitude = $("input[id='longitudeSearch']").val();
-            var layer = $('input[name=map-type]:checked').val();
-
             if (latitude != '' && longitude != ''){
-                window.app.navigate("#map-creator/" + window.MapCreatorConfig.mapType + "?lat=" + latitude + "&lng=" + longitude + "&zoom=" + window.appConfig.initial_map_zoom + "&layer=" + layer, {
-                    trigger: true,
-                    replace: true,
-                });
-            } else {
-                alert("NO!");
-                window.app.navigate("", {
+                window.app.navigate("#map-creator/" + window.MapCreatorConfig.mapType + "?lat=" + latitude + "&lng=" + longitude + "&zoom=" + this.viewObject.mapZoomLevel + "&layer=" + layer, {
                     trigger: true,
                     replace: true,
                 });
             }
-
         },
 
         generateImage: function(){
@@ -181,7 +198,7 @@
                 center: viewObject.mapCenter,
                 zoom: parseInt(viewObject.mapZoomLevel),
                 scrollwheel: false,
-                draggable: false,
+                draggable: true,
                 mapTypeControl: false,
                 navigationControl: true,
                 streetViewControl: false,
@@ -271,5 +288,226 @@
                 });
             });
         }
+
+    });
+
+    App.Models.LineSegment = Backbone.Model.extend({
+        defaults: {
+            position: null,
+            draggable: null,
+            map: null,
+            lat: null,
+            lng: null
+        }
+    });
+
+    App.Collections.LineSegments = Backbone.Collection.extend({
+        model: App.Models.LineSegment
+    });
+
+
+
+
+
+
+    // renders the map to draw lines on
+    App.Views.LineMapInstance = Backbone.View.extend({
+
+        template: template("templates/line-map.html"),
+
+        el: ".content-map-data",
+
+        initialize: function(viewObject){
+
+            $("#map-type-list option").each(function(){
+                if ($(this).val() === viewObject.mapType){
+                    this.selected = true;
+                }
+            });
+
+            this.viewObject = viewObject;
+
+            window.MapCreatorConfig.mapType = this.viewObject.mapType;
+
+            $(this.viewObject.container).html(_.template(this.template));
+
+            $("#" + this.viewObject.mapLayerId).prop("checked", true);
+
+            this.render();
+        },
+
+        events: {
+            "keyup :input": "addressSearch",
+            "change input[type=radio]": "changeRadio",
+            //"change input[type=checkbox]": "beginDrawing",
+            "click button#submit": "generateImage",
+        },
+
+        addressSearch: function(e){
+
+            $("input[id='addressSearch']").geocomplete({
+                details: "form"
+            });
+
+            var latitude = $("input[id='latitudeSearch']").val();
+            var longitude = $("input[id='longitudeSearch']").val();
+            var layer = $('input[name=map-type]:checked').val();
+
+            if(e.keyCode != 13) {
+                return false;
+            } else if (e.keyCode === 13 && latitude === '' && longitude === '') {
+                return false;
+            } else {
+                this.updateMap(latitude, longitude, layer);
+            }
+
+        },
+
+        changeRadio: function(e){
+            var latitude = this.viewObject.mapCenter.k;
+            var longitude = this.viewObject.mapCenter.B;
+            var layer = e.target.value;
+            this.updateMap(latitude, longitude, layer);
+        },
+
+        updateMap: function(latitude, longitude, layer){
+            if (latitude != '' && longitude != ''){
+                window.app.navigate("#map-creator/" + window.MapCreatorConfig.mapType + "?lat=" + latitude + "&lng=" + longitude + "&zoom=" + this.viewObject.mapZoomLevel + "&layer=" + layer, {
+                    trigger: true,
+                    replace: true,
+                });
+            }
+        },
+
+        beginDrawing: function(viewObject){
+
+            var thatViewObject = viewObject;
+
+            var drawingManager = new google.maps.drawing.DrawingManager({
+                drawingMode: google.maps.drawing.OverlayType.MARKER,
+                drawingControl: true,
+                drawingControlOptions: {
+                    position: google.maps.ControlPosition.TOP_CENTER,
+                    drawingModes: [
+                        //google.maps.drawing.OverlayType.MARKER,
+                        //google.maps.drawing.OverlayType.CIRCLE,
+                        //google.maps.drawing.OverlayType.POLYGON,
+                        google.maps.drawing.OverlayType.POLYLINE,
+                        //google.maps.drawing.OverlayType.RECTANGLE
+                    ]
+                },
+                circleOptions: {
+                    fillColor: '#ffffff',
+                    fillOpacity: 1,
+                    strokeWeight: 5,
+                    clickable: false,
+                    editable: true,
+                    zIndex: 1
+                }
+            });
+
+            drawingManager.setMap(thatViewObject.map);
+
+            thatViewObject.polylinePaths = [];
+
+            google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+                var polylinePaths = event.overlay.getPath();
+                thatViewObject.polylinePaths.push(polylinePaths.j);
+            });
+
+        },
+
+        generateImage: function(){
+            var baseUrl = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCgh93OAbzooidV0OUpIOoc6kTxV5o69do";
+            var mapCenter = "&center=" + this.viewObject.mapCenter.k + "," + this.viewObject.mapCenter.B;
+            var mapZoom = "&zoom=" + this.viewObject.mapZoomLevel;
+            var mapSize = "&size=" + window.MapCreatorConfig.sizeParams;
+            var mapType = "&scale=2&maptype=" + this.viewObject.mapLayerId.toLowerCase();
+            var mapPolyline = "&path=color:0xff0000ff|weight:5";
+
+            var pathArrays = this.viewObject.polylinePaths;
+
+
+            //mapPolyline += "|" + lineArray[x].k + "," + lineArray[x].B;
+
+            console.log(pathArrays);
+
+            //var imageUrl = baseUrl + mapCenter + mapZoom + mapSize + mapType + mapPolyline;
+
+            //console.log(imageUrl);
+
+            /*
+            $("#content-map-image").html(
+                "<img src='" + imageUrl + "' />"
+            );
+
+            $("#content-map-image").before(
+                "<h6 class='image-link'>Copy the <a href='" + imageUrl + "' target='blank'>url</a> for this image</h6>"
+            );
+            */
+
+        },
+
+        render: function(){
+
+            var mapDiv = document.getElementById(this.viewObject.mapDiv);
+
+            var map = new google.maps.Map(mapDiv, {
+                center: this.viewObject.mapCenter,
+                zoom: parseInt(this.viewObject.mapZoomLevel),
+                scrollwheel: false,
+                draggable: true,
+                mapTypeControl: false,
+                navigationControl: true,
+                streetViewControl: false,
+                panControl: false,
+                scaleControl: false,
+                mapTypeId: google.maps.MapTypeId[this.viewObject.mapLayerId],
+                navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.SMALL,
+                position: google.maps.ControlPosition.RIGHT_TOP
+                }
+            });
+
+            this.viewObject.map = map;
+
+            /*
+            var marker = new google.maps.Marker({
+                position: this.viewObject.mapCenter,
+                draggable: true,
+                map: map
+            });
+            */
+
+
+            //this.mapClickListener(map, this.viewObject);
+
+
+            /*
+            this.mapZoomListener(map, this.viewObject);
+
+            this.mapDragListener(marker, this.viewObject);
+
+            google.maps.event.addDomListener(map, "idle", function() {
+                center = map.getCenter();
+            });
+
+            google.maps.event.addDomListener(window, "resize", function() {
+                map.setCenter(this.mapCenter);
+            });
+            */
+
+            this.beginDrawing(this.viewObject);
+
+            if (this.viewObject.route === "custom"){
+                $("input[id='latitudeSearch']").val(this.viewObject.mapCenter.k);
+                $("input[id='longitudeSearch']").val(this.viewObject.mapCenter.B);
+            }
+
+            $("#content-map-canvas").before(
+                "<h6 class='live-link'>" + this.viewObject.mapType + "</h6>"
+            );
+
+        },
 
     });
