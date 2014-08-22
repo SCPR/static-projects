a165261a8a0e58e8a7e4edebe09585317b0c9b72
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-22 14:40:40 -0700

    updates to exide timeline to move the data to static files and add entries over the past six months

--- timelines/exide-shutdown-timeline/timeline-config.js
+++ timelines/exide-shutdown-timeline/timeline-config.js
@@ -1,40 +1,40 @@
 var kpccTimelineConfig = {
     // choose spreadsheet or flat-file
-    dataSource: 'spreadsheet',
+    dataSource: "flat-file",
 
     // example spreadsheet key
-    key: '0Aq8qwSArzKP9dEcyNW9TS3V3NVdCM3hOMFpYZHVhcmc',
+    key: "0Aq8qwSArzKP9dEcyNW9TS3V3NVdCM3hOMFpYZHVhcmc",
 
     // example path to data file
-    sourceFile: 'timeline-data.json',
+    sourceFile: "timeline-data.json",
 
     // example path to meta file
-    metaFile: 'timeline-meta.json',
+    metaFile: "timeline-meta.json",
 
     // name of sheet containing timeline entries
-    sheetName: 'Posts',
+    sheetName: "Posts",
 
     // name of sheet containing timeline meta
-    sheetMeta: 'MetaData',
+    sheetMeta: "MetaData",
 
     // make the timeline embeddable
     embedThis: true,
 
     // url of the timeline
-    projectDirectory: 'http://projects.scpr.org/static/timelines/exide-shutdown-timeline/',
+    projectDirectory: "http://projects.scpr.org/static/timelines/exide-shutdown-timeline/",
 
     // newest or oldest
-    defaultDirection: 'newest',
+    defaultDirection: "newest",
 
     // collapsed or expanded
-    defaultExpansion: 'expanded',
+    defaultExpansion: "expanded",
 
     // display social sharing links
     sharing: true,
 
     // groupSegmentByYear or groupSegmentByDecade
-    groupFunction: 'groupSegmentByYear',
+    groupFunction: "groupSegmentByYear",
 
     // adjust timeline width
-    width: '90%'
+    width: "90%"
 };
