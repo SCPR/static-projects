f3609d6312c34bbfdcb259ae7424662035e2cca7
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-26 08:46:16 -0700

    adjusts height of embed code and twitter share text and read more link for the station fire project

--- maps/2009-station-fire-perimeters/scripts/config.js
+++ maps/2009-station-fire-perimeters/scripts/config.js
@@ -4,10 +4,10 @@ window.appConfig = {
     open_about_this: true,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/static/maps/2009-station-fire-perimeters",
+    project_root: "http://projects.scpr.org/maps/2009-station-fire-perimeters",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
-    embed_height: "1650px",
-    twitter_share_text: "Map: View a county-by-county breakdown of whooping cough cases in California so far in 2014",
+    embed_height: "750px",
+    twitter_share_text: "This animated image shows the growth of the Station Fire between Aug. 29 and Sept. 14, 2009.",
 };
