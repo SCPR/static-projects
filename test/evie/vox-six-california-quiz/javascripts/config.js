301a6c168b59c9d89e8df151b89a8f7052b55001
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-26 10:27:55 -0700

    updates and cleans up the html markup for the data sources tab in the six california's quiz

--- test/evie/vox-six-california-quiz/javascripts/config.js
+++ test/evie/vox-six-california-quiz/javascripts/config.js
@@ -3,9 +3,9 @@ window.appConfig = {
     comments: false,
     is_mobile: null,
     project_root: "http://projects.scpr.org/static/interactives/six-californias-quiz",
-    embed_this: false,
+    embed_this: true,
     is_embedded: null,
     embed_width: "100%",
-    embed_height: "1650px",
-    twitter_share_text: "I belong to Central California according to KPCC six California quiz! Check your result here",
+    embed_height: "1000px",
+    twitter_share_text: "I belong to Central California according to KPCC's Six California quiz! Check your result here",
 };
