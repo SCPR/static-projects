e414dd6b629b0c3636143f494ff607142dd7b1c3
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-26 10:40:05 -0700

    adds six californias quiz to live directory

---
+++ interactives/six-californias-quiz/javascripts/config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/interactives/six-californias-quiz",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1000px",
+    twitter_share_text: "I belong to Central California according to KPCC's Six California quiz! Check your result here",
+};
