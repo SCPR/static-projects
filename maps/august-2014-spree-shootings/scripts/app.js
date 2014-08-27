bf84597a87a3068ca37d2b2617bbda42a7b26138
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-27 12:00:36 -0700

    updates august spree killing map to use proper template

--- maps/august-2014-spree-shootings/scripts/app.js
+++ maps/august-2014-spree-shootings/scripts/app.js
@@ -1,89 +1,36 @@
-var jqueryNoConflict = jQuery;
-var initializeTemplates = initializeTemplates || {};
-var fn = fn || {};
-var appConfig = appConfig || {};
+    App.Router = Backbone.Router.extend({
+        initialize: function(){
+            this.applicationWrapper = new App.Views.ApplicationWrapper();
+            return this.applicationWrapper;
+        },
+        routes: {
+            "": "renderApplicationVisuals",
+        },
+        renderApplicationVisuals: function(collection){
+            if (this.applicationVisuals){
+                this.applicationVisuals.remove();
+            };
+            this.applicationVisuals = new App.Views.ApplicationVisuals({
+                container: ".data-visuals"
+            });
+            return this.applicationVisuals;
+        }
+    });
 
-// begin main function
-jqueryNoConflict(document).ready(function() {
-    Swag.registerHelpers(Handlebars);
-
-    initializeTemplates.renderStaticTemplates();
-
-    var urlLink = window.location.href;
-    if (urlLink.indexOf("embed") > -1){
-        appConfig.openAboutThis = false;
-    };
-
-    var storymap_data = "data/published.json";
-
-    var storymap_options = {};
-
-    var storymap = new VCO.StoryMap("story-map-container", storymap_data, storymap_options);
-
-    window.onresize = function(event) {
-        storymap.updateDisplay();
-    }
-
-});
-
-// application configuration object
-var appConfig = {
-    openAboutThis: true,
-    embed_this: true,
-    embed_url_root: "http://projects.scpr.org/static/maps/anniversary-of-oj-simpson-pursuit/?=embed/",
-    embed_width: "92%",
-    embed_height: "1000px"
-};
-
-// begin template rendering object
-var initializeTemplates = {
-    renderStaticTemplates: function(){
-        var proxyPrefix = 'http://projects.scpr.org/static/static-files/v3-dependencies/templates/';
-        renderHandlebarsTemplate(proxyPrefix + 'kpcc-header.handlebars', '.kpcc-header');
-        renderHandlebarsTemplate(proxyPrefix + 'kpcc-footer.handlebars', '.kpcc-footer');
-        renderHandlebarsTemplate('templates/data-share.handlebars', '.data-share');
-
-        var checkExist = setInterval(function() {
-
-            if (jqueryNoConflict('.header-links').length) {
-                clearInterval(checkExist);
-                initializeTemplates.hideEmbedBox();
+    App.Views.ApplicationVisuals = Backbone.View.extend({
+        template: template("templates/data-visuals.html"),
+        el: ".data-visuals",
+        initialize: function(viewObject){
+            $(".data-details").remove();
+            this.render(viewObject);
+        },
+        render: function(viewObject){
+            $(viewObject.container).html(_.template(this.template));
+            var storymap_data = "data/published.json";
+            var storymap_options = {};
+            var storymap = new VCO.StoryMap("story-map-container", storymap_data, storymap_options);
+            window.onresize = function(event) {
+                storymap.updateDisplay();
             }
-
-            if (jqueryNoConflict('.buttons').length) {
-                clearInterval(checkExist);
-                initializeTemplates.toggleDisplayIcon();
-            }
-        }, 1000);
-    },
-
-    hideEmbedBox: function(){
-        if (appConfig.embed_this === false){
-            jqueryNoConflict('li.projects-embed').addClass('hidden');
-        };
-    },
-
-    renderEmbedBox: function(){
-        jAlert("<h4>Embed this on your site or blog</h4><span>Copy this code and paste to source of your page. You may need to adjust the height parameter.<br /><br /><textarea>&lt;iframe src='" + appConfig.embed_url_root + "' width='" + appConfig.embed_width + "' height='" + appConfig.embed_height + "' style='margin: 0 3% 0 3%;' frameborder='no'&gt;&lt;/iframe></textarea>");
-    },
-
-    toggleDisplayIcon: function(){
-        jqueryNoConflict('.text').on('shown.bs.collapse', function(){
-            jqueryNoConflict('span.text').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.text').on('hidden.bs.collapse', function(){
-            jqueryNoConflict('span.text').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.about').on('shown.bs.collapse', function(){
-            jqueryNoConflict('span.about').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        });
-        jqueryNoConflict('.about').on('hidden.bs.collapse', function(){
-            jqueryNoConflict('span.about').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
-        });
-
-        if (appConfig.openAboutThis === true){
-            $('.text').collapse('show');
-        };
-    }
-};
-// end template rendering object
+        },
+    });
---
+++ maps/august-2014-spree-shootings/scripts/config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/static/maps/august-2014-spree-shootings",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1650px",
+    twitter_share_text: "Map: Man charged in LA-area shootings, suspect in four others",
+};
