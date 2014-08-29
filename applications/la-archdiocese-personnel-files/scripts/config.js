fc04e2bdce62377a23a062f6d392c7c01d618463
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-28 22:48:49 -0700

    refactors the la archdiocese document project to use the updated template

---
+++ applications/la-archdiocese-personnel-files/scripts/config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/applications/la-archdiocese-personnel-files",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "750px",
+    twitter_share_text: "Search KPCC's database of the files released by the L.A. Archdiocese in connection with the priest abuse scandal",
+};
