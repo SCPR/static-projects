2d5b9f83d6e93d4b3893bc4b2dbc5006ded963e1
Chris Keller <christopherlawrencekeller@gmail.com>
2014-08-29 00:11:45 -0700

    adds lausd ipads emails project

---
+++ applications/lausd-ipad-emails/scripts/app.js
@@ -0,0 +1,37 @@
+    App.Router = Backbone.Router.extend({
+        initialize: function(){
+            this.applicationWrapper = new App.Views.ApplicationWrapper();
+            return this.applicationWrapper;
+        },
+        routes: {
+            "": "renderApplicationVisuals",
+        },
+        renderApplicationVisuals: function(collection){
+            if (this.applicationVisuals){
+                this.applicationVisuals.remove();
+            };
+            this.applicationVisuals = new App.Views.ApplicationVisuals({
+                container: ".data-visuals"
+            });
+            return this.applicationVisuals;
+        },
+    });
+
+    App.Views.ApplicationVisuals = Backbone.View.extend({
+        template: template("templates/data-visuals.html"),
+        el: ".data-visuals",
+        initialize: function(viewObject){
+            this.render(viewObject);
+        },
+        render: function(viewObject){
+            $(viewObject.container).html(_.template(this.template));
+            dc.embed.load('http://www.documentcloud.org/search/embed/', {
+                q: "projectid: 15522-lausd-ipad-emails ",
+                container: "#DC-search-projectid-15522-lausd-ipad-emails",
+                order: "title",
+                per_page: 6,
+                search_bar: true,
+                organization: 97
+            });
+        },
+    });
