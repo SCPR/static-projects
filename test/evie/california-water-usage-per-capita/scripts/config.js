b1ae2134abd40105aef54c809a80fe4f87fe98d3
Evie Liu <eviexing@gmail.com>
2014-09-30 10:24:51 -0700

    added water usage charts

---
+++ test/evie/california-water-usage-per-capita/scripts/config.js
@@ -0,0 +1,25 @@
+window.appConfig = {
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1000px",
+    twitter_share_text: "",
+
+    project_root: "http://projects.scpr.org/charts/so-cal-marriage-licenses/",
+    csv_url: "data/july-water-data.csv",
+    title: "Top 10 agencies with most per capita water usage in California, July 2014 data",
+    source: "California Water Resources Control Board",
+    type: "bar",// line, bar, spline, area, area-spline, pie, donut, gauge
+    axis_x_label: "Water Supplier",
+    axis_y_label: "Per capita water usage (Gallon)",
+    rotated: true,
+    labels: true,   
+    //grid_value1: "1000",
+    //grid_value2: "2000",
+    //grid_value3: "3000",
+    //grid_value4: "4000",
+    stacked_group: []
+};
