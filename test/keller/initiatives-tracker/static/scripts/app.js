b4d5ab60b5d2afbf59eec289d91ad1f22538713a
Chris Keller <christopherlawrencekeller@gmail.com>
2014-10-14 09:40:46 -0700

    add static assets to directory

--- test/keller/initiatives-tracker/static/scripts/app.js
+++ test/keller/initiatives-tracker/static/scripts/app.js
@@ -1,26 +1,39 @@
     var w = 400, //width
-        h = 400, //height
+        h = 420, //height
         r = 180, //radius
         pi = Math.PI,
-        color = d3.scale.category20c();
+        //color = d3.scale.category20c();
         //border = 5,
-        outerRadius = w / 2 - 5,
+        outerRadius = w / 2 - 10,
         innerRadius = w / 3,
         data = [
             {"value": chartConfig.support_percent},
             {"value": chartConfig.oppose_percent}
         ];
 
-    console.log(outerRadius);
-    console.log(innerRadius);
+    var color = d3.scale.ordinal()
+        .domain(["one","two"])
+        .range(["#B4DD0A", "#66D8D8"]);
 
     var vis = d3.select(".chart-container")
-        .append("svg:svg") //create the SVG element inside the <body>
-        .data([data]) //associate our data with the document
-        .attr("width", w) //set the width and height of our visualization
+        .append("svg:svg")
+        .data([data])
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr('viewBox','0 0 '+Math.min(w, h) + ' ' + Math.min(w, h))
+        .attr('preserveAspectRatio','xMinYMin')
+        .append("svg:g")
+        .attr("transform", "translate(" + Math.min(w, h) / 2 + "," + Math.min(w, h) / 2 + ")");
+
+    /*
+    var vis = d3.select(".chart-container")
+        .append("svg:svg")
+        .data([data])
+        .attr("width", w)
         .attr("height", h)
-        .append("svg:g") //make a group to hold our pie chart
+        .append("svg:g")
         .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+    */
 
     var arc = d3.svg.arc() //this will create <path> elements for us using arc data
         .outerRadius(r);
@@ -59,16 +72,3 @@
         arcs.append("svg:path")
             .attr("fill", "#ffffff")
             .attr("d", spaceBorder);
-
-        /*
-        arcs.append("svg:text")
-            .attr("transform", function(d){
-                d.innerRadius = 0;
-                d.outerRadius = r;
-                return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("text-anchor", "middle")
-            .text(function(d, i){
-                return data[i].label;
-            });
-        */
