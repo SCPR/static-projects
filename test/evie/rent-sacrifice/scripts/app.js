b210cc76545252ae1091115aeb56bd46989aec33
Evie Liu <eviexing@gmail.com>
2014-10-28 10:50:35 -0700

    second draft

--- test/evie/rent-sacrifice/scripts/app.js
+++ test/evie/rent-sacrifice/scripts/app.js
@@ -13,8 +13,8 @@ function scaleFont() {
 
   if (viewPortWidth >= 1900) {$('body').attr('class','extraWide');}
   else if (viewPortWidth >= 1400) {$('body').attr('class','wide');}
-  else if (viewPortWidth >= 1000) {$('body').attr('class','');}
-  else if (viewPortWidth >= 700) {$('body').attr('class','narrow');}
+  else if (viewPortWidth >= 1000) {$('body').attr('class','medium');}
+  else if (viewPortWidth >= 600) {$('body').attr('class','narrow');}
   else {$('body').attr('class','extraNarrow');}
 };
 
@@ -24,9 +24,10 @@ $.getJSON("data/sacrifice_data.json", function(data){
 
 	$("#button").click(function(){
 
-	    if (i < 19) {
+	    if (i < 16) {
 	    	i++;
 	    	$("#iconimg").attr("src","img/" + data[i].image + ".png");
+	    	$("#category").html(data[i].category);
 	    	$("#quotes").html('"' + data[i].quote + '"');
 	    	$("#name").html(data[i].name);
 	    	$("#location").html(data[i].location);
@@ -34,6 +35,7 @@ $.getJSON("data/sacrifice_data.json", function(data){
 	    else {
 	    	i = 0;
 	    	$("#iconimg").attr("src","img/" + data[i].image + ".png");
+	    	$("#category").html(data[i].category);
 	    	$("#quotes").html('"' + data[i].quote + '"');
 			$("#name").html(data[i].name);
 	    	$("#location").html(data[i].location);
