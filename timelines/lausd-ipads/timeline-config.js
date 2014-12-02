a8630f0d365de501fffb0ecfe6001c88afd9000b
Chris Keller <christopherlawrencekeller@gmail.com>
2014-12-02 12:14:18 -0800

    adds lausd timeline to static projects

---
+++ timelines/lausd-ipads/timeline-config.js
@@ -0,0 +1,40 @@
+var kpccTimelineConfig = {
+    // choose spreadsheet or flat-file
+    dataSource: "flat-file",
+
+    // example spreadsheet key
+    key: "0Aq8qwSArzKP9dEh0MEZsOGpRTlg2X0dNaHRmWVZfa0E",
+
+    // example path to data file
+    sourceFile: "timeline-data.json",
+
+    // example path to meta file
+    metaFile: "timeline-meta.json",
+
+    // name of sheet containing timeline entries
+    sheetName: "Posts",
+
+    // name of sheet containing timeline meta
+    sheetMeta: "MetaData",
+
+    // make the timeline embeddable
+    embedThis: true,
+
+    // url of the timeline
+    projectDirectory: "http://projects.scpr.org/timelines/lausd-ipads/",
+
+    // newest or oldest
+    defaultDirection: "newest",
+
+    // collapsed or expanded
+    defaultExpansion: "expanded",
+
+    // display social sharing links
+    sharing: true,
+
+    // groupSegmentByYear or groupSegmentByDecade
+    groupFunction: "groupSegmentByYear",
+
+    // adjust timeline width
+    width: "90%"
+};
