dbe0a43d2b5137e55daab51eace1f0f30e5474ca
Chris Keller <christopherlawrencekeller@gmail.com>
2015-01-23 15:48:15 -0800

    refactors 2015 academy awards preview to use backbone template

---
+++ interactives/2015-academy-awards-preview/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/interactives/2015-academy-awards-preview/",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1650px",
+    twitter_share_text: "Who will take home the coveted Oscars for best picture, director, actor and actress and supporting actor and actress at the 2015 Academy Awards?",
+};
