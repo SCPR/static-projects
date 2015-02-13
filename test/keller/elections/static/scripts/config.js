7952152f5be0fbc7b09f4e02c6982bae7d1b2481
Chris Keller <christopherlawrencekeller@gmail.com>
2015-02-13 01:00:59 -0800

    rectifies local branch

---
+++ test/keller/elections/static/scripts/config.js
@@ -0,0 +1,30 @@
+window.appConfig = {
+
+    open_about_this: true,
+
+    comments: true,
+
+    is_mobile: false,
+
+    project_root: "http://projects.scpr.org/interactives/2015-academy-awards-preview/",
+
+    embed_this: true,
+
+    is_embedded: false,
+
+    embed_width: "100%",
+
+    embed_height: "1650px",
+
+    // "flat-file", "spreadsheet" or null
+    data_source: null,
+
+    // path to data source
+    // "data/2015_oscars_prediction_data.json"
+    data_url: null,
+
+    // path to google spreadsheet
+    // "https://docs.google.com/spreadsheet/pub?key=0Aq8qwSArzKP9dEJNQjhYREtjU2lGLVcwYkNBRlQwMVE&output=html"
+    spreadsheet_key: null
+
+};
