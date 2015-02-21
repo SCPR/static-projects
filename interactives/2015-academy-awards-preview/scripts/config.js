1f3aee0e0caf360712383ec97ebb1db293e60d94
Chris Keller <christopherlawrencekeller@gmail.com>
2015-02-20 16:13:37 -0800

    updates to the 2015 academy awards interactive to flip the switch to json file data source

--- interactives/2015-academy-awards-preview/scripts/config.js
+++ interactives/2015-academy-awards-preview/scripts/config.js
@@ -10,7 +10,6 @@ window.appConfig = {
     embed_width: "100%",
     embed_height: "1650px",
     twitter_share_text: "Who will take home the coveted Oscars for best picture, director, actor and actress and supporting actor and actress at the 2015 Academy Awards?",
-    data_source: "spreadsheet",
+    data_source: "flat-file",
     data_url: "data/2015_oscars_prediction_data.json",
-    spreadsheet_key: "https://docs.google.com/spreadsheet/pub?key=0Aq8qwSArzKP9dEJNQjhYREtjU2lGLVcwYkNBRlQwMVE&output=html"
 };
