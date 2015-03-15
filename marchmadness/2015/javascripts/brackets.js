302cafb9b0e358030252b76665339960e09d7ee3
Sean Dillingham <sean.dillingham@gmail.com>
2015-03-14 20:01:29 -0700

    add 2015 March Madness brackets

---
+++ marchmadness/2015/javascripts/brackets.js
@@ -0,0 +1,59 @@
+//= require "placeholders.jquery.min"
+
+
+// Homepage hero resizing
+(function () {
+var e;
+e = function () {
+  if ($(".download").css("position") != "relative" ){
+    $(".hero").css("height", $(window).height() - 90);
+  } 
+};
+$(function () {
+    return $(window).on("load resize", e)
+})
+}).call(this);
+
+// Smooth anchor scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+// Facebook popup
+function fbs_click() {
+  u=location.href;
+  t=document.title;
+  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
+  return false;
+}
+
+// Expose Matchup polls for touch and no-touch devices
+$(".touch .matchup:not(.focus,.final)").on("click", function() {
+  $(this).addClass("focus");
+});
+$(".close").on("click", function() {
+  $(this).closest(".matchup").removeClass("focus");
+  return false;
+});
+$(".no-touch .matchup:not(.final)").hover(
+  function() {
+    $( this ).addClass("focus");
+  }, function() {
+    $( this ).removeClass("focus");
+  });
+
+// Style Matchups that have been voted on
+$(".pds-vote-button").click(function() {
+  $(this).closest(".matchup").addClass("voted");
+});
