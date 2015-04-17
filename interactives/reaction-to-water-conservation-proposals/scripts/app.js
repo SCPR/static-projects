7e380dc6df5426b21b7d24cde675e4a4e5f697aa
Chris Keller <christopherlawrencekeller@gmail.com>
2015-04-17 15:30:42 -0700

    adds document cloud search application for water resources board public comments

---
+++ interactives/reaction-to-water-conservation-proposals/scripts/app.js
@@ -0,0 +1,35 @@
+    window.splitTitle = function(title){
+        var split_title = title.split("Email");
+        parsed_date = moment(split_title[0]).format('MMMM D, YYYY');
+        split_title[0] = parsed_date;
+        return split_title[0] + " email " + split_title[1];
+    };
+
+    App.Router = Backbone.Router.extend({
+
+        initialize: function(){
+            this.applicationWrapper = new App.Views.ApplicationWrapper();
+            return this.applicationWrapper;
+        },
+
+        routes: {
+            "": "renderSearchView",
+        },
+
+        renderSearchView: function(){
+            $(".data-visuals").html(
+                "<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>" +
+                    "<div id='DC-search-projectid-19698-public-comments-regarding-state-water-board-conservation-proposal' class='DC-search-container'></div>" +
+                "</div>"
+            );
+            dc.embed.load('http://www.documentcloud.org/search/embed/', {
+                q: "projectid: 19698-public-comments-regarding-state-water-board-conservation-proposal ",
+                container: "#DC-search-projectid-19698-public-comments-regarding-state-water-board-conservation-proposal",
+                order: "title",
+                per_page: 12,
+                search_bar: true,
+                organization: 97
+            });
+        }
+
+    });
---
+++ interactives/reaction-to-water-conservation-proposals/scripts/config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: false,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/interactives/reaction-to-water-conservation-proposals/",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "750px",
+    twitter_share_text: "Search public comments submitted to the state about a proposal requiring cities to cut consumption anywhere from 10 to 35 percent depending on recent use rates and conservation efforts.",
+};
