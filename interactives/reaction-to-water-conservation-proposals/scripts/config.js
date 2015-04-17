d0bf04f22aad0365fe936880c4974a0b440acb0d
Chris Keller <christopherlawrencekeller@gmail.com>
2015-04-17 15:35:00 -0700

    updates document cloud search application for water resources board public comments

--- interactives/reaction-to-water-conservation-proposals/scripts/config.js
+++ interactives/reaction-to-water-conservation-proposals/scripts/config.js
@@ -1,5 +1,5 @@
 window.appConfig = {
-    open_about_this: false,
+    open_about_this: true,
     comments: false,
     is_mobile: null,
     project_root: "http://projects.scpr.org/interactives/reaction-to-water-conservation-proposals/",
@@ -7,5 +7,5 @@ window.appConfig = {
     is_embedded: null,
     embed_width: "100%",
     embed_height: "750px",
-    twitter_share_text: "Search public comments submitted to the state about a proposal requiring cities to cut consumption anywhere from 10 to 35 percent depending on recent use rates and conservation efforts.",
+    twitter_share_text: "Search public comments submitted to the state about a proposal requiring water agencies to cut consumption anywhere from 10 to 35 percent depending on recent use rates and conservation efforts.",
 };
