0efb473e194decceb3aff1431e9b3a0bc8b8721c
Chris Keller <christopherlawrencekeller@gmail.com>
2015-04-17 15:37:21 -0700

    updates document cloud search application for water resources board public comments

--- interactives/reaction-to-water-conservation-proposals/scripts/config.js
+++ interactives/reaction-to-water-conservation-proposals/scripts/config.js
@@ -6,6 +6,6 @@ window.appConfig = {
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
-    embed_height: "750px",
+    embed_height: "1750px",
     twitter_share_text: "Search public comments submitted to the state about a proposal requiring water agencies to cut consumption anywhere from 10 to 35 percent depending on recent use rates and conservation efforts.",
 };
