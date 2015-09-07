cf2ee8128c917562b78db2388bd589e8d8988a2d
Chris Keller <christopherlawrencekeller@gmail.com>
2015-09-07 08:12:12 -0700

    moves lead remediation map to live directory

--- maps/los-angeles-lead-remediation/scripts/config.js
+++ maps/los-angeles-lead-remediation/scripts/config.js
@@ -4,7 +4,7 @@ window.appConfig = {
     open_about_this: true,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/",
+    project_root: "http://projects.scpr.org/maps/lead-remediation-efforts-by-los-angeles/",
     embed_this: false,
     is_embedded: null,
     embed_width: "100%",
