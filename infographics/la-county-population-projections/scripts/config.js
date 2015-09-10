25a79d9f29a0186d5055f6606ed68cf7efd8e039
Chris Keller <christopherlawrencekeller@gmail.com>
2015-09-10 15:08:42 -0700

    adds la county population projections gif to repo

---
+++ infographics/la-county-population-projections/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: false,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/infographics/la-county-population-projections/",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1650px",
+    twitter_share_text: "Forecasting LA County's population growth",
+};
