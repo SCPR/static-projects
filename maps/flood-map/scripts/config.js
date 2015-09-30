32aa2dbd10faf5aac6bd4e7de5b50d36a1e2abac
Aaron M <amendelson@scpr.org>
2015-09-30 11:47:43 -0700

    updates config

--- maps/flood-map/scripts/config.js
+++ maps/flood-map/scripts/config.js
@@ -8,6 +8,6 @@ window.appConfig = {
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
-    embed_height: "1000px",
-    twitter_share_text: "More homeless people are sleeping in RVs and campers. See where in @KPCC's interactive chart",
+    embed_height: "950px",
+    twitter_share_text: "Explore flood zones in Southern California in @KPCC's interactive map",
 };
