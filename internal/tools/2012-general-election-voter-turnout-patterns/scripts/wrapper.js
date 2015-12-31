46cbbb62c80e338f88c2ecfa41ef1c3d8a501b7c
Chris Keller <christopherlawrencekeller@gmail.com>
2015-12-30 16:37:37 -0800

    removes testing header from 2012 voter turnout map

--- internal/tools/2012-general-election-voter-turnout-patterns/scripts/wrapper.js
+++ internal/tools/2012-general-election-voter-turnout-patterns/scripts/wrapper.js
@@ -1,7 +1,7 @@
 (function(){
 
     window.appConfig = {
-        testing: true,
+        testing: false,
         project_root: "http://projects.scpr.org/internal/tools/",
         open_about_this: false,
         comments: false,
