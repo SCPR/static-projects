6282ff4520533095c715abf28750b6803f9438e2
Aaron M <amendelson@scpr.org>
2016-01-04 13:48:09 -0800

    adds alternate map, changes read more link

---
+++ maps/do-i-live-in-a-california-flood-zone-northern-ca/scripts/_config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: false,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/maps/do-i-live-in-a-california-flood-zone/",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1150px",
+    twitter_share_text: "Do you live in a flood zone? Check using @KPCC's interactive map."
+};
