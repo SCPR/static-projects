ad097bccbfc13c429f1a829462554af111d13518
Aaron M <amendelson@scpr.org>
2016-01-04 13:33:12 -0800

    updates project root

--- maps/do-i-live-in-a-california-flood-zone/scripts/_config.js
+++ maps/do-i-live-in-a-california-flood-zone/scripts/_config.js
@@ -2,7 +2,7 @@ window.appConfig = {
     open_about_this: false,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/applications/do-i-live-in-a-california-flood-zone/",
+    project_root: "http://projects.scpr.org/maps/do-i-live-in-a-california-flood-zone/",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
