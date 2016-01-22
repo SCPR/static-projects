f70446882708f0019f2fdc9c076573ad71cfa15a
Aaron M <amendelson@scpr.org>
2016-01-21 17:17:21 -0800

    add gauge to el nino repo

--- charts/el-nino-rainfall-to-date/scripts/config.js
+++ charts/el-nino-rainfall-to-date/scripts/config.js
@@ -4,10 +4,10 @@ window.appConfig = {
     open_about_this: true,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/charts/income-and-water-use/",
+    project_root: "http://projects.scpr.org/charts/el-nino-rainfall-to-date/",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
-    embed_height: "1650px",
-    twitter_share_text: "Do rich communities use more water? Explore the issue in @KPCC's interactive chart.",
+    embed_height: "1350px",
+    twitter_share_text: "How much rain has SoCal gotten this year? Explore the issue in @KPCC's interactive chart.",
 };
