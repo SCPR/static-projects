b3f13f1c7137bc6d411a75a3d0ab9a197f1c9114
Aaron M <amendelson@scpr.org>
2016-10-07 14:28:41 -0700

    updates project root

--- test/aaron/who-mailed-it/scripts/config.js
+++ test/aaron/who-mailed-it/scripts/config.js
@@ -4,10 +4,10 @@ window.appConfig = {
     open_about_this: true,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/maps/bus-shelters/",
+    project_root: "http://projects.scpr.org/test/aaron/who-mailed-it/",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
     embed_height: "1000px",
-    twitter_share_text: "LA's missing bus shelters. Explore @KPCC's map",
+    twitter_share_text: "KPCC explores political mailers with the #WhoMailedIt project",
 };
