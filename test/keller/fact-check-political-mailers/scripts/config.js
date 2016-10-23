4b4e405ae7fa80cbf7f2628e69d8e25cb039d3cf
Chris Keller <christopherlawrencekeller@gmail.com>
2016-10-23 15:18:59 -0700

    Adds political mailers project

---
+++ test/keller/fact-check-political-mailers/scripts/config.js
@@ -0,0 +1,11 @@
+window.appConfig = {
+    open_about_this: false,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/applications/fact-check-political-mailers/#search",
+    embed_this: false,
+    is_embedded: false,
+    embed_width: "100%",
+    embed_height: "750px",
+    twitter_share_text: "",
+};
