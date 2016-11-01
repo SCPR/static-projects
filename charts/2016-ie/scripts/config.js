07544d614ffb167c6eaf28adbe3ac80c7b2d5b23
Aaron M <amendelson@scpr.org>
2016-10-31 17:47:46 -0700

    moves IEs

--- charts/2016-ie/scripts/config.js
+++ charts/2016-ie/scripts/config.js
@@ -4,10 +4,10 @@ window.appConfig = {
     open_about_this: false,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/charts/el-nino-rainfall-to-date/",
+    project_root: "http://projects.scpr.org/charts/2016-ie/",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
     embed_height: "770px",
-    twitter_share_text: "How much rain has SoCal gotten this year? Explore the issue in @KPCC's interactive chart.",
+    twitter_share_text: "How money from charter school advocates is flooding 2016 elections.",
 };
