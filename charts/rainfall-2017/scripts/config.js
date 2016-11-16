aab26229cbbc02f3f898762e7d6694b842097633
Aaron M <amendelson@scpr.org>
2016-11-16 13:04:25 -0800

    adds version of rainfall chart for 2017 water year

---
+++ charts/rainfall-2017/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/charts/rainfall-2017/",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "770px",
+    twitter_share_text: "How much rain has SoCal seen this year? Explore in @KPCC's interactive chart.",
+};
