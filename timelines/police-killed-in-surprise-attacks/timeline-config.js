2841b90f4131a86c8e9ea6c4050420d96389b7bc
Chris Keller <christopherlawrencekeller@gmail.com>
2017-02-13 10:42:12 -0800

    updates to only existing tik-tok timeline to streamline config and setup beyond the POC stage

---
+++ timelines/police-killed-in-surprise-attacks/timeline-config.js
@@ -0,0 +1,17 @@
+$.get("police-ambush-killings-data.csv", function(csv) {
+    var tikTok = new TikTok({
+        el: "#tik-tok-csv",
+        projecturl: null,
+        kicker: null,
+        title: "Police killed in surprise attacks in the U.S. and its territories",
+        credits: null,
+        published: null,
+        text: null,
+        readmoreurl: null,
+        sources: null,
+        entries: csv,
+        descending: true,
+        groupBy: "years",
+        dateDisplay: "MMMM D, YYYY"
+    });
+});
