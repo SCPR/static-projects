7e9504c25dbdf7ee01a8698e70b28255a8db8f5e
Chris Keller <christopherlawrencekeller@gmail.com>
2017-02-13 15:18:07 -0800

    makes the timeline config kicker a variable accessible via underscore template

--- timelines/pasadenas-devils-gate-dam/timeline-config.js
+++ timelines/pasadenas-devils-gate-dam/timeline-config.js
@@ -2,8 +2,8 @@ $.get("pasadenas-devils-gate-dam.csv", function(csv) {
     var tikTok = new TikTok({
         el: "#tik-tok-instance",
         projecturl: null,
-        kicker: null,
-        title: "Xxxxxxx",
+        kicker: "Timeline",
+        title: "Sediment buildup at Pasadena's Devil's Gate Dam",
         credits: null,
         published: null,
         text: null,
