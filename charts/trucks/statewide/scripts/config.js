9127c77be33dab82eddf260caf93d814f6cd232f
amendelson <amendelson@scpr.org>
2017-03-28 15:03:57 -0700

    add full state chart

---
+++ charts/trucks/statewide/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/charts/trucks",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1000px",
+    twitter_share_text: "Charting truck collisions in Southern California",
+};
