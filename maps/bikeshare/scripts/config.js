291520527977807301d4f2e765cb6742418f418c
amendelson <amendelson@scpr.org>
2017-04-18 16:28:35 -0700

    update w bikeshare changes

--- maps/bikeshare/scripts/config.js
+++ maps/bikeshare/scripts/config.js
@@ -4,10 +4,10 @@ window.appConfig = {
     open_about_this: true,
     comments: false,
     is_mobile: null,
-    project_root: "http://projects.scpr.org/maps/bus-shelters/",
+    project_root: "http://projects.scpr.org/maps/bikeshare/",
     embed_this: true,
     is_embedded: null,
     embed_width: "100%",
     embed_height: "1000px",
-    twitter_share_text: "LA's missing bus shelters. Explore @KPCC's map",
+    twitter_share_text: "Explore DTLA's bike share network in @KPCC's interactive map",
 };
