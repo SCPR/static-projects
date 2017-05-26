4df00be244bb110a01bc19f9b7fd3af9e3adb588
amendelson <amendelson@scpr.org>
2017-05-26 12:59:36 -0700

    new tl

---
+++ timelines/single-payer-california/timeline-config.js
@@ -0,0 +1,17 @@
+$.get("single-payer-california.csv", function(csv) {
+    var tikTok = new TikTok({
+        el: "#tik-tok-instance",
+        projecturl: null,
+        kicker: "Health",
+        title: "Timeline: Single payer in California",
+        credits: null,
+        published: null,
+        text: null,
+        readmoreurl: null,
+        sources: null,
+        entries: csv,
+        descending: true,
+        groupBy: "years",
+        dateDisplay: "YYYY"
+    });
+});
