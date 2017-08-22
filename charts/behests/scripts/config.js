f4f0a059d0ff6b830bf8d4f8df12a38837a6f0f6
amendelson <aaron.a.mendelson@gmail.com>
2017-08-21 21:05:05 -0700

    moves chart

---
+++ charts/behests/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "http://projects.scpr.org/charts/behests",
+    embed_this: true,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1000px",
+    twitter_share_text: "Chart: Payments behested by Los Angeles Mayor Eric Garcetti",
+};
