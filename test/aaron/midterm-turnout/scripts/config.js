b5b1c8b1257e9ce0d4930592539ac408e22e7d0f
amendelson <aaron.a.mendelson@gmail.com>
2018-05-30 17:13:00 -0700

    changes to turnout viz

---
+++ test/aaron/midterm-turnout/scripts/config.js
@@ -0,0 +1,13 @@
+window.appConfig = {
+
+    // general configs
+    open_about_this: true,
+    comments: false,
+    is_mobile: null,
+    project_root: "https://projects.scpr.org/charts/midterm-turnout/",
+    embed_this: false,
+    is_embedded: null,
+    embed_width: "100%",
+    embed_height: "1000px",
+    twitter_share_text: "Does your county turn out to vote in midterm primaries?",
+};
