b7cddc11d040fccba0c1b9088707dd6f1b9d7473
Dana Amihere <lizzy7395@gmail.com>
2018-10-19 23:39:21 -0700

    Responsive audio box

--- prop-13/stories/fairness/fairness.js
+++ prop-13/stories/fairness/fairness.js
@@ -12,7 +12,7 @@ if ($(window).width() > 800){
     const videoName = $(this).attr('data-videoname');
 
     $(this).children('.mp4-source').attr('src', `../fairness/video/${videoName}-large.mp4`);
-    // $(this).children('.webm-source').attr('src', `../../../../prop-13/video/${videoName}-${videoSize}.webm`);
+    $(this).children('.webm-source').attr('src', `../fairness/video/${videoName}-large..webm`);
 
     $(this)[0].load();
 
