d9f944d0d4a855e643c8e7507f82337f108380cb
Dana Amihere <lizzy7395@gmail.com>
2018-10-20 00:57:47 -0700

    Workaround to autoplay?

--- prop-13/js/scripts.js
+++ prop-13/js/scripts.js
@@ -14,6 +14,19 @@
 
 });
 
+// // Show loading animation.
+// var playPromise = video.play();
+//
+// if (playPromise !== undefined) {
+//   playPromise.then(_ => {
+//     // Automatic playback started!
+//     // Show playing UI.
+//   })
+//   .catch(error => {
+//     // Auto-play was prevented
+//     // Show paused UI.
+//   });
+// }
 
 $(document).ready(function(){
 
