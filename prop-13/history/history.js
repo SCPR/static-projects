d8e4953fdca3b376331aa58f6a0e46d834e3a6c6
Dana Amihere <lizzy7395@gmail.com>
2018-10-21 09:40:23 -0700

    History section is up

---
+++ prop-13/history/history.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function(){
+
+  if ($(window).width() > 1615){
+
+    const targetOffset1 = $("#history-section-1").offset().top;
+    const targetOffset2 = $("#anchor-point-1").offset().top;
+    //const targetOffset4 = $("#anchor-point-3").offset().top;
+
+    const $w = $(window).scroll(function() {
+
+      const scrollTop = $(window).scrollTop();
+      //$('.visuals-container').addClass('is-visible',scrollTop >= $('body').offset().top);
+
+      if (($w.scrollTop() + $(window).height()) > targetOffset1) {
+        $('.visuals-container img').attr("src","../history/images/history-jon-coupal.jpg");
+        $('.scroll-cutline').hide();
+        $('#cutline-history-jon-coupal').show();
+      }
+
+      if (($w.scrollTop() + $(window).height()) > targetOffset2) {
+        $('.visuals-container img').attr("src","../history/images/history-jarvis-gann-win.jpg");
+        $('.scroll-cutline').hide();
+        $('#cutline-history-jarvis-gann-win').show();
+      }
+
+    });
+  } else {
+    $('.visuals-container').addClass('is-visible');
+  }
+
+});
