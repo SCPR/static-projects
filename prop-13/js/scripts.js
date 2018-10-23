f5ebe1febef0418da292bfb3ffa04bed8e02f389
Dana Amihere <lizzy7395@gmail.com>
2018-10-23 12:11:13 -0700

    Updated gentrification

--- prop-13/js/scripts.js
+++ prop-13/js/scripts.js
@@ -7,8 +7,8 @@
     const videoSize = elSize > 800 ? 'large' : 'small';
     const videoName = $(this).attr('data-videoname');
 
-    $(this).children('.webm-source').attr('src', `../video/${videoName}-${videoSize}.webm`);
-    $(this).children('.mp4-source').attr('src', `../video/${videoName}-${videoSize}.mp4`);
+    $(this).children('.webm-source').attr('src', `../prop-13/video/${videoName}-${videoSize}.webm`);
+    $(this).children('.mp4-source').attr('src', `../prop-13/video/${videoName}-${videoSize}.mp4`);
 
 
     $(this)[0].load();
